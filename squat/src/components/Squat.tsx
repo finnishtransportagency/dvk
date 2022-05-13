9de8e66393f05ca03de073fe736ec56b9417665e
Aki Hiisilä <aki.hiisila@cgi.com>
2022-05-13 10:14:32 +0300

    Add squat home page template

---
+++ squat/src/components/Squat.tsx
@@ -0,0 +1,13 @@
+import './Squat.css';
+
+interface ContainerProps { }
+
+const Squat: React.FC<ContainerProps> = () => {
+  return (
+    <div>
+      <p>TODO: Squat-laskenta</p>
+    </div>
+  );
+};
+
+export default Squat;
--- squat/src/pages/Home.tsx
+++ squat/src/pages/Home.tsx
@@ -1,22 +1,17 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import Squat from '../components/Squat';
 import './Home.css';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+        <IonToolbar color="primary">
+          <IonTitle>Squat-laskenta</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent className="ion-padding">
+        <Squat />
       </IonContent>
     </IonPage>
   );
