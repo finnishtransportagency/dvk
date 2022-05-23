accf19c649626193f4df67b319917d43b818373e
Petri Hakala <EXT-Petri.Hakala@hsl.fi>
2022-05-23 15:43:08 +0300

    i18n support added

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,11 +1,13 @@
 import './Map.css';
+import { useTranslation } from 'react-i18next';
 
 interface ContainerProps { }
 
 const Map: React.FC<ContainerProps> = () => {
+  const { t } = useTranslation();
   return (
     <div>
-      <p>TODO: Kartta</p>
+      <p>{t('homePage.map.content')}</p>
     </div>
   );
 };
