da2b908710cbe1e86d02b8dd068cfdeb21f37b9a
Petri Hakala <92077410+hakalap@users.noreply.github.com>
2022-05-24 10:40:19 +0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'squat',
+  appName: 'dvk',
   webDir: 'build',
   bundledWebRuntime: false
 };
