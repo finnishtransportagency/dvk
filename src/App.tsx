105fb662e3c0efc7b551ee88a52c8b808f661867
Petri Hakala <petri.hakala@cgi.com>
2022-05-24 10:53:16 +0300

    Redirect to /home removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Redirect, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
@@ -28,11 +28,8 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
         <Route exact path="/">
-          <Redirect to="/home" />
+          <Home />
         </Route>
       </IonRouterOutlet>
     </IonReactRouter>
