057c50607abb765f6b8a777cc411a8d822502431
kettunenj <juhani.kettunen@cgi.com>
2022-06-05 19:24:27 +0300

    codebuild stage

--- squat/cdk/lib/squat-pipeline.ts
+++ squat/cdk/lib/squat-pipeline.ts
@@ -2,11 +2,10 @@ import * as cdk from "aws-cdk-lib";
 import { SecretValue, Stack } from "aws-cdk-lib";
 import * as codepipeline from "aws-cdk-lib/aws-codepipeline";
 import * as codebuild from "aws-cdk-lib/aws-codebuild";
-import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
 import * as iam from "aws-cdk-lib/aws-iam";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
-declare const distribution: cloudfront.Distribution;
+import { LinuxBuildImage } from "aws-cdk-lib/aws-codebuild";
 interface SquatPipelineProps {
   env: string;
 }
@@ -34,34 +33,73 @@ export class SquatPipeline extends Construct {
       actions: [sourceAction],
     });
 
+    // Create the build project for Squat app
+    // Tarvitsee STANDARD 5.0 Ubuntu OS, koska tarvitaan node > 14
+    const squatBuildProject = new codebuild.PipelineProject(this, "SquatBuild", {
+      environment: {
+        buildImage: LinuxBuildImage.STANDARD_5_0,
+      },
+      buildSpec: codebuild.BuildSpec.fromObject({
+        version: "0.2",
+        env: {
+          variables: {
+            CODEBUILD_SRC_DIR: "/squat",
+          },
+        },
+        phases: {
+          install: {
+            commands: ["echo update npm", "node -v", "npm -v"],
+            "runtime-versions": {
+              nodejs: "14.x",
+            },
+          },
+          build: {
+            commands: ["echo build squat app", "cd squat", "npm install", "npm run build"],
+          },
+        },
+        artifacts: {
+          "base-directory": "squat",
+          files: "build/*",
+        },
+      }),
+    });
+
+    const buildOutput = new codepipeline.Artifact();
+
     const buildStage = pipeline.addStage({
       stageName: "Build",
       actions: [
-        // optional property
-        // see below...
+        new cdk.aws_codepipeline_actions.CodeBuildAction({
+          actionName: "BuildSquatApp",
+          project: squatBuildProject,
+          input: sourceOutput,
+          outputs: [buildOutput],
+        }),
       ],
     });
 
+    const importedDistributionId = cdk.Fn.importValue("SquatDistribution"); //TODO: imoprt ympariston mukaan
+
     // Create the build project that will invalidate the cache
-    const invalidateBuildProject = new codebuild.PipelineProject(this, `InvalidateProject`, {
+    const invalidateBuildProject = new codebuild.PipelineProject(this, "InvalidateProject", {
       buildSpec: codebuild.BuildSpec.fromObject({
         version: "0.2",
         phases: {
           build: {
             commands: [
+              // eslint-disable-next-line no-template-curly-in-string
               'aws cloudfront create-invalidation --distribution-id ${CLOUDFRONT_ID} --paths "/*"',
-              // Choose whatever files or paths you'd like, or all files as specified here
             ],
           },
         },
       }),
       environmentVariables: {
-        CLOUDFRONT_ID: { value: distribution.distributionId },
+        CLOUDFRONT_ID: { value: importedDistributionId },
       },
     });
 
     // Add Cloudfront invalidation permissions to the project
-    const distributionArn = `arn:aws:cloudfront::${account}:distribution/${distribution.distributionId}`;
+    const distributionArn = `arn:aws:cloudfront::${account}:distribution/${importedDistributionId}`;
     console.log("distribution ARN: ", distributionArn);
     invalidateBuildProject.addToRolePolicy(
       new iam.PolicyStatement({
@@ -78,13 +116,13 @@ export class SquatPipeline extends Construct {
         new cdk.aws_codepipeline_actions.S3DeployAction({
           actionName: "S3Deploy",
           bucket: s3.Bucket.fromBucketName(this, "Bucket", importedBucketValue.toString()),
-          input: sourceOutput,
+          input: buildOutput,
           runOrder: 1,
         }),
         new cdk.aws_codepipeline_actions.CodeBuildAction({
           actionName: "InvalidateCache",
           project: invalidateBuildProject,
-          input: sourceOutput,
+          input: buildOutput,
           runOrder: 2,
         }),
       ],
