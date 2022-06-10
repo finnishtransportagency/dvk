b9c931c8d3484625a7de8f5e621d75a75fc73e55
Petri Hakala <petri.hakala@cgi.com>
2022-06-10 14:22:01 +0300

    Build script moved

--- cdk/bin/build_image.sh
+++ cdk/bin/build_image.sh
@@ -6,4 +6,4 @@ REPO_TAG=$REPO/$IMAGE:$BUILD_IMAGE_VERSION
 aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin $REPO
 aws ecr create-repository --repository-name $IMAGE || true
 docker pull $REPO_TAG || true
-docker build --progress=plain --cache-from $REPO_TAG -t $IMAGE:$BUILD_IMAGE_VERSION ../ && docker tag $IMAGE:$BUILD_IMAGE_VERSION $REPO_TAG && docker push $REPO_TAG
+docker build --progress=plain --cache-from $REPO_TAG -t $IMAGE:$BUILD_IMAGE_VERSION ../../ && docker tag $IMAGE:$BUILD_IMAGE_VERSION $REPO_TAG && docker push $REPO_TAG
