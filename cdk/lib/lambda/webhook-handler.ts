b54bdb512fea535f566efdff2fa9bdd39eec2de0
kettunenj <juhani.kettunen@cgi.com>
2022-06-15 10:53:23 +0300

    lambdaan eventin kasittelya ja commitin parsimista

--- cdk/lib/lambda/webhook-handler.ts
+++ cdk/lib/lambda/webhook-handler.ts
@@ -4,14 +4,47 @@ const devPipelineSquat = process.env.DEV_PIPELINE_SQUAT;
 const testPipelineSquat = process.env.TEST_PIPELINE_SQUAT;
 
 const handler = async function (event: any, context: any) {
+  console.log("Full event", event);
+
+  const method = event.requestContext.http.method;
+  if (!method || method !== "POST") {
+    return {
+      statusCode: 400,
+      body: `Cannot process http method: ${method}`,
+    };
+  }
   //TODO: validoi github webhook pyynto kts. github headerit ja secret
-  console.log("validate signature:", event.headers);
+  validateSignature(event.headers);
 
-  console.log(event);
-  console.log(devPipelineSquat);
-  console.log(testPipelineSquat);
+  //TODO: parsi event tiedot
+  const githubData = event.body;
+  const branch = githubData.ref;
 
-  //TODO: parsi event tiedot ja kaynnista oikea pipeline
+  if (!branch || !isDevTestProd(branch)) {
+    return {
+      statusCode: 400,
+      body: `Branch isn't deployable: ${branch}`,
+    };
+  }
+
+  const commit = githubData?.commits?.[0];
+
+  if (!commit) {
+    return {
+      statusCode: 400,
+      body: `No commit found`,
+    };
+  }
+
+  const commitAdded: string[] = commit.added;
+  const commitRemoved: string[] = commit.removed;
+  const commitModified: string[] = commit.modified;
+
+  getDistinctFolders(commitAdded);
+  getDistinctFolders(commitRemoved);
+  getDistinctFolders(commitModified);
+
+  // TODO: kaynnista oikea pipeline
   try {
     const client = new CodePipelineClient({});
     const command = new ListPipelinesCommand({});
@@ -31,5 +64,54 @@ const handler = async function (event: any, context: any) {
     body: JSON.stringify({ received: true }),
   };
 };
+
+const validBranches = ["main", "test", "prod"];
+function isDevTestProd(branch: string) {
+  return validBranches.includes(branch);
+}
+
+const ignoredTypes = ["txt", "md", "gitignore", "dockerignore", "pdf"];
+function getDistinctFolders(fileinfo: string[]): string[] {
+  console.log("fileinfo in: ", fileinfo);
+  const filteredFiles = fileinfo.filter((file) => {
+    const extension = file.split(".").pop();
+    if (extension && ignoredTypes.includes(extension)) {
+      console.log("removing ignored file: ", file);
+      return false;
+    }
+    return true;
+  });
+  console.log("after removing ignored files: ", filteredFiles);
+  const folders = filteredFiles.map((filteredFile) => filteredFile.slice(0, filteredFile.indexOf("/")));
+  const distinctFolders = [...new Set<string>(folders)];
+  console.log("distinct folders: ", distinctFolders);
+
+  return distinctFolders;
+}
+
+type EventHeaders = {
+  "x-hub-signature-256": string;
+  "content-length": number;
+  "x-amzn-tls-version": string;
+  "x-forwarded-proto": string;
+  "x-github-delivery": string;
+  "x-forwarded-port": number;
+  "x-forwarded-for": string;
+  "x-github-hook-installation-target-id": string;
+  "x-amzn-tls-cipher-suite": string;
+  "x-amzn-trace-id": string;
+  "x-github-event": string;
+  host: string;
+  accept: string;
+  "content-type": string;
+  "x-github-hook-id": string;
+  "user-agent": string;
+  "x-github-hook-installation-target-type": string;
+  "x-hub-signature": string;
+};
+function validateSignature(headers: EventHeaders) {
+  // validate signature
+  console.log("validating headers", headers);
+}
 
 export { handler };
