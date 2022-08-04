a5355049955ba36a01565d34f0b75b1406abeed2
Petri Hakala <petri.hakala@cgi.com>
2022-08-04 17:41:03 +0300

    Saimaa tifs added

--- cdk/bin/dynamodb.ts
+++ cdk/bin/dynamodb.ts
@@ -2,18 +2,51 @@ import { ListTablesCommand } from '@aws-sdk/client-dynamodb';
 import { PutCommand } from '@aws-sdk/lib-dynamodb';
 import Config from '../lib/config';
 import { getDynamoDBDocumentClient } from '../lib/lambda/db/dynamoClient';
-import vuosaariJson from './data/4927/vuosaari.json';
-import kemiJson from './data/10/kemi.json';
-import uusikaupunkiJson from './data/2345/uusikaupunki.json';
+import path from 'path';
+import fs from 'fs';
+import FairwayDBModel from '../lib/lambda/db/fairwayDBModel';
+
+function getAllFiles(dirPath: string, arrayOfFiles: string[]) {
+  const files = fs.readdirSync(dirPath);
+  arrayOfFiles = arrayOfFiles || [];
+  files.forEach(function (file) {
+    if (fs.statSync(dirPath + '/' + file).isDirectory()) {
+      arrayOfFiles = getAllFiles(dirPath + '/' + file, arrayOfFiles);
+    } else {
+      arrayOfFiles.push(path.join(dirPath, '/', file));
+    }
+  });
+  return arrayOfFiles;
+}
 
 async function main() {
   const response = await getDynamoDBDocumentClient().send(new ListTablesCommand({}));
   console.log(`Table names: ${response.TableNames?.join(', ')}`);
   const tableName = `Fairway-${Config.getEnvironment()}`;
   if (response.TableNames?.includes(tableName)) {
-    await getDynamoDBDocumentClient().send(new PutCommand({ TableName: tableName, Item: vuosaariJson }));
-    await getDynamoDBDocumentClient().send(new PutCommand({ TableName: tableName, Item: kemiJson }));
-    await getDynamoDBDocumentClient().send(new PutCommand({ TableName: tableName, Item: uusikaupunkiJson }));
+    const directoryPath = path.join(__dirname, 'data');
+    const arrayOfFiles = getAllFiles(directoryPath, []);
+    const geoTiffMap = new Map<number, string[]>();
+    for (const file of arrayOfFiles.filter((f) => f.endsWith('.tif'))) {
+      const id = file.match(/\/\d*\//)?.toString();
+      if (id) {
+        const fairwayId = parseInt(id.substring(1, id.length - 1), 10);
+        if (!geoTiffMap.get(fairwayId)) {
+          geoTiffMap.set(fairwayId, []);
+        }
+        geoTiffMap.get(fairwayId)?.push(file);
+      }
+    }
+    for (const file of arrayOfFiles.filter((f) => f.endsWith('.json'))) {
+      const fairway = JSON.parse(fs.readFileSync(file).toString()) as FairwayDBModel;
+      if (geoTiffMap.get(fairway.id)) {
+        fairway.geotiff = geoTiffMap.get(fairway.id)?.map((f) => {
+          const i = f.lastIndexOf('/');
+          return f.substring(i + 1);
+        });
+      }
+      await getDynamoDBDocumentClient().send(new PutCommand({ TableName: tableName, Item: fairway }));
+    }
     console.log(`Fairway table ${tableName} updated`);
   }
 }
