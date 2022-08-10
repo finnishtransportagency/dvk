70e0ad8491ebddb97cf1540232ad6fd688066e5b
Petri Hakala <petri.hakala@cgi.com>
2022-08-10 12:58:48 +0300

    Use proxy url in permanent environments

--- cdk/bin/setup.ts
+++ cdk/bin/setup.ts
@@ -80,13 +80,14 @@ async function main() {
   const backendStackOutputs = await readBackendStackOutputs();
   const frontendStackOutputs = await readFrontendStackOutputs();
   const envParameters = await readParametersForEnv(Config.getEnvironment());
+  const restAPIUrl = Config.isPermanentEnvironment()
+    ? 'https://' + envParameters.DMZProxyEndpoint
+    : 'http://' + backendStackOutputs.LoadBalancerDnsName;
   writeEnvFile('../.env.local', {
     REACT_APP_API_URL: backendStackOutputs.AppSyncAPIURL,
     REACT_APP_API_KEY: backendStackOutputs.AppSyncAPIKey,
-    REACT_APP_REST_API_URL: `http://${backendStackOutputs.LoadBalancerDnsName}/api`,
+    REACT_APP_REST_API_URL: `${restAPIUrl}/api`,
     REACT_APP_FRONTEND_DOMAIN_NAME: frontendStackOutputs.CloudFrontDomainName,
-    REACT_APP_BASIC_USERNAME: envParameters.BasicUsername,
-    REACT_APP_BASIC_PASSWORD: envParameters.BasicPassword,
   });
 }
 
