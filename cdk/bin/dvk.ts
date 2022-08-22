e4d9e961fedda8a95ff059628d66344827413a9e
Petri Hakala <petri.hakala@cgi.com>
2022-08-22 15:12:21 +0300

    Initial commit for combining cdk projects

--- cdk/bin/dvk.ts
+++ cdk/bin/dvk.ts
@@ -10,6 +10,8 @@ import { DvkBackendStack } from '../lib/dvk-backend';
 import { DvkFeaturePipelineStack } from '../lib/dvk-feature-pipeline';
 import { DvkSonarPipelineStack } from '../lib/dvk-sonar-pipeline';
 import { PipelineMessaging } from '../lib/pipeline-messaging';
+import { SquatSite } from '../lib/squat-site';
+import { SquatPipeline } from '../lib/squat-pipeline';
 
 class DvkPipelineMessagingStack extends cdk.Stack {
   constructor(parent: App, id: string, props: StackProps) {
@@ -31,6 +33,26 @@ class DvkPipelineStack extends cdk.Stack {
     super(parent, id, props);
 
     new DvkPipeline(this, 'DvkPipeline', { env });
+  }
+}
+
+class SquatSiteStack extends cdk.Stack {
+  constructor(parent: App, id: string, props: StackProps, env: string) {
+    super(parent, id, props);
+    const cloudfrontCertificateArn = Config.isPermanentEnvironment() ? new Config(this).getStringParameter('CloudFrontCertificateArn') : undefined;
+    new SquatSite(this, 'SquatSite', {
+      domainName: env === 'prod' ? 'vaylapilvi.fi' : 'testivaylapilvi.fi',
+      siteSubDomain: env === 'prod' ? 'dvk' : 'dvk' + env,
+      env,
+      cloudfrontCertificateArn,
+    });
+  }
+}
+class SquatPipelineStack extends cdk.Stack {
+  constructor(parent: App, id: string, props: StackProps, env: string) {
+    super(parent, id, props);
+
+    new SquatPipeline(this, 'SquatPipeline', { env });
   }
 }
 
@@ -91,3 +113,29 @@ new DvkPipelineMessagingStack(app, 'DvkPipelineMessagingStack', {
   },
   stackName: 'DvkPipelineMessagingStack',
 });
+
+new SquatSiteStack(
+  app,
+  'SquatSiteStack',
+  {
+    env: {
+      account: process.env.CDK_DEFAULT_ACCOUNT,
+      region: 'eu-west-1',
+    },
+    stackName: 'SquatSiteStack-' + appEnv,
+  },
+  appEnv
+);
+
+new SquatPipelineStack(
+  app,
+  'SquatPipelineStack',
+  {
+    env: {
+      account: process.env.CDK_DEFAULT_ACCOUNT,
+      region: 'eu-west-1',
+    },
+    stackName: 'SquatPipelineStack-' + appEnv,
+  },
+  appEnv
+);
