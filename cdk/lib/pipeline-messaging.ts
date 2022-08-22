4b7bd4661cd915fd543cd5d51ebfb27c2d090972
kettuncgi <48211670+kettunju@users.noreply.github.com>
2022-08-22 13:48:50 +0300

    feature: cicd triggerit ja ilmoitukset (DVK-68) (#44)
    * initial commit
    
    * change cdk subscription type and lambda event type
    
    * parse pipeline message for rocketchat, uncomment all environment pipelines
    
    * added dvk pipeline arn, removed unused variable

---
+++ cdk/lib/pipeline-messaging.ts
@@ -0,0 +1,63 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as notifications from 'aws-cdk-lib/aws-codestarnotifications';
+import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
+import * as nodejsfunction from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as ssm from 'aws-cdk-lib/aws-ssm';
+import * as subscription from 'aws-cdk-lib/aws-sns-subscriptions';
+import { CfnOutput } from 'aws-cdk-lib';
+export class PipelineMessaging extends Construct {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    const messageHandler = new nodejsfunction.NodejsFunction(this, 'WebhookHandler', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      entry: 'lib/lambda/message-handler.ts',
+      handler: 'handler',
+      environment: {
+        ROCKETCHAT_USER: ssm.StringParameter.valueForStringParameter(this, 'RocketchatUser'),
+        ROCKETCHAT_PASSWORD: ssm.StringParameter.valueForStringParameter(this, 'RocketchatPassword'),
+      },
+    });
+
+    const importedSquatPipelineArnDev = cdk.Fn.importValue('SquatPipeline-ARN-dev');
+    const importedSquatPipelineArnTest = cdk.Fn.importValue('SquatPipeline-ARN-test');
+    const importedDVKPipelineArnDev = cdk.Fn.importValue('DvkPipeline-ARN-dev');
+    const importedDVKPipelineArnTest = cdk.Fn.importValue('DvkPipeline-ARN-test');
+
+    const pipelineMap = new Map([
+      [importedSquatPipelineArnDev, '-squat-dev'],
+      [importedSquatPipelineArnTest, '-squat-test'],
+      [importedDVKPipelineArnDev, '-dvk-dev'],
+      [importedDVKPipelineArnTest, '-dvk-test'],
+    ]);
+
+    // Shared SNS and message handler
+    const topic = new sns.Topic(this, 'DvkBuildNotificationsTopic');
+    topic.addSubscription(new subscription.LambdaSubscription(messageHandler));
+
+    pipelineMap.forEach((pipelineId, pipelineARN) => {
+      const pipeline = codepipeline.Pipeline.fromPipelineArn(this, 'ImportedPipeline' + pipelineId, pipelineARN);
+
+      // Pipeline notification
+      const rule = new notifications.NotificationRule(this, 'NotificationRule' + pipelineId, {
+        source: pipeline,
+        events: [
+          'codepipeline-pipeline-pipeline-execution-started',
+          'codepipeline-pipeline-pipeline-execution-succeeded',
+          'codepipeline-pipeline-pipeline-execution-failed',
+        ],
+        targets: [topic],
+      });
+      console.log('Rule created', rule.notificationRuleArn);
+    });
+
+    new CfnOutput(this, 'TopicARN', {
+      value: topic.topicArn,
+      description: 'Notifications SNS topic ARN',
+      exportName: 'NotificationsTopicARN',
+    });
+  }
+}
