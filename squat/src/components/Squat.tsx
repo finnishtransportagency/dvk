0adb86e5d7f28f2f43fa25025ee1545c5535d3d4
Riku Mäkinen <riku.makinen@cgi.com>
2022-08-26 09:16:06 +0300

    DVK-77 related responsiveness tweaks.

--- squat/src/components/Squat.tsx
+++ squat/src/components/Squat.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
 import './Squat.css';
 import { IonGrid, IonRow, IonCol } from '@ionic/react';
-import { PinFormatter } from '@ionic/core/dist/types/components/range/range-interface';
 
-import i18n from '../i18n';
 import Calculations from './Calculations';
 import Vessel from './Vessel';
 import Environment from './Environment';
 import TitleBar from './TitleBar';
-
-// Initialize common formatters
-export const percentFormatter: PinFormatter = (value: number) => `${value * 100}%`;
-export const degreeFormatter: PinFormatter = (value: number) => `${value.toString().padStart(3, '0')}`;
-export const decimalFormatter: PinFormatter = (value: number) =>
-  `${value.toLocaleString(i18n.language, { minimumFractionDigits: 1, maximumFractionDigits: 2 })}`;
 
 const Squat: React.FC = () => {
   return (
@@ -22,15 +14,15 @@ const Squat: React.FC = () => {
 
       <IonGrid>
         <IonRow>
-          <IonCol size-lg="4">
+          <IonCol size="12" sizeSm="6" sizeLg="4">
             <Vessel />
           </IonCol>
 
-          <IonCol size-lg="4">
+          <IonCol size="12" sizeSm="6" sizeLg="4">
             <Environment />
           </IonCol>
 
-          <IonCol size-lg="4">
+          <IonCol size="12" sizeLg="4">
             <Calculations />
           </IonCol>
         </IonRow>
