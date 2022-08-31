9546c9de3defd4a48a3def156701fb6f47f255f3
Petri Hakala <92077410+hakalap@users.noreply.github.com>
2022-08-31 10:52:54 +0300

    Fix windows cdk problems for good (#52)

--- cdk/lib/lambda/graphql/lambdaFunctions.ts
+++ cdk/lib/lambda/graphql/lambdaFunctions.ts
@@ -2,16 +2,20 @@ import * as path from 'path';
 
 interface BackendLambda {
   entry: string;
-  fieldName?: string; // this should correlate to graphql schema field name
-  typeName?: string;
+  fieldName: string; // this should correlate to graphql schema field name
+  typeName: string;
 }
 
 const lambdaFunctions: BackendLambda[] = [
   {
     entry: path.join(__dirname, 'query/fairways-handler.ts'),
+    typeName: 'Query',
+    fieldName: 'fairways',
   },
   {
     entry: path.join(__dirname, 'query/fairway-handler.ts'),
+    typeName: 'Query',
+    fieldName: 'fairway',
   },
   {
     entry: path.join(__dirname, 'query/fairwayNavigationLines-handler.ts'),
@@ -20,6 +24,8 @@ const lambdaFunctions: BackendLambda[] = [
   },
   {
     entry: path.join(__dirname, 'query/navigationLines-handler.ts'),
+    typeName: 'Query',
+    fieldName: 'navigationLines',
   },
 ];
 
