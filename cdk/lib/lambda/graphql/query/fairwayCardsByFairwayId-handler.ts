d81fb7a6a41846a62151aceed1d1d73d64166d54
Petri Hakala <petri.hakala@cgi.com>
2022-09-14 16:37:26 +0300

    Fix generated type

--- cdk/lib/lambda/graphql/query/fairwayCardsByFairwayId-handler.ts
+++ cdk/lib/lambda/graphql/query/fairwayCardsByFairwayId-handler.ts
@@ -1,11 +1,11 @@
 import { AppSyncResolverEvent, AppSyncResolverHandler } from 'aws-lambda/trigger/appsync-resolver';
-import { FairwayCard, QueryFairwayCardByFairwayIdArgs } from '../../../../graphql/generated';
+import { FairwayCard, QueryFairwayCardsByFairwayIdArgs } from '../../../../graphql/generated';
 import { log } from '../../logger';
 import FairwayCardDBModel from '../../db/fairwayCardDBModel';
 import { mapFairwayCardDBModelToGraphqlType } from '../../db/modelMapper';
 
-export const handler: AppSyncResolverHandler<QueryFairwayCardByFairwayIdArgs, FairwayCard[]> = async (
-  event: AppSyncResolverEvent<QueryFairwayCardByFairwayIdArgs>
+export const handler: AppSyncResolverHandler<QueryFairwayCardsByFairwayIdArgs, FairwayCard[]> = async (
+  event: AppSyncResolverEvent<QueryFairwayCardsByFairwayIdArgs>
 ): Promise<FairwayCard[]> => {
   log.info(`fairwayCardsByFairwayId(${event.arguments.id})`);
   const fairwayCards = await FairwayCardDBModel.findByFairwayId(event.arguments.id);
