394c511761a1fcfa6bf4ed3d5bfaf1b5a502db96
kettuncgi <48211670+kettunju@users.noreply.github.com>
2022-10-06 19:03:24 +0300

    feat: DVK-325 aws notifikaatiot (#101)
    * add new rocket chat endpoint, change credential handling, add information to chat message
    
    * change url to codebuild details

--- cdk/lib/lambda/message-handler.ts
+++ cdk/lib/lambda/message-handler.ts
@@ -1,25 +1,49 @@
 import { SNSEvent } from 'aws-lambda';
 import axios from 'axios';
-const rocketUser = process.env.ROCKETCHAT_USER;
-const rocketPassword = process.env.ROCKETCHAT_PASSWORD;
+import { getRocketChatCredentials } from './environment';
+let credentials: { RocketchatUser: string; RocketchatPassword: string } | undefined = undefined;
+
+function getColor(result: string) {
+  switch (result) {
+    case 'SUCCEEDED':
+      return '#008000';
+    case 'FAILED':
+      return '#ff0000';
+    default:
+      return '#000000';
+  }
+}
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const handler = async function (event: SNSEvent, context: any, callback: any) {
+  if (!credentials) {
+    credentials = await getRocketChatCredentials();
+  }
+
   console.log(event);
   console.log(event.Records[0].Sns);
   const message = event.Records[0].Sns.Message;
   const details = JSON.parse(message).detail;
+  const codebuildURL = `https://eu-west-1.console.aws.amazon.com/codesuite/codepipeline/pipelines/${details.pipeline}/executions/${details['execution-id']}/timeline?region=eu-west-1`;
 
   const url = 'https://rocketchat.vaylapilvi.fi/api/v1/chat.postMessage';
   const data = {
-    channel: '#DVK_notifications',
-    text: `Pipeline state change: ${details.pipeline} ${details.state}`,
+    channel: '#DVK_notifikaatiot',
+    text: `Pipeline state change: ${details.pipeline}`,
+    attachments: [
+      {
+        title: details.state,
+        title_link: codebuildURL,
+        text: codebuildURL,
+        color: getColor(details.state),
+      },
+    ],
   };
 
   const response = await axios.post(url, data, {
     headers: {
-      'X-Auth-Token': rocketPassword || '',
-      'X-User-Id': rocketUser || '',
+      'X-Auth-Token': credentials.RocketchatPassword || '',
+      'X-User-Id': credentials.RocketchatUser || '',
       'Content-type': 'application/json',
     },
   });
