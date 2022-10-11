b7cd104d9084dacd6acac4daed1a217df7936816
Petri Hakala <petri.hakala@cgi.com>
2022-10-11 16:08:29 +0300

    Sleep longer before starting ssh tunnel so that script works also in Git Bash

--- cdk/bin/bastion.sh
+++ cdk/bin/bastion.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 aws ssm start-session --target i-0cbddb944359c553d --document-name AWS-StartPortForwardingSession --parameters "portNumber"=["22"],"localPortNumber"=["9999"] > /dev/null &
-sleep 2
+sleep 5
 ssh dvk /home/ec2-user/uptime.sh > /dev/null 2>&1
