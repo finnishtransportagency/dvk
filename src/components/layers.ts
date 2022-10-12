753a3b25ff9a8b8062740e8bd8e87196ae5e49e9
Petri Hakala <petri.hakala@cgi.com>
2022-10-12 11:56:43 +0300

    Fix pilot place styling

--- src/components/layers.ts
+++ src/components/layers.ts
@@ -2,12 +2,13 @@ import VectorSource from 'ol/source/Vector';
 import VectorLayer from 'ol/layer/Vector';
 import Style from 'ol/style/Style';
 import Stroke from 'ol/style/Stroke';
-import { Icon } from 'ol/style';
+import { Fill, Icon } from 'ol/style';
 import GeoJSON from 'ol/format/GeoJSON';
 // eslint-disable-next-line import/named
 import { FeatureLike } from 'ol/Feature';
 import Map from 'ol/Map';
 import pilot_logo from '../theme/img/pilotPlace.svg';
+import CircleStyle from 'ol/style/Circle';
 
 const url = process.env.REACT_APP_REST_API_URL ? process.env.REACT_APP_REST_API_URL + '/featureloader' : '/api/featureloader';
 
@@ -35,18 +36,26 @@ export function addPilotLayer(map: Map) {
   const image = new Icon({
     src: pilot_logo,
   });
-  const pilotStyleFunction = function () {
-    return new Style({
+  const style = [
+    new Style({
       image,
-    });
-  };
+    }),
+    new Style({
+      image: new CircleStyle({
+        radius: 10,
+        fill: new Fill({
+          color: 'rgba(0,0,0,0)',
+        }),
+      }),
+    }),
+  ];
   const pilotSource = new VectorSource({
     url: url + '?type=pilot',
     format: new GeoJSON({ featureProjection: 'EPSG:4326' }),
   });
   const pilotLayer = new VectorLayer({
     source: pilotSource,
-    style: pilotStyleFunction,
+    style,
     properties: { id: 'pilot' },
   });
   map.addLayer(pilotLayer);
