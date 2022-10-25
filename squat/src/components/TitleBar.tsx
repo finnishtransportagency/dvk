e6e18bd191daac7eba0f58b39d57cab3dd6e6b5a
kettuncgi <48211670+kettunju@users.noreply.github.com>
2022-10-25 15:25:27 +0300

    fix: DVK-327 fix titlebar responsiveness (#118)
    * some bubble gum icon swapping
    
    * install window size hook, create two layouts
    
    * change h1 media size equal to other window size logic, remove obsolete logo classes
    
    * remove window size hook
    
    * Updated language selection texts.
    
    * Styling updates for full responsiveness support.
    
    * Correct title for en locale.
    
    Co-authored-by: Riku Mäkinen <riku.makinen@cgi.com>

--- squat/src/components/TitleBar.tsx
+++ squat/src/components/TitleBar.tsx
@@ -1,91 +1,37 @@
-import React, { useState } from 'react';
-import { IonButton, IonCol, IonGrid, IonIcon, IonImg, IonItem, IonRow, IonText, IonTextarea, IonToast } from '@ionic/react';
-import { checkmarkCircleOutline, clipboardOutline, printOutline, shareSocialOutline } from 'ionicons/icons';
+import React from 'react';
+import { IonCol, IonGrid, IonImg, IonRow, IonText } from '@ionic/react';
 import { useTranslation } from 'react-i18next';
-import Modal from './Modal';
-import { copyToClipboard, createShareableLink } from '../utils/helpers';
-import { useSquatContext } from '../hooks/squatContext';
 import './TitleBar.css';
 import LanguageBar from './LanguageBar';
 import { showLanguages } from '../pages/Home';
+import PrintBar from './PrintBar';
 
 const TitleBar: React.FC = () => {
   const { t } = useTranslation('', { keyPrefix: 'homePage' });
-  const { state } = useSquatContext();
-
-  const [showCopyToast, setShowCopyToast] = useState<boolean>(false);
-
-  const handleCopyClick = () => {
-    copyToClipboard(createShareableLink(state, true));
-    setShowCopyToast(true);
-  };
 
   return (
-    <IonGrid className="titlebar">
-      <IonRow>
-        <IonCol class="ion-align-self-center">
+    <IonGrid className="titlebar ion-no-padding">
+      <IonRow className="ion-align-items-center">
+        <IonCol className="mainTitle">
           <IonText color="dark" className="equal-margin-top">
             <h1>
               <strong>{t('squat.content')}</strong>
             </h1>
           </IonText>
         </IonCol>
-        {showLanguages() && (
-          <IonCol class="ion-align-self-center" style={{ textAlign: 'end' }}>
-            <LanguageBar />
-          </IonCol>
-        )}
-        <IonCol size="auto" className="ion-align-self-center">
+        <IonCol class="ion-align-self-center mobile-logo" size="auto">
+          <IonImg className="logo" src="assets/icon/vayla_alla_fi_sv_rgb.png" alt="Väylävirasto" />
+        </IonCol>
+        <IonCol className="langbar" style={{ textAlign: 'end' }}>
+          {showLanguages() && <LanguageBar />}
+        </IonCol>
+        <IonCol size="auto">
           <IonGrid>
             <IonRow>
-              <IonCol class="ion-align-self-center">
-                <Modal
-                  title={t('header.shareable-link-title')}
-                  content={
-                    <>
-                      <p>{t('header.shareable-link-body')}</p>
-                      <IonItem lines="none">
-                        <IonItem fill="outline">
-                          <IonTextarea value={createShareableLink(state, true)} autoGrow readonly className="small-text" />
-                        </IonItem>
-                        <IonButton
-                          fill="clear"
-                          className="icon-only"
-                          onClick={() => handleCopyClick()}
-                          id="hover-trigger_"
-                          title={t('header.copy-to-clipboard')}
-                          slot="end"
-                        >
-                          <IonIcon color="primary" slot="icon-only" icon={clipboardOutline} />
-                        </IonButton>
-                      </IonItem>
-                      <IonToast
-                        isOpen={showCopyToast}
-                        onDidDismiss={() => setShowCopyToast(false)}
-                        message={t('header.copied-to-clipboard')}
-                        duration={2000}
-                        position="middle"
-                        icon={checkmarkCircleOutline}
-                      />
-                    </>
-                  }
-                  trigger={<IonIcon icon={shareSocialOutline} size="large" />}
-                  triggerTitle={t('header.shareable-link-title')}
-                />
+              <IonCol className="ion-align-self-center">
+                <PrintBar />
               </IonCol>
-              <IonCol class="ion-align-self-center">
-                <IonButton
-                  fill="clear"
-                  className="icon-only"
-                  onClick={() => window.print()}
-                  title={t('header.print')}
-                  aria-label={t('header.print')}
-                  role="button"
-                >
-                  <IonIcon color="primary" slot="icon-only" icon={printOutline} size="large" />
-                </IonButton>
-              </IonCol>
-              <IonCol class="ion-align-self-center">
+              <IonCol className="ion-align-self-center desktop-logo" size="auto">
                 <IonImg className="logo" src="assets/icon/vayla_alla_fi_sv_rgb.png" alt="Väylävirasto" />
               </IonCol>
             </IonRow>
