141282552291476d6963b530b28cdf116479711a
kettuncgi <48211670+kettunju@users.noreply.github.com>
2022-10-31 15:46:44 +0200

    feat: DVK-344 main sidemenu  (#122)
    * add app links to sidemenu, change language selections
    
    * add translations
    
    * Minor style fixes + added h1 heading.
    
    * Updated svg icons to have transparent fill.
    
    * fixes per comments
    
    * fix language choice colors
    
    * change ionrouterlinks to ion-items with link, add icons to link
    
    * ionitem lines=none
    
    * Styling + accessibility fixes.
    
    Co-authored-by: Riku Mäkinen <riku.makinen@cgi.com>

--- src/components/SidebarMenu.tsx
+++ src/components/SidebarMenu.tsx
@@ -8,59 +8,110 @@ import {
   IonGrid,
   IonRow,
   IonCol,
-  IonLabel,
   IonFooter,
   IonToolbar,
   IonTitle,
+  IonText,
+  useIonRouter,
+  IonItem,
 } from '@ionic/react';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { menuController } from '@ionic/core/components';
 import vayla_logo from '../theme/img/vayla_logo.png';
-import close from '../theme/img/close.svg';
 import './SidebarMenu.css';
-import { useFindAllFairwayCardsQuery } from '../graphql/generated';
 
 const SidebarMenu: React.FC = () => {
   const { t, i18n } = useTranslation();
-  const { data } = useFindAllFairwayCardsQuery();
-  const lang = i18n.resolvedLanguage as 'fi' | 'sv' | 'en';
+  const router = useIonRouter();
+
   return (
-    <IonMenu disabled={false} hidden={false} side="start" maxEdgeStart={24} content-id="MainContent">
+    <IonMenu disabled={false} hidden={false} side="start" maxEdgeStart={24} content-id="MainContent" className="sideBar">
       <IonContent className="sidebarMenu">
         <IonGrid className="ion-no-padding">
-          <IonRow className="header">
+          <IonRow className="header ion-align-items-end">
             <IonCol>
               <IonImg className="logo" src={vayla_logo} alt="Väylävirasto" />
             </IonCol>
-            <IonCol size="2">
-              <IonButton fill="clear" color="#000000" className="closeButton" onClick={async () => menuController.close()}>
-                <IonIcon className="otherIconLarge" icon={close} />
+            <IonCol size="auto">
+              <IonButton fill="clear" className="closeButton" onClick={async () => menuController.close()}>
+                <IonIcon className="otherIconLarge" src="/assets/icon/close_black_24dp.svg" />
               </IonButton>
             </IonCol>
           </IonRow>
           <IonRow className="title">
-            <IonCol>{t('homePage.sidebarMenu.title')}</IonCol>
+            <IonCol>
+              <IonText>
+                <h1>{t('homePage.sidebarMenu.title')}</h1>
+              </IonText>
+            </IonCol>
+          </IonRow>
+          <IonRow className="ion-direction-column">
+            <IonCol size="auto">
+              <IonItem
+                routerLink="/vaylakortit/"
+                detail={false}
+                lines="none"
+                className="ion-no-padding internal"
+                onClick={async () => menuController.close()}
+                disabled={router.routeInfo.pathname === '/vaylakortit/'}
+              >
+                <IonIcon slot="start" src="/assets/icon/fairways_icon.svg" />
+                {t('homePage.sidebarMenu.fairway-cards')}
+              </IonItem>
+            </IonCol>
+            <IonCol size="auto">
+              <IonItem
+                href="/squat/"
+                rel="external"
+                target="_blank"
+                detail={false}
+                lines="none"
+                className="ion-no-padding external"
+                onClick={async () => menuController.close()}
+              >
+                <IonIcon slot="start" src="/assets/icon/squat_icon.svg" />
+                {t('homePage.sidebarMenu.squat')}
+                <IonIcon slot="end" src="/assets/icon/ext_link.svg" />
+              </IonItem>
+            </IonCol>
           </IonRow>
           <IonRow className="languageSelection">
-            <IonCol>
-              <IonButtons>
-                <IonButton onClick={() => i18n.changeLanguage('fi')}>FI</IonButton>
-                <IonButton onClick={() => i18n.changeLanguage('en')}>EN</IonButton>
-                <IonButton onClick={() => i18n.changeLanguage('sv')}>SV</IonButton>
+            <IonCol size="12">
+              <IonButtons className="ion-justify-content-around">
+                <IonButton
+                  className="languageSelection"
+                  onClick={(e) => {
+                    i18n.changeLanguage('fi');
+                    e.preventDefault();
+                  }}
+                  disabled={i18n.language === 'fi'}
+                >
+                  Suomeksi
+                </IonButton>
+                <IonButton
+                  className="languageSelection"
+                  onClick={(e) => {
+                    i18n.changeLanguage('sv');
+                    e.preventDefault();
+                  }}
+                  disabled={i18n.language === 'sv'}
+                >
+                  På svenska
+                </IonButton>
+                <IonButton
+                  className="languageSelection"
+                  onClick={(e) => {
+                    i18n.changeLanguage('en');
+                    e.preventDefault();
+                  }}
+                  disabled={i18n.language === 'en'}
+                >
+                  In English
+                </IonButton>
               </IonButtons>
             </IonCol>
           </IonRow>
-          {/* For proxy routing testing */}
-          {data?.fairwayCards.map((fairwayCard, idx) => {
-            return (
-              <IonRow key={idx} className="fairwayCards">
-                <IonCol>
-                  <IonLabel>{fairwayCard.name[lang]}</IonLabel>
-                </IonCol>
-              </IonRow>
-            );
-          })}
         </IonGrid>
       </IonContent>
 
