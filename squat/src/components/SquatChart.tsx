fd258b7c80819ea43172e76cc2f2b7efc69d74a7
kettuncgi <48211670+kettunju@users.noreply.github.com>
2022-11-16 13:51:15 +0200

    add aria-hidden for squat chart (#162)

--- squat/src/components/SquatChart.tsx
+++ squat/src/components/SquatChart.tsx
@@ -391,7 +391,7 @@ const SquatChart: React.FC = () => {
   }, [state, width, t]);
 
   return (
-    <IonGrid>
+    <IonGrid aria-hidden="true">
       <IonRow>
         <IonCol>
           <h4 className="squatChartTitle">{t('heading')}</h4>
