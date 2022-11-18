144f8e284c86449aa253d6ba0fa1177e059072e6
Petri Hakala <petri.hakala@cgi.com>
2022-11-18 12:27:02 +0200

    Bastion script updated

--- cdk/bin/bastion.sh
+++ cdk/bin/bastion.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 aws ssm start-session --target i-0cbddb944359c553d --document-name AWS-StartPortForwardingSession --parameters "portNumber"=["22"],"localPortNumber"=["9999"] > /dev/null &
-sleep 5
+while curl localhost:9999 > /dev/null 2>&1; [ $? -ne 1 ]; do
+    sleep 1
+done
 ssh dvk -fT "while true; do uptime; sleep 30; done" > /dev/null 2>&1
