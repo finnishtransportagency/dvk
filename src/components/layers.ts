ab647ee7735e1f8db9fc24cf5c6240b7c96366e7
Petri Hakala <petri.hakala@cgi.com>
2022-11-29 09:27:43 +0200

    DVK-346: Restriction area max resolution update

--- src/components/layers.ts
+++ src/components/layers.ts
@@ -241,7 +241,7 @@ export function addAPILayers(map: Map) {
   addFeatureLayer(map, 'area3456', 30, 1, getAreaStyle('#207A43', 1, 'rgba(32, 122, 67, 0.1)'));
 
   // Nopeusrajoitus
-  addFeatureLayer(map, 'restrictionarea', 10, 2, getLineStyle('purple', 2));
+  addFeatureLayer(map, 'restrictionarea', 15, 2, getLineStyle('purple', 2));
   // Ankkurointialue, Kohtaamis- ja ohittamiskieltoalue
   addFeatureLayer(map, 'specialarea', 30, 2, (feature) => getSpecialAreaStyle(feature, '#C57A11', 2));
   // Turvalaitteet
