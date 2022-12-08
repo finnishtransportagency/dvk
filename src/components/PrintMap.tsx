4afdc4539308f5482672f47deed204c26563166c
Riku Mäkinen <riku.makinen@cgi.com>
2022-12-08 12:23:37 +0200

    Revert duplicate checks.

--- src/components/PrintMap.tsx
+++ src/components/PrintMap.tsx
@@ -19,60 +19,52 @@ const PrintMap: React.FC<FairwayCardProps> = ({ name, modified, isN2000 }) => {
   const { t, i18n } = useTranslation(undefined, { keyPrefix: 'fairwayCards' });
   const lang = i18n.resolvedLanguage as Lang;
 
-  const isMapExportInDOM = !!document.getElementById('mapExport');
-
   const debouncedPrintImageRefresh = React.useRef(
     debounce(() => {
       refreshPrintableMap();
     }, 500)
   ).current;
 
-  if (!isMapExportInDOM) {
-    dvkMap.olMap?.on('moveend', () => {
-      debouncedPrintImageRefresh();
-    });
-    dvkMap.olMap?.on('loadend', () => {
-      debouncedPrintImageRefresh();
-    });
+  dvkMap.olMap?.on('moveend', () => {
+    debouncedPrintImageRefresh();
+  });
+  dvkMap.olMap?.on('loadend', () => {
+    debouncedPrintImageRefresh();
+  });
 
-    dvkMap.olMap?.once('rendercomplete', () => {
-      debouncedPrintImageRefresh();
-    });
-  }
+  dvkMap.olMap?.once('rendercomplete', () => {
+    debouncedPrintImageRefresh();
+  });
 
   return (
     <>
-      {!isMapExportInDOM && (
-        <>
-          <div className="mapWrapper">
-            <div className="mapContent">
-              <div id="mapExport"></div>
-              <div className="mapLegend">
-                <div className="bg"></div>
-                <div id="compassInfo">
-                  <img id="compassNeedle" src={north_arrow} alt="" />
-                </div>
-                <div className="cardInfo">
-                  <IonText>
-                    <h3 id="exportFairwayName">{name ? name[lang] : t('documentTitle')}</h3>
-                  </IonText>
-                  {modified && (
-                    <em>
-                      {t('modified')}{' '}
-                      {t('modifiedDate', {
-                        val: modified ? new Date(modified * 1000) : '-',
-                      })}
-                      {isN2000 ? ' - N2000 (BSCD2000)' : ' - MW'}
-                    </em>
-                  )}
-                  <em className="danger">{t('notForNavigation')}</em>
-                  <div id="mapScale"></div>
-                </div>
-              </div>
+      <div className="mapWrapper">
+        <div className="mapContent">
+          <div id="mapExport"></div>
+          <div className="mapLegend">
+            <div className="bg"></div>
+            <div id="compassInfo">
+              <img id="compassNeedle" src={north_arrow} alt="" />
+            </div>
+            <div className="cardInfo">
+              <IonText>
+                <h3 id="exportFairwayName">{name ? name[lang] : t('documentTitle')}</h3>
+              </IonText>
+              {modified && (
+                <em>
+                  {t('modified')}{' '}
+                  {t('modifiedDate', {
+                    val: modified ? new Date(modified * 1000) : '-',
+                  })}
+                  {isN2000 ? ' - N2000 (BSCD2000)' : ' - MW'}
+                </em>
+              )}
+              <em className="danger">{t('notForNavigation')}</em>
+              <div id="mapScale"></div>
             </div>
           </div>
-        </>
-      )}
+        </div>
+      </div>
     </>
   );
 };
