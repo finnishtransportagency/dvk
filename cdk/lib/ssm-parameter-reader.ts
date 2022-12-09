791871248d2526f6ccc5fd2825b5e113907e6e7f
kettunenj <juhani.kettunen@cgi.com>
2022-12-09 13:31:32 +0200

    add cloudfront wafconfig

---
+++ cdk/lib/ssm-parameter-reader.ts
@@ -0,0 +1,32 @@
+import { AwsCustomResource, AwsCustomResourcePolicy, PhysicalResourceId } from 'aws-cdk-lib/custom-resources';
+import { Construct } from 'constructs';
+
+interface SSMParameterReaderProps {
+  parameterName: string;
+  region: string;
+}
+
+export class SSMParameterReader extends AwsCustomResource {
+  constructor(scope: Construct, name: string, props: SSMParameterReaderProps) {
+    const { parameterName, region } = props;
+
+    super(scope, name, {
+      onUpdate: {
+        action: 'getParameter',
+        service: 'SSM',
+        parameters: {
+          Name: parameterName,
+        },
+        region,
+        physicalResourceId: PhysicalResourceId.of(name),
+      },
+      policy: AwsCustomResourcePolicy.fromSdkCalls({
+        resources: AwsCustomResourcePolicy.ANY_RESOURCE,
+      }),
+    });
+  }
+
+  public getParameterValue(): string {
+    return this.getResponseFieldReference('Parameter.Value').toString();
+  }
+}
