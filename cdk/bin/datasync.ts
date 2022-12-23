6e09e55deb33983a24c71fc045f4e9324a95af56
Petri Hakala <petri.hakala@cgi.com>
2022-12-23 10:36:41 +0200

    DVK-506: Inkoo fairway card update + updating only selection of cards

--- cdk/bin/datasync.ts
+++ cdk/bin/datasync.ts
@@ -5,12 +5,8 @@ import { getDynamoDBDocumentClient } from '../lib/lambda/db/dynamoClient';
 import path from 'path';
 import fs from 'fs';
 import FairwayCardDBModel from '../lib/lambda/db/fairwayCardDBModel';
-// eslint-disable-next-line import/named
-import { PutObjectCommand, PutObjectCommandOutput, S3Client } from '@aws-sdk/client-s3';
 import { mapFairwayIds } from '../lib/lambda/db/modelMapper';
 import HarborDBModel from '../lib/lambda/db/harborDBModel';
-
-const s3Client = new S3Client({ region: 'eu-west-1' });
 
 function getAllFiles(dirPath: string, arrayOfFiles: string[]) {
   const files = fs.readdirSync(dirPath);
@@ -25,66 +21,11 @@ function getAllFiles(dirPath: string, arrayOfFiles: string[]) {
   return arrayOfFiles.filter((f) => !f.endsWith('template.json'));
 }
 
-function getGeoTiffMap(arrayOfFiles: string[]) {
-  const geoTiffMap = new Map<number, string[]>();
-  for (const file of arrayOfFiles.filter((f) => f.endsWith('.tif'))) {
-    const id = file.match(/\/\d*\//)?.toString();
-    if (id) {
-      const fairwayId = parseInt(id.substring(1, id.length - 1), 10);
-      if (!geoTiffMap.get(fairwayId)) {
-        geoTiffMap.set(fairwayId, []);
-      }
-      geoTiffMap.get(fairwayId)?.push(file);
-    }
-  }
-  return geoTiffMap;
-}
-
-function isDbOnly(): boolean {
-  return process.argv.includes('--dbonly');
-}
-
-function getBucketName() {
-  if (Config.isProductionEnvironment()) {
-    return `geotiff.dvk.vaylapilvi.fi`;
-  } else {
-    return `geotiff.dvk${Config.getEnvironment()}.testivaylapilvi.fi`;
-  }
-}
-
-function processGeoTiff(filepath: string, id: number, s3Outputs: Promise<PutObjectCommandOutput>[]): string {
-  const i = filepath.lastIndexOf('/');
-  const filename = filepath.substring(i + 1);
-  const command = new PutObjectCommand({
-    Key: `${id}/${filename}`,
-    Bucket: getBucketName(),
-    Body: fs.readFileSync(filepath),
-  });
-  if (!isDbOnly()) {
-    const s3Response = s3Client.send(command);
-    s3Outputs.push(s3Response);
-  }
-  return filename;
-}
-
-async function processCard(file: string, geoTiffMap: Map<number, string[]>): Promise<FairwayCardDBModel> {
+async function processCard(file: string): Promise<FairwayCardDBModel> {
   const fairwayCard = JSON.parse(fs.readFileSync(file).toString()) as FairwayCardDBModel;
   fairwayCard.fairwayIds = mapFairwayIds(fairwayCard);
   fairwayCard.modificationTimestamp = Math.round(Date.now() / 1000);
-  const s3Outputs: Promise<PutObjectCommandOutput>[] = [];
   console.log(`Fairway card: ${fairwayCard.name?.fi}`);
-  for (const fairway of fairwayCard.fairways) {
-    console.log(`Fairway: ${fairway.id}`);
-    if (geoTiffMap.get(fairway.id)) {
-      fairway.geotiffImages = geoTiffMap.get(fairway.id)?.map((f) => {
-        return processGeoTiff(f, fairway.id, s3Outputs);
-      });
-    }
-  }
-  if (!isDbOnly()) {
-    await Promise.all(s3Outputs);
-    console.log(`${s3Outputs.length || 0} image(s) uploaded`);
-  }
   return fairwayCard;
 }
 
@@ -101,9 +42,10 @@ function getRootDirectory(dir: string): string {
 async function updateTable(tableName: string, directoryPath: string, card: boolean) {
   console.log(`Scanning directory: ${directoryPath}`);
   const arrayOfFiles = getAllFiles(directoryPath, []);
-  const geoTiffMap = getGeoTiffMap(arrayOfFiles);
-  for (const file of arrayOfFiles.filter((f) => f.endsWith('.json'))) {
-    const item = card ? await processCard(file, geoTiffMap) : processHarbor(file);
+  for (const file of arrayOfFiles.filter(
+    (f) => f.endsWith('.json') && (process.argv.length === 2 || process.argv.slice(2).some((arg) => f.indexOf(arg) >= 0))
+  )) {
+    const item = card ? await processCard(file) : processHarbor(file);
     await getDynamoDBDocumentClient().send(new PutCommand({ TableName: tableName, Item: item }));
   }
 }
