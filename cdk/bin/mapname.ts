752e8285f7ef0b2a6380035e4dd2a80cdc6b8030
Petri Hakala <petri.hakala@cgi.com>
2023-01-25 18:00:00 +0200

    Name generator added

---
+++ cdk/bin/mapname.ts
@@ -0,0 +1,42 @@
+import fs from 'fs';
+import { Feature, FeatureCollection, Point } from 'geojson';
+import { roundGeometry } from '../lib/lambda/util';
+import proj4 from 'proj4';
+
+async function main() {
+  proj4.defs('EPSG:4326', '+proj=longlat +datum=WGS84 +no_defs');
+  proj4.defs('EPSG:3067', '+proj=utm +zone=35 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs');
+  if (process.argv.length > 2) {
+    const features: Feature[] = [];
+    let i = 0;
+    for (const file of process.argv.slice(2)) {
+      const names = JSON.parse(fs.readFileSync(file).toString()) as FeatureCollection;
+      for (const feature of names.features) {
+        if (feature.geometry.type === 'Point') {
+          const coordinates = proj4('EPSG:4326', 'EPSG:3067').forward(feature.geometry.coordinates);
+          const geometry = { type: 'Point', coordinates: coordinates } as Point;
+          roundGeometry(geometry);
+          features.push({
+            id: ++i,
+            type: 'Feature',
+            geometry,
+            properties: {
+              name: {
+                fi: feature.properties?.AHTI_NAMFI,
+                sv: feature.properties?.AHTI_NAMSV,
+                priority: Number.parseInt(feature.properties?.AHTI_PRIOR, 10),
+              },
+            },
+          });
+        }
+      }
+    }
+    const collection: FeatureCollection = { type: 'FeatureCollection', features };
+    fs.writeFileSync('bin/data/names.json', JSON.stringify(collection, undefined, 2) + '\n');
+  }
+}
+
+main().catch((e) => {
+  console.log(e);
+  process.exit(1);
+});
