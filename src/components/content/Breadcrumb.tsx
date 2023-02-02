e4b5039dc12eafc0a93c4a4de7852083c72fc6d8
kettunenj <juhani.kettunen@cgi.com>
2023-02-02 09:26:12 +0200

    add onlick handler to breadcrumb pathitems as alternative to normal routing

--- src/components/content/Breadcrumb.tsx
+++ src/components/content/Breadcrumb.tsx
@@ -2,12 +2,13 @@ import { IonLabel, IonBreadcrumbs, IonBreadcrumb } from '@ionic/react';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
-type PathItem = {
+export type PathItem = {
   title?: string;
   route?: string;
+  onClick?: () => void;
 };
 
-type BreadcrumbProps = {
+export type BreadcrumbProps = {
   path: PathItem[];
 };
 
@@ -21,7 +22,12 @@ const Breadcrumb: React.FC<BreadcrumbProps> = ({ path }) => {
         <IonLabel slot="separator">&gt;</IonLabel>
       </IonBreadcrumb>
       {path.map((item, idx) => (
-        <IonBreadcrumb key={item.route || item.title} routerLink={idx < path.length - 1 ? item.route : undefined}>
+        <IonBreadcrumb
+          key={item.route || item.title}
+          routerLink={!!item.onClick || idx < path.length - 1 ? item.route : undefined}
+          onClick={item.onClick}
+          className={item.onClick ? 'ion-activatable ion-focusable' : undefined}
+        >
           {idx < path.length - 1 && (
             <>
               {item.title}
