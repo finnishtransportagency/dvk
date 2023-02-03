16756247cc813fcb9b113e212e429274b786ee26
Petri Hakala <petri.hakala@cgi.com>
2023-02-03 15:57:18 +0200

    DVK-615: Zero decimals from names and Vuosaaren satama ignored

--- cdk/lib/lambda/util.ts
+++ cdk/lib/lambda/util.ts
@@ -1,45 +1,45 @@
 import { Geometry, Position } from 'geojson';
 
 const GEOMETRY_DECIMALS = 5;
-const power = Math.pow(10, GEOMETRY_DECIMALS);
 
-function transform(coordinates: number[]) {
+function transform(coordinates: number[], power: number) {
   for (let i = 0, ii = coordinates.length; i < ii; ++i) {
     coordinates[i] = Math.round(coordinates[i] * power) / power;
   }
   return coordinates;
 }
 
-function transformArray(coordinates: Position[]) {
+function transformArray(coordinates: Position[], power: number) {
   for (const position of coordinates) {
-    transform(position);
+    transform(position, power);
   }
 }
 
-function transformArray2(coordinates: Position[][]) {
+function transformArray2(coordinates: Position[][], power: number) {
   for (const positionArray of coordinates) {
-    transformArray(positionArray);
+    transformArray(positionArray, power);
   }
 }
 
-function transformArray3(coordinates: Position[][][]) {
+function transformArray3(coordinates: Position[][][], power: number) {
   for (const positionArray of coordinates) {
-    transformArray2(positionArray);
+    transformArray2(positionArray, power);
   }
 }
 
-export function roundGeometry(geometry: Geometry) {
+export function roundGeometry(geometry: Geometry, decimals = GEOMETRY_DECIMALS) {
+  const power = Math.pow(10, decimals);
   if (geometry.type === 'Point') {
-    transform(geometry.coordinates);
+    transform(geometry.coordinates, power);
   } else if (geometry.type === 'LineString' || geometry.type === 'MultiPoint') {
-    transformArray(geometry.coordinates);
+    transformArray(geometry.coordinates, power);
   } else if (geometry.type === 'Polygon' || geometry.type === 'MultiLineString') {
-    transformArray2(geometry.coordinates);
+    transformArray2(geometry.coordinates, power);
   } else if (geometry.type === 'MultiPolygon') {
-    transformArray3(geometry.coordinates);
+    transformArray3(geometry.coordinates, power);
   } else {
     for (const geom of geometry.geometries) {
-      roundGeometry(geom);
+      roundGeometry(geom, decimals);
     }
   }
   return geometry;
