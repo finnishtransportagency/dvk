5464980c34d1ac474159c35adc0e32e9a7c47cca
Riku Mäkinen <riku.makinen@cgi.com>
2023-02-17 11:31:28 +0200

    Add closeButton adjacent to breadcrumbs.

--- src/components/content/Breadcrumb.tsx
+++ src/components/content/Breadcrumb.tsx
@@ -1,6 +1,7 @@
-import { IonLabel, IonBreadcrumbs, IonBreadcrumb } from '@ionic/react';
+import { IonLabel, IonBreadcrumbs, IonBreadcrumb, IonGrid, IonRow, IonCol, IonButton, IonIcon } from '@ionic/react';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { useHistory } from 'react-router';
 
 type PathItem = {
   title?: string;
@@ -14,30 +15,46 @@ type BreadcrumbProps = {
 
 const Breadcrumb: React.FC<BreadcrumbProps> = ({ path }) => {
   const { t } = useTranslation(undefined, { keyPrefix: 'common' });
+  const history = useHistory();
+
+  const backToHome = () => {
+    history.push('/');
+  };
 
   return (
-    <IonBreadcrumbs>
-      <IonBreadcrumb routerLink="/">
-        {t('home')}
-        <IonLabel slot="separator">&gt;</IonLabel>
-      </IonBreadcrumb>
-      {path.map((item, idx) => (
-        <IonBreadcrumb
-          key={item.route || item.title}
-          routerLink={idx < path.length - 1 ? item.route : undefined}
-          onClick={item.onClick}
-          className={item.onClick ? 'ion-activatable ion-focusable' : undefined}
-        >
-          {idx < path.length - 1 && (
-            <>
-              {item.title}
+    <IonGrid className="ion-no-padding breadcrumbs">
+      <IonRow className="ion-align-items-center">
+        <IonCol>
+          <IonBreadcrumbs>
+            <IonBreadcrumb routerLink="/">
+              {t('home')}
               <IonLabel slot="separator">&gt;</IonLabel>
-            </>
-          )}
-          {idx === path.length - 1 && <strong>{item.title}</strong>}
-        </IonBreadcrumb>
-      ))}
-    </IonBreadcrumbs>
+            </IonBreadcrumb>
+            {path.map((item, idx) => (
+              <IonBreadcrumb
+                key={item.route || item.title}
+                routerLink={idx < path.length - 1 ? item.route : undefined}
+                onClick={item.onClick}
+                className={item.onClick ? 'ion-activatable ion-focusable' : undefined}
+              >
+                {idx < path.length - 1 && (
+                  <>
+                    {item.title}
+                    <IonLabel slot="separator">&gt;</IonLabel>
+                  </>
+                )}
+                {idx === path.length - 1 && <strong>{item.title}</strong>}
+              </IonBreadcrumb>
+            ))}
+          </IonBreadcrumbs>
+        </IonCol>
+        <IonCol size="auto">
+          <IonButton fill="clear" className="closeButton" title={t('closePane')} aria-label={t('closePane')} onClick={() => backToHome()}>
+            <IonIcon className="otherIconLarge" src="/assets/icon/close_black_24dp.svg" />
+          </IonButton>
+        </IonCol>
+      </IonRow>
+    </IonGrid>
   );
 };
 
