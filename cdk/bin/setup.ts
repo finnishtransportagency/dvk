6e63d42778ede01141f16b7ca83796449b8aa277
Petri Hakala <petri.hakala@cgi.com>
2023-02-24 16:47:19 +0200

    remove console log

--- cdk/bin/setup.ts
+++ cdk/bin/setup.ts
@@ -65,7 +65,6 @@ function writeEnvFile(fileName: string, variables: { [p: string]: string }) {
       envFile += `${key}=${variables[key]}\n`;
     }
   }
-  console.log('envFile: ' + envFile);
   fs.writeFileSync(fileName, envFile);
 }
 
