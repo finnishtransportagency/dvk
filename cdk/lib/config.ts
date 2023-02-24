152bf6732bb72a862a7d1eed5b7402477bf9d606
Petri Hakala <petri.hakala@cgi.com>
2023-02-24 14:45:08 +0200

    Last fix

--- cdk/lib/config.ts
+++ cdk/lib/config.ts
@@ -19,7 +19,7 @@ class Config {
   }
 
   static isFeatureEnvironment() {
-    return 'feature' === Config.getEnvironment();
+    return Config.getEnvironment().startsWith('feature');
   }
 
   static isDeveloperEnvironment(env?: string) {
