56204addb9ac2a3ba4b199fd4e9b83abf6f9b4ca
Riku Mäkinen <riku.makinen@cgi.com>
2023-02-24 13:18:48 +0200

    Set up ionic and headerBar.

---
+++ admin/src/components/LanguageBar.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { IonButton, IonCol, IonGrid, IonRow } from '@ionic/react';
+import { useTranslation } from 'react-i18next';
+
+const LanguageBar: React.FC = () => {
+  const { i18n } = useTranslation();
+  const changeLanguage = (lang: string) => {
+    i18n.changeLanguage(lang);
+  };
+
+  return (
+    <IonGrid className="languageBar">
+      <IonRow>
+        <IonCol>
+          <IonButton
+            id="language_fi"
+            fill="clear"
+            className="plainButton"
+            onClick={(e) => {
+              changeLanguage('fi');
+              e.preventDefault();
+            }}
+            disabled={i18n.language === 'fi'}
+          >
+            Suomeksi
+          </IonButton>
+          <IonButton
+            id="language_sv"
+            fill="clear"
+            className="plainButton"
+            onClick={(e) => {
+              changeLanguage('sv');
+              e.preventDefault();
+            }}
+            disabled={i18n.language === 'sv'}
+          >
+            På svenska
+          </IonButton>
+          <IonButton
+            id="language_en"
+            fill="clear"
+            className="plainButton"
+            onClick={(e) => {
+              changeLanguage('en');
+              e.preventDefault();
+            }}
+            disabled={i18n.language === 'en'}
+          >
+            In English
+          </IonButton>
+        </IonCol>
+      </IonRow>
+    </IonGrid>
+  );
+};
+
+export default LanguageBar;
