5e116b74e6d394d12e904ba8743402cf6ffb88ea
Riku Mäkinen <riku.makinen@cgi.com>
2023-03-13 13:15:02 +0200

    Update api.

--- admin/src/graphql/api.ts
+++ admin/src/graphql/api.ts
@@ -1,4 +1,4 @@
-import { useCurrentUserQuery, useFairwayCardsAndHarborsQuery } from './generated';
+import { useCurrentUserQuery, useFairwayCardByIdQuery, useFairwayCardsAndHarborsQuery, useFairwaysQuery, useHarborsQuery } from './generated';
 
 const datasourceClient = {
   endpoint: process.env.REACT_APP_API_URL ? process.env.REACT_APP_API_URL : '/yllapito/graphql',
@@ -15,4 +15,16 @@ export function useCurrentUserQueryData() {
 
 export function useFairwayCardsAndHarborsQueryData() {
   return useFairwayCardsAndHarborsQuery(datasourceClient);
+}
+
+export function useFairwayCardByIdQueryData(id: string, refetchOnWindowFocus?: boolean) {
+  return useFairwayCardByIdQuery(datasourceClient, { id }, { refetchOnWindowFocus: refetchOnWindowFocus });
+}
+
+export function useFairwaysQueryData() {
+  return useFairwaysQuery(datasourceClient);
+}
+
+export function useHarboursQueryData() {
+  return useHarborsQuery(datasourceClient);
 }
