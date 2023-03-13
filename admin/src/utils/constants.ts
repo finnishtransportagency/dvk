54f392d9b487a2f120b84bbf9345403471d2a587
Riku Mäkinen <riku.makinen@cgi.com>
2023-03-13 13:18:37 +0200

    Add constants for state actions.

--- admin/src/utils/constants.ts
+++ admin/src/utils/constants.ts
@@ -1,3 +1,6 @@
 export type Lang = 'fi' | 'sv' | 'en';
 
 export type ItemType = '' | 'CARD' | 'HARBOR';
+
+export type ActionType = 'nameFi' | 'nameSv' | 'nameEn' | 'primaryId';
+export type ActionTypeSelect = 'fairwayIds' | 'fairwayPrimary' | 'fairwaySecondary' | 'harbours' | 'status' | 'referenceLevel';
