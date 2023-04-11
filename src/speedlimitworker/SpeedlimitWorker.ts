d5beb4a4b6635440c11149cdd99aff5552ce752f
Aki Hiisilä <aki.hiisila@cgi.com>
2023-04-11 12:21:43 +0300

    Clean up speedlimitworker code

--- src/speedlimitworker/SpeedlimitWorker.ts
+++ src/speedlimitworker/SpeedlimitWorker.ts
@@ -5,24 +5,18 @@ import { register } from 'ol/proj/proj4';
 import { getSpeedLimitFeatures } from './SpeedlimitUtils';
 
 function initOLProj4() {
-  proj4.defs('EPSG:4258', '+proj=longlat +ellps=GRS80 +no_defs +type=crs');
   proj4.defs('EPSG:4326', '+proj=longlat +datum=WGS84 +no_defs');
-  proj4.defs('EPSG:3395', '+proj=merc +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs');
   proj4.defs(MAP.EPSG, '+proj=utm +zone=35 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs');
   register(proj4);
 }
 
 onmessage = (e: MessageEvent<{ raData: string; aData: string }>) => {
   initOLProj4();
-  const data = e.data;
-  const raData = JSON.parse(data.raData);
-  const aData = JSON.parse(data.aData);
+  const raData = JSON.parse(e.data.raData);
+  const aData = JSON.parse(e.data.aData);
   const format = new GeoJSON();
-  const afs = format.readFeatures(aData);
-  const rafs = format.readFeatures(raData);
-  const speedLimitFeatures2 = getSpeedLimitFeatures(rafs, afs);
-  const slFeaturesString = format.writeFeatures(speedLimitFeatures2);
-  postMessage(slFeaturesString);
+  const speedLimitFeatures = getSpeedLimitFeatures(format.readFeatures(raData), format.readFeatures(aData));
+  postMessage(format.writeFeatures(speedLimitFeatures));
 };
 
 export {};
