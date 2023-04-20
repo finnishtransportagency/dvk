8914dd556a9301fe1869b8a8edf8ea61f1b35794
Petri Hakala <petri.hakala@cgi.com>
2023-04-20 16:54:48 +0300

    Fix cache issue

--- src/utils/dataLoader.ts
+++ src/utils/dataLoader.ts
@@ -23,7 +23,7 @@ export function useFeatureData(featureDataId: FeatureDataId, refetchOnMount: 'al
   });
   return {
     ...response,
-    data: response.data?.data,
+    data: response.data?.data ? response.data?.data : response.data,
     dataUpdatedAt: response.data?.lastModified ? Date.parse(response.data?.lastModified) : response.dataUpdatedAt,
   };
 }
