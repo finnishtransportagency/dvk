fe3ac139a2b356cfe22036d2049d1958a40c19ca
Petri Hakala <92077410+hakalap@users.noreply.github.com>
2023-04-24 10:01:20 +0300

    DVK-853: Cache all backend api responses (#465)

--- cdk/lib/lambda/graphql/query/marineWarnings-handler.ts
+++ cdk/lib/lambda/graphql/query/marineWarnings-handler.ts
@@ -1,27 +1,46 @@
 import { MarineWarning } from '../../../../graphql/generated';
 import { fetchMarineWarnings, parseDateTimes } from '../../api/pooki';
+import { log } from '../../logger';
+import { cacheResponse, getFromCache } from '../cache';
+
+function getKey() {
+  return 'marinewarning-graphql';
+}
 
 export const handler = async (): Promise<MarineWarning[]> => {
-  const resp = await fetchMarineWarnings();
-  const warnings = [];
-  for (const feature of resp.features) {
-    const dates = parseDateTimes(feature);
-    const warning: MarineWarning = {
-      id: feature.properties?.ID,
-      number: feature.properties?.NUMERO || 0,
-      area: { fi: feature.properties?.ALUEET_FI, sv: feature.properties?.ALUEET_SV, en: feature.properties?.ALUEET_EN },
-      type: { fi: feature.properties?.TYYPPI_FI, sv: feature.properties?.TYYPPI_SV, en: feature.properties?.TYYPPI_EN },
-      location: { fi: feature.properties?.SIJAINTI_FI, sv: feature.properties?.SIJAINTI_SV, en: feature.properties?.SIJAINTI_EN },
-      description: { fi: feature.properties?.SISALTO_FI, sv: feature.properties?.SISALTO_SV, en: feature.properties?.SISALTO_EN },
-      startDateTime: dates.startDateTime,
-      endDateTime: dates.endDateTime,
-      dateTime: dates.dateTime || 0,
-      notifier: feature.properties?.TIEDOKSIANTAJA,
-      equipmentId: Number(feature.properties?.TURVALAITE_TXT?.match(/\d.*/)[0]),
-      lineId: Number(feature.properties?.NAVIGOINTILINJA_TXT?.match(/\d.*/)[0]),
-      areaId: Number(feature.properties?.VAYLAALUE_TXT?.match(/\d.*/)[0]),
-    };
-    warnings.push(warning);
+  const key = getKey();
+  try {
+    const resp = await fetchMarineWarnings();
+    const warnings = [];
+    for (const feature of resp.features) {
+      const dates = parseDateTimes(feature);
+      const warning: MarineWarning = {
+        id: feature.properties?.ID,
+        number: feature.properties?.NUMERO || 0,
+        area: { fi: feature.properties?.ALUEET_FI, sv: feature.properties?.ALUEET_SV, en: feature.properties?.ALUEET_EN },
+        type: { fi: feature.properties?.TYYPPI_FI, sv: feature.properties?.TYYPPI_SV, en: feature.properties?.TYYPPI_EN },
+        location: { fi: feature.properties?.SIJAINTI_FI, sv: feature.properties?.SIJAINTI_SV, en: feature.properties?.SIJAINTI_EN },
+        description: { fi: feature.properties?.SISALTO_FI, sv: feature.properties?.SISALTO_SV, en: feature.properties?.SISALTO_EN },
+        startDateTime: dates.startDateTime,
+        endDateTime: dates.endDateTime,
+        dateTime: dates.dateTime || 0,
+        notifier: feature.properties?.TIEDOKSIANTAJA,
+        equipmentId: Number(feature.properties?.TURVALAITE_TXT?.match(/\d.*/)[0]),
+        lineId: Number(feature.properties?.NAVIGOINTILINJA_TXT?.match(/\d.*/)[0]),
+        areaId: Number(feature.properties?.VAYLAALUE_TXT?.match(/\d.*/)[0]),
+      };
+      warnings.push(warning);
+    }
+    await cacheResponse(key, warnings);
+    return warnings;
+  } catch (e) {
+    log.error('Getting marine warnings failed: %s', e);
+    const cacheResponseData = await getFromCache(key);
+    if (cacheResponseData.data) {
+      log.warn('Returning expired response from s3 cache');
+      return JSON.parse(cacheResponseData.data);
+    } else {
+      throw e;
+    }
   }
-  return warnings;
 };
