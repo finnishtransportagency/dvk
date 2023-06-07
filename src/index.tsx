955d9bba2f31722c9eaf4b7b8373f88333255614
kettunenj <juhani.kettunen@cgi.com>
2023-06-07 12:43:53 +0300

    add language detection from url params

--- src/index.tsx
+++ src/index.tsx
@@ -4,22 +4,56 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import './i18n';
+import i18next, { changeLanguage } from 'i18next';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement as Element);
 
-root.render(<App />);
+export function getUrlParam(param: string) {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(param);
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register({
-  onUpdate: (registration: ServiceWorkerRegistration) => {
-    registration.waiting?.postMessage({ type: 'SKIP_WAITING' });
-  },
-});
+(async () => {
+  const paramLang = getUrlParam('lang');
+  if (paramLang === 'sv') {
+    await changeLanguage('sv');
+  } else if (paramLang === 'en') {
+    await changeLanguage('en');
+  } else {
+    await changeLanguage('fi');
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  i18next.on('languageChanged', (lang: string) => {
+    const currentLang = getUrlParam('lang');
+    if (!currentLang) {
+      window.history.replaceState(
+        null,
+        '',
+        'index.html' + (window.location.search ? window.location.search + '&lang=' + lang : '?lang=' + lang) + window.location.hash
+      );
+    } else {
+      window.history.replaceState(
+        null,
+        '',
+        'index.html' + window.location.search.replace('lang=' + currentLang, 'lang=' + lang) + window.location.hash
+      );
+    }
+  });
+
+  root.render(<App />);
+
+  // If you want your app to work offline and load faster, you can change
+  // unregister() to register() below. Note this comes with some pitfalls.
+  // Learn more about service workers: https://cra.link/PWA
+  serviceWorkerRegistration.register({
+    onUpdate: (registration: ServiceWorkerRegistration) => {
+      registration.waiting?.postMessage({ type: 'SKIP_WAITING' });
+    },
+  });
+
+  // If you want to start measuring performance in your app, pass a function
+  // to log results (for example: reportWebVitals(console.log))
+  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  reportWebVitals();
+})();
