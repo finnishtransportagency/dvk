8b6d8601c4a19f95953f568d9da5e7f617baf013
Petri Hakala <petri.hakala@cgi.com>
2023-06-09 14:54:23 +0300

    Use new bastion host without ssh keys

--- cdk/bin/bastion.sh
+++ cdk/bin/bastion.sh
@@ -7,7 +7,7 @@ else
     ALB=`aws cloudformation list-exports --output json 2> /dev/null | grep DvkALB-$ENVIRONMENT | awk -F \" '{print $4}'`
     if [ "$ALB" != "" ]
     then
-        aws ssm start-session --target i-0cbddb944359c553d --document-name AWS-StartPortForwardingSessionToRemoteHost --parameters "host=$ALB,portNumber"=["80"],"localPortNumber"=["$PORT"] > /dev/null &
+        aws ssm start-session --target i-07f225266686f7c65 --document-name AWS-StartPortForwardingSessionToRemoteHost --parameters "host=$ALB,portNumber"=["80"],"localPortNumber"=["$PORT"] > /dev/null &
         echo "Port forwarding to $ALB started on port $PORT"
     else
         echo "Make sure you have logged in your aws account or environment $ENVIRONMENT exists."
