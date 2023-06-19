6d1e4f2f7891e3a9da99f2cdf50b8796ef9d2b1d
Petri Hakala <petri.hakala@cgi.com>
2023-06-19 16:37:41 +0300

    Lambda added

--- cdk/lib/lambda/graphql/mutation/uploadPicture-handler.ts
+++ cdk/lib/lambda/graphql/mutation/uploadPicture-handler.ts
@@ -9,22 +9,28 @@ const s3Client = new S3Client({ region: 'eu-west-1' });
 
 async function savePictures(picture: PictureUploadInput) {
   const bucketName = getNewStaticBucketName();
+  const key = `${picture.cardId}/${picture.id}`;
   const command = new PutObjectCommand({
-    Key: `${picture.cardId}/${picture.id}`,
+    Key: key,
     Bucket: bucketName,
     Body: Buffer.from(picture.base64Data, 'base64'),
     Tagging: 'InUse=false',
   });
   await s3Client.send(command);
-  log.debug('picture saved to %s', bucketName);
+  log.debug('picture %s saved to %s', key, bucketName);
 }
 
-export const handler: AppSyncResolverHandler<MutationUploadPictureArgs, number> = async (
+export const handler: AppSyncResolverHandler<MutationUploadPictureArgs, boolean> = async (
   event: AppSyncResolverEvent<MutationUploadPictureArgs>
-): Promise<number> => {
+): Promise<boolean> => {
   const user = await getCurrentUser(event);
   log.info(`uploadPicture(${event.arguments.picture.id}, ${user.uid})`);
-  await savePictures(event.arguments.picture);
-  auditLog.info({ cardId: event.arguments.picture.cardId, id: event.arguments.picture.id, user: user.uid }, 'Picture uploaded');
-  return Date.now();
+  try {
+    await savePictures(event.arguments.picture);
+    auditLog.info({ cardId: event.arguments.picture.cardId, id: event.arguments.picture.id, user: user.uid }, 'Picture uploaded');
+    return true;
+  } catch (e) {
+    log.error('Uploading picture failed: %s', e);
+    return false;
+  }
 };
