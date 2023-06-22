1efd6e37fd09947a16bd8cfa7266f5772001c2c8
Riku Mäkinen <riku.makinen@cgi.com>
2023-06-22 15:02:50 +0300

    Copy layerStyles.

---
+++ admin/src/components/map/layerStyles/circleStyles.ts
@@ -0,0 +1,96 @@
+import { FeatureLike } from 'ol/Feature';
+import { LineString, Point, Polygon } from 'ol/geom';
+import { Fill, Icon, Stroke, Style, Text } from 'ol/style';
+import arrowIcon from '../../../theme/img/arrow-right.svg';
+
+let startStyle: Style | undefined = undefined;
+let endStyle: Style | undefined = undefined;
+const lineDashStyle = new Style({
+  stroke: new Stroke({
+    color: '#000000',
+    width: 1,
+    lineDash: [10, 5],
+  }),
+});
+
+export function getCircleStyle(feature: FeatureLike, resolution: number) {
+  if (resolution > 5) {
+    return lineDashStyle;
+  }
+  const diameter = `ø ${feature.getProperties().diameter} m`;
+  const geom = feature.getGeometry() as Polygon;
+  const xCoordinates = [];
+  for (const a of geom.getCoordinates()) {
+    for (const b of a) {
+      xCoordinates.push(b[0]);
+    }
+  }
+  const maxX = Math.max(...xCoordinates);
+  const minX = Math.min(...xCoordinates);
+  const point = geom.getInteriorPoint().getCoordinates();
+  const lineString = new LineString([
+    [maxX, point[1]],
+    [minX, point[1]],
+  ]);
+  const start = lineString.getFirstCoordinate();
+  const end = lineString.getLastCoordinate();
+  const dx = end[0] - start[0];
+  const dy = end[1] - start[1];
+  const rotation = Math.atan2(dy, dx);
+  const lineStyle = new Style({
+    geometry: function () {
+      return lineString;
+    },
+    stroke: new Stroke({
+      color: '#000000',
+      width: 1,
+      lineDash: [10, 5],
+    }),
+    text: new Text({
+      font: '12px "Exo2"',
+      placement: 'line',
+      textBaseline: 'bottom',
+      textAlign: 'center',
+      offsetY: -8,
+      text: diameter,
+      overflow: true,
+      fill: new Fill({
+        color: '#000000',
+      }),
+      stroke: new Stroke({
+        width: 3,
+        color: '#FFFFFF',
+      }),
+      rotateWithView: true,
+    }),
+  });
+  if (!startStyle) {
+    startStyle = new Style({
+      geometry: new Point(start),
+      image: new Icon({
+        src: arrowIcon,
+        anchor: [1, 0.5],
+        rotateWithView: true,
+        rotation: Math.PI - rotation,
+      }),
+    });
+  } else {
+    startStyle.setGeometry(new Point(start));
+    startStyle.getImage().setRotation(Math.PI - rotation);
+  }
+  if (!endStyle) {
+    endStyle = new Style({
+      geometry: new Point(end),
+      image: new Icon({
+        src: arrowIcon,
+        anchor: [1, 0.5],
+        rotateWithView: true,
+        rotation: -rotation,
+      }),
+    });
+  } else {
+    endStyle.setGeometry(new Point(end));
+    endStyle.getImage().setRotation(-rotation);
+  }
+  return [lineDashStyle, lineStyle, startStyle, endStyle];
+}
---
+++ admin/src/components/map/layerStyles/depthStyles.ts
@@ -0,0 +1,111 @@
+import { Fill, Icon, Stroke, Style, Text } from 'ol/style';
+import { FeatureLike } from 'ol/Feature';
+import { getMap } from '../DvkMap';
+import depthIconMWsmall from '../../../theme/img/depthmw1.svg';
+import depthIconMWbig from '../../../theme/img/depthmw2.svg';
+import { AreaFeatureProperties, LineFeatureProperties } from '../features';
+import { Polygon } from 'ol/geom';
+
+export function isShowN2000HeightSystem(props: AreaFeatureProperties | LineFeatureProperties): boolean {
+  if (props.n2000HeightSystem !== undefined) {
+    return props.n2000HeightSystem;
+  } else {
+    return (props.referenceLevel && props.referenceLevel.indexOf('N2000') !== -1) || !!props.n2000depth || !!props.n2000draft;
+  }
+}
+
+export function getDepthStyle(feature: FeatureLike) {
+  const props = feature.getProperties() as AreaFeatureProperties;
+  const dvkMap = getMap();
+  const n2000HeightSystem = isShowN2000HeightSystem(feature.getProperties() as AreaFeatureProperties);
+  const text =
+    dvkMap.t('homePage.map.numberFormat', { val: n2000HeightSystem ? props.n2000depth || props.depth : props.depth }) +
+    (n2000HeightSystem ? 'm' : '');
+
+  const specialFeature = feature.getProperties().areaType === 2;
+  let image;
+  if (!n2000HeightSystem) {
+    image = new Icon({
+      src: props.depth || 0 > 10 ? depthIconMWbig : depthIconMWsmall,
+      anchor: [0.5, specialFeature ? -15 : 0.5],
+      anchorXUnits: 'fraction',
+      anchorYUnits: specialFeature ? 'pixels' : 'fraction',
+      opacity: 1,
+    });
+  }
+  return [
+    new Style({
+      zIndex: 100,
+      image,
+      geometry: function (feat) {
+        const geometry = feat.getGeometry() as Polygon;
+        return geometry.getInteriorPoint();
+      },
+      text: new Text({
+        font: 'bold 12px "Exo2"',
+        placement: 'line',
+        offsetY: specialFeature ? 28 : 1,
+        text,
+        fill: new Fill({
+          color: '#FF00FF',
+        }),
+        stroke: new Stroke({
+          width: 1,
+          color: '#FFFFFF',
+        }),
+      }),
+    }),
+  ];
+}
+
+const soundingPointStyle = new Style({
+  text: new Text({
+    font: 'bold 12px "Exo2"',
+    placement: 'point',
+    text: '',
+    fill: new Fill({
+      color: '#000000',
+    }),
+    stroke: new Stroke({
+      width: 1,
+      color: '#FFFFFF',
+    }),
+  }),
+});
+
+export function getSoundingPointStyle(feature: FeatureLike) {
+  soundingPointStyle.getText().setText(String(feature.getProperties().DEPTH));
+  return soundingPointStyle;
+}
+
+const depthContourStyle1 = new Style({
+  stroke: new Stroke({
+    color: '#3B6683',
+    width: 1,
+    lineDash: [2, 2],
+  }),
+});
+const depthContourStyle2 = new Style({
+  stroke: new Stroke({
+    color: '#3B6683',
+    width: 1,
+    lineDash: [5, 5],
+  }),
+});
+const depthContourStyle3 = new Style({
+  stroke: new Stroke({
+    color: '#3B6683',
+    width: 1,
+  }),
+});
+
+export function getDepthContourStyle(feature: FeatureLike) {
+  const level = feature.getProperties().VALDCO as number;
+  if (level === 3) {
+    return depthContourStyle1;
+  } else if (level === 6) {
+    return depthContourStyle2;
+  } else {
+    return depthContourStyle3;
+  }
+}
