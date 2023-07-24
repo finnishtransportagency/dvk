7d2152187ad442865378038c59f7212879da4e21
Petri Hakala <petri.hakala@cgi.com>
2023-07-24 10:05:03 +0300

    Use fake timers to avoid random test failures

--- admin/src/App.test.tsx
+++ admin/src/App.test.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { vi } from 'vitest';
 beforeEach(() => {
   vi.resetAllMocks();
+  vi.useFakeTimers();
 });
 
 vi.mock('react-i18next', () => ({
