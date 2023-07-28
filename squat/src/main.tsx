178096ded519d704f66d5b8cbd1f1d7319796a96
Petri Hakala <petri.hakala@cgi.com>
2023-07-28 10:07:59 +0300

    Linter fix

--- squat/src/main.tsx
+++ squat/src/main.tsx
@@ -2,5 +2,5 @@ import Squat from './components/Squat';
 import SquatChart from './components/SquatChart';
 import { SquatReducer, initialState } from './hooks/squatReducer';
 import SquatContext, { useSquatContext } from './hooks/squatContext';
-import { changeLanguage as changeSquatLanguage} from 'i18next';
+import { changeLanguage as changeSquatLanguage } from 'i18next';
 export { Squat, SquatChart, SquatReducer, useSquatContext, initialState, SquatContext, changeSquatLanguage };
