c237de101acc5c062a6126a629b713428dd446be
Aki Hiisilä <75674217+hiisilaa@users.noreply.github.com>
2023-08-11 15:49:32 +0300

    Change coastal warning -> general marine warning (#606)

--- src/components/layerStyles/marineWarningStyles.ts
+++ src/components/layerStyles/marineWarningStyles.ts
@@ -4,7 +4,7 @@ import { LineString, Point, Polygon } from 'ol/geom';
 import marinearea from '../../theme/img/merivaroitus_tausta.svg';
 import marineareaSelected from '../../theme/img/merivaroitus_tausta_valittu.svg';
 import marine from '../../theme/img/merivaroitus_ikoni.svg';
-import { isCoastalWarning } from '../../utils/common';
+import { isGeneralMarineWarning } from '../../utils/common';
 import { MarineWarningFeatureProperties } from '../features';
 
 const marineAreaImage = new Image();
@@ -16,7 +16,7 @@ marineAreaSelectedImage.src = marineareaSelected;
 export function getMarineWarningStyle(feature: FeatureLike, selected: boolean) {
   const featureProperties = feature.getProperties() as MarineWarningFeatureProperties;
   const selectedScale = selected ? 1.2 : 1;
-  const iconScale = isCoastalWarning(featureProperties.type) ? 1.5 * selectedScale : 1 * selectedScale;
+  const iconScale = isGeneralMarineWarning(featureProperties.area) ? 1.5 * selectedScale : 1 * selectedScale;
 
   if (feature.getGeometry()?.getType() === 'Polygon') {
     const canvas = document.createElement('canvas');
