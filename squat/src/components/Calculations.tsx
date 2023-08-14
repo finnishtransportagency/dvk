7586331996ee2d9ad4a391f01211483bb152be79
kettunenj <juhani.kettunen@cgi.com>
2023-08-14 13:38:35 +0300

    Revert "Remove squat limited calculator (#603)"
    This reverts commit 8c55e96a0b80bdaf087e7c5706e9c99c4249bcfe.

--- squat/src/components/Calculations.tsx
+++ squat/src/components/Calculations.tsx
@@ -325,6 +325,18 @@ const Calculations: React.FC = () => {
     [setStateStatus]
   );
 
+  const handleSelectedViewChange = useCallback(
+    (e: IonSegmentCustomEvent<SegmentChangeEventDetail>) => {
+      const val = e.detail.value;
+      setStateStatus('showLimitedView', val);
+      if (val === 'true') {
+        setStateStatus('showDeepWaterValues', 'false');
+        setStateStatus('showBarrass', 'false');
+      }
+    },
+    [setStateStatus]
+  );
+
   return (
     <>
       <div className="pagebreak"></div>
@@ -389,6 +401,20 @@ const Calculations: React.FC = () => {
             </IonSegmentButton>
             <IonSegmentButton value="true">
               <IonLabel>{t('squat-barrass')}</IonLabel>
+            </IonSegmentButton>
+          </IonSegment>
+
+          <IonSegment
+            onIonChange={handleSelectedViewChange}
+            value={showLimitedView ? 'true' : 'false'}
+            className="top-padding print-hide"
+            selectOnFocus
+          >
+            <IonSegmentButton value="false">
+              <IonLabel>{t('extensive-calculator')}</IonLabel>
+            </IonSegmentButton>
+            <IonSegmentButton value="true">
+              <IonLabel>{t('limited-calculator')}</IonLabel>
             </IonSegmentButton>
           </IonSegment>
         </div>
