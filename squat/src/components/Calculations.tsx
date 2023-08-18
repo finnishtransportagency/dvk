0d23a242257b9f87a1906ea37643498a2c5afe8c
kettuncgi <48211670+kettunju@users.noreply.github.com>
2023-08-18 12:57:13 +0300

    feat: DVK-995 suppea squat muutokset (#607)
    * Revert "Remove squat limited calculator (#603)"
    
    This reverts commit 8c55e96a0b80bdaf087e7c5706e9c99c4249bcfe.
    
    * revert limited view removal, enable barrass and waterlevel in limited view
    
    * remove barrass reset from limited view

--- squat/src/components/Calculations.tsx
+++ squat/src/components/Calculations.tsx
@@ -325,6 +325,17 @@ const Calculations: React.FC = () => {
     [setStateStatus]
   );
 
+  const handleSelectedViewChange = useCallback(
+    (e: IonSegmentCustomEvent<SegmentChangeEventDetail>) => {
+      const val = e.detail.value;
+      setStateStatus('showLimitedView', val);
+      if (val === 'true') {
+        setStateStatus('showDeepWaterValues', 'false');
+      }
+    },
+    [setStateStatus]
+  );
+
   return (
     <>
       <div className="pagebreak"></div>
@@ -381,7 +392,7 @@ const Calculations: React.FC = () => {
             onIonChange={handleCalculationMethodChange}
             value={state.status.showBarrass ? 'true' : 'false'}
             className="top-padding"
-            disabled={showLimitedView || getSquatValue() === ''}
+            disabled={getSquatValue() === ''}
             selectOnFocus
           >
             <IonSegmentButton value="false">
@@ -389,6 +400,20 @@ const Calculations: React.FC = () => {
             </IonSegmentButton>
             <IonSegmentButton value="true">
               <IonLabel>{t('squat-barrass')}</IonLabel>
+            </IonSegmentButton>
+          </IonSegment>
+
+          <IonSegment
+            onIonChange={handleSelectedViewChange}
+            value={showLimitedView ? 'true' : 'false'}
+            className="top-padding print-hide"
+            selectOnFocus
+          >
+            <IonSegmentButton value="false">
+              <IonLabel>{t('extensive-calculator')}</IonLabel>
+            </IonSegmentButton>
+            <IonSegmentButton value="true">
+              <IonLabel>{t('limited-calculator')}</IonLabel>
             </IonSegmentButton>
           </IonSegment>
         </div>
