c151dc4ebc7e36f0ebc200a1fdfb387eaabf8549
kettuncgi <48211670+kettunju@users.noreply.github.com>
2023-08-25 15:43:40 +0300

    feat: DVK-205 squat pakolliset tiedot -infolaatikko (#621)
    * add alert types, make closable
    
    * add styles for alert types, remove info boxes from print
    
    * add getasseturl hack for tests
    
    * fix missing alert types
    
    * remove icon color from alert, use colored svg or css
    
    * change from rgb to ionic variables

--- squat/src/components/Alert.tsx
+++ squat/src/components/Alert.tsx
@@ -1,22 +1,58 @@
-import React, { ReactElement } from 'react';
-import { IonCol, IonGrid, IonIcon, IonRow, IonText } from '@ionic/react';
-import { warningOutline } from 'ionicons/icons';
+import React, { ReactElement, useCallback, useState } from 'react';
+import { IonButton, IonCol, IonGrid, IonIcon, IonRow, IonText } from '@ionic/react';
+import { closeOutline } from 'ionicons/icons';
+import infoIcon from '../theme/img/info-circle-solid.svg';
+import successIcon from '../theme/img/green_info_icon.svg';
+import warningIcon from '../theme/img/warning_icon.svg';
+import errorIcon from '../theme/img/alert_icon.svg';
+import { getAssetUrl } from '../utils/helpers';
 
 interface AlertProps {
   title: string | ReactElement;
+  alertType: 'info' | 'success' | 'warning' | 'error';
   className?: string;
+  closable?: boolean;
 }
 
-const Alert: React.FC<AlertProps> = (props) => {
+const Alert: React.FC<AlertProps> = ({ title, alertType, className, closable = false }) => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handleClickClose = useCallback(() => {
+    setIsOpen(false);
+  }, []);
+
+  const iconByType = () => {
+    if (alertType === 'info') return getAssetUrl(infoIcon);
+    if (alertType === 'success') return getAssetUrl(successIcon);
+    if (alertType === 'warning') return getAssetUrl(warningIcon);
+    if (alertType === 'error') return getAssetUrl(errorIcon);
+  };
+
+  if (!isOpen) {
+    return <></>;
+  }
+
   return (
-    <IonGrid className={props.className ? 'danger ' + props.className : 'danger'}>
+    <IonGrid className={'infobox ' + alertType + ' ' + className ?? ''}>
       <IonRow className="ion-align-items-center">
         <IonCol size="auto" className="icon">
-          <IonIcon icon={warningOutline} color="danger" />
+          <IonIcon icon={iconByType()} />
         </IonCol>
         <IonCol>
-          <IonText>{props.title}</IonText>
+          <IonText>{title}</IonText>
         </IonCol>
+        {closable && (
+          <IonCol size="auto">
+            <IonButton
+              onClick={handleClickClose}
+              className="icon-only button-clear no-background-focused no-box-shadow"
+              title={'close-dialog'}
+              aria-label={'close-dialog'}
+            >
+              <IonIcon icon={closeOutline} />
+            </IonButton>
+          </IonCol>
+        )}
       </IonRow>
     </IonGrid>
   );
