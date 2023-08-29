cd6d25db251cde055ee338841ce88e9d214ccc3f
kettunenj <juhani.kettunen@cgi.com>
2023-08-29 09:36:50 +0300

    move squat info icons to public folder

--- squat/src/components/Alert.tsx
+++ squat/src/components/Alert.tsx
@@ -1,10 +1,6 @@
 import React, { ReactElement, useCallback, useState } from 'react';
 import { IonButton, IonCol, IonGrid, IonIcon, IonRow, IonText } from '@ionic/react';
 import { closeOutline } from 'ionicons/icons';
-import infoIcon from '../theme/img/info-circle-solid.svg';
-import successIcon from '../theme/img/green_info_icon.svg';
-import warningIcon from '../theme/img/warning_icon.svg';
-import errorIcon from '../theme/img/alert_icon.svg';
 import { getAssetUrl } from '../utils/helpers';
 
 interface AlertProps {
@@ -22,10 +18,10 @@ const Alert: React.FC<AlertProps> = ({ title, alertType, className, closable = f
   }, []);
 
   const iconByType = () => {
-    if (alertType === 'info') return getAssetUrl(infoIcon);
-    if (alertType === 'success') return getAssetUrl(successIcon);
-    if (alertType === 'warning') return getAssetUrl(warningIcon);
-    if (alertType === 'error') return getAssetUrl(errorIcon);
+    if (alertType === 'info') return getAssetUrl('assets/info-circle-solid.svg');
+    if (alertType === 'success') return getAssetUrl('assets/green_info_icon.svg');
+    if (alertType === 'warning') return getAssetUrl('assets/warning_icon.svg');
+    if (alertType === 'error') return getAssetUrl('assets/alert_icon.svg');
   };
 
   if (!isOpen) {
