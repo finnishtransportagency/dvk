0a9ee7ae35ba9b7730a011b1cbca1fe0e54d2506
samkaariCGI <sampo.kaariainen@cgi.com>
2023-10-17 11:27:50 +0300

    added condition if old and new status is draft

--- admin/src/components/ConfirmationModal.tsx
+++ admin/src/components/ConfirmationModal.tsx
@@ -54,7 +54,10 @@ const ConfirmationModal: React.FC<ModalProps> = ({ confirmationType, setConfirma
     buttonTitle = t('general.update');
     title = t(`modal.publish-${saveType}-title`);
     description = t(`modal.publish-${saveType}-description`);
+  } else if (oldState.status === Status.Draft && newStatus === Status.Draft) {
+    return;
   }
+
   const closeModal = () => {
     modal.current?.dismiss().catch((err) => console.error(err));
     setTimeout(() => {
