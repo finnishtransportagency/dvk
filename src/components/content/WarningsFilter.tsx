dbeb30f6e1880d3549b02c3c15f80332211b41db
samkaariCGI <sampo.kaariainen@cgi.com>
2023-10-27 12:04:30 +0300

    style drafting in comments

--- src/components/content/WarningsFilter.tsx
+++ src/components/content/WarningsFilter.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { marineWarningAreas, marineWarningTypes } from '../../utils/constants';
-import { IonButton, IonCol, IonIcon, IonItem, IonRow, IonSelect, IonSelectOption, IonText } from '@ionic/react';
+import { IonButton, IonCol, IonIcon, IonRow, IonSelect, IonSelectOption, IonText } from '@ionic/react';
 import { IonSelectCustomEvent, SelectChangeEventDetail } from '@ionic/core/dist/types/components';
 import { useTranslation } from 'react-i18next';
 import sortArrow from '../../theme/img/back_arrow-1.svg';
@@ -16,7 +16,7 @@ interface WarningFilterProps {
 
 const WarningsFilter: React.FC<WarningFilterProps> = ({ setAreaFilter, setTypeFilter, setSortNewFirst, sortNewFirst, widePane }) => {
   const { t } = useTranslation();
-
+  console.log(widePane);
   const handleAreaChange = (event?: IonSelectCustomEvent<SelectChangeEventDetail<string[]>>) => {
     setAreaFilter(event?.detail.value ?? []);
     event?.preventDefault();
@@ -31,43 +31,43 @@ const WarningsFilter: React.FC<WarningFilterProps> = ({ setAreaFilter, setTypeFi
     <IonRow id="customRow">
       <IonCol>
         <IonText className="filterTitle">{t('warnings.area')}</IonText>
-        <IonItem>
-          <IonSelect
-            className={widePane ? 'marineWarningSelectWide rightMargin' : 'marineWarningSelectNarrow rightMargin'}
-            placeholder={t('common.filter')}
-            multiple={true}
-            interface="popover"
-            fill="outline"
-            labelPlacement="stacked"
-            onIonChange={(ev) => handleAreaChange(ev)}
-          >
-            {marineWarningAreas.map((area) => (
-              <IonSelectOption key={area} value={t(`areas.${area}`)}>
-                {t(`areas.${area}`)}
-              </IonSelectOption>
-            ))}
-          </IonSelect>
-        </IonItem>
+        {/*<IonItem className="filterSelectInput rightMargin">*/}
+        <IonSelect
+          className={widePane ? 'marineWarningSelectWide rightMargin' : 'marineWarningSelectNarrow rightMargin'}
+          placeholder={t('common.filter')}
+          multiple={true}
+          interface="popover"
+          fill="outline"
+          labelPlacement="stacked"
+          onIonChange={(ev) => handleAreaChange(ev)}
+        >
+          {marineWarningAreas.map((area) => (
+            <IonSelectOption key={area} value={t(`areas.${area}`)}>
+              {t(`areas.${area}`)}
+            </IonSelectOption>
+          ))}
+        </IonSelect>
+        {/*</IonItem>*/}
       </IonCol>
       <IonCol>
         <IonText className="filterTitle">{t('warnings.type')}</IonText>
-        <IonItem>
-          <IonSelect
-            className={widePane ? 'marineWarningSelectWide' : 'marineWarningSelectNarrow'}
-            placeholder={t('common.filter')}
-            multiple={true}
-            interface="popover"
-            fill="outline"
-            labelPlacement="stacked"
-            onIonChange={(ev) => handleTypeChange(ev)}
-          >
-            {marineWarningTypes.map((type) => (
-              <IonSelectOption key={type} value={t(`homePage.map.controls.layer.${type}`)}>
-                {t(`homePage.map.controls.layer.${type}`)}
-              </IonSelectOption>
-            ))}
-          </IonSelect>
-        </IonItem>
+        {/*<IonItem className="filterSelectInput rightMargin">*/}
+        <IonSelect
+          className={widePane ? 'marineWarningSelectWide' : 'marineWarningSelectNarrow'}
+          placeholder={t('common.filter')}
+          multiple={true}
+          interface="popover"
+          fill="outline"
+          labelPlacement="stacked"
+          onIonChange={(ev) => handleTypeChange(ev)}
+        >
+          {marineWarningTypes.map((type) => (
+            <IonSelectOption key={type} value={t(`homePage.map.controls.layer.${type}`)}>
+              {t(`homePage.map.controls.layer.${type}`)}
+            </IonSelectOption>
+          ))}
+        </IonSelect>
+        {/*</IonItem>*/}
       </IonCol>
       <IonCol>
         <IonButton
