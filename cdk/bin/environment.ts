3fb50d2cf4a8a4f12dd3896b8705ae83938ead57
nooksanen <noora.oksanen@cgi.com>
2023-10-27 12:24:01 +0300

    Fix typescript error

--- cdk/bin/environment.ts
+++ cdk/bin/environment.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/named
 import { GetParametersByPathCommand, GetParametersByPathCommandOutput, SSMClient } from '@aws-sdk/client-ssm';
 // eslint-disable-next-line import/named
-import { GetSecretValueCommand, ListSecretsCommand, ListSecretsCommandOutput, SecretsManagerClient } from '@aws-sdk/client-secrets-manager';
+import { FilterNameStringType, GetSecretValueCommand, ListSecretsCommand, ListSecretsCommandOutput, SecretsManagerClient } from '@aws-sdk/client-secrets-manager';
 
 const euWestSSMClient = new SSMClient({ region: 'eu-west-1' });
 const euWestSecretClient = new SecretsManagerClient({ region: 'eu-west-1' });
@@ -27,9 +27,9 @@ export async function readSecrets(path: string, global = false): Promise<Record<
   const variables: Record<string, string> = {};
   let nextToken;
   do {
-    const filters = [{ Key: 'name', Values: [(global ? '!' : '') + path] }];
+    const filters = [{ Key: FilterNameStringType.name, Values: [(global ? '!' : '') + path] }];
     const output: ListSecretsCommandOutput = await euWestSecretClient.send(new ListSecretsCommand({ Filters: filters, NextToken: nextToken }));
-    for (const param of output.SecretList || []) {
+    for (const param of output.SecretList ?? []) {
       const value = await euWestSecretClient.send(new GetSecretValueCommand({ SecretId: param.ARN }));
       if (param.Name && value.SecretString) {
         variables[global ? param.Name : param.Name.replace(path, '')] = value.SecretString;
