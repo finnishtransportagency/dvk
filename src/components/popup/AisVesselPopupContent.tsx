b5ed5b2aaf12c4f70dbf250af6222da4e337bbc8
samkaariCGI <141242759+samkaariCGI@users.noreply.github.com>
2023-11-01 15:31:39 +0200

    Dvk-1108-fix - speed and direction added to popup (#716)
    * initial components for aisvessel popup
    
    * make aislayers interactable
    
    * update aisfeatures
    
    * remove console log
    
    * make vessels interactable FIX
    
    * vessel styles when pushed or hovered
    
    * added util to get vessel type from ship type num
    
    * translation placeholders and static popup content
    
    * Popup content
    
    * popover container size and placeholder translations
    
    * resolution in popup styles
    
    * speed and direction added
    
    * double type removed
    
    * english translation placeholders
    
    * cog and sog information not available noted
    
    * all popup values in variables
    
    * speed and course value periods to commas

--- src/components/popup/AisVesselPopupContent.tsx
+++ src/components/popup/AisVesselPopupContent.tsx
@@ -12,7 +12,7 @@ import InfoIcon from '../../theme/img/info.svg?react';
 
 type AisVesselInfoRowProperties = {
   title: string;
-  body: string;
+  body: string | undefined;
 };
 
 const AisVesselInfoRow: React.FC<AisVesselInfoRowProperties> = ({ title, body }) => {
@@ -51,21 +51,28 @@ const AisVesselPopupContent: React.FC<AisVesselPopupContentProps> = ({ vessel, s
   const properties = vessel.properties;
   const dataUpdatedTime = reformatAisvesselDataUpdatedTime(properties.dataUpdatedTime);
   const coordinates = coordinatesToStringHDM(vessel.coordinates).replace('N', 'N / ');
+  const navState = checkIfMoored(properties.navStat) ? t('popup.ais.moored') : t('popup.ais.moving');
+  //speed and course combined
+  const speed = properties.sog > 102 ? '-' : properties.sog + 'kn';
+  const course = properties.cog === 360 ? '-' : properties.cog + '°';
+  const speedAndCourse = speed === '-' && course === '-' ? '' : `${String(speed).replace('.', ',')} / ${String(course).replace('.', ',')}`;
   const vesselDimensions = calculateVesselDimensions(
     properties.referencePointA,
     properties.referencePointB,
     properties.referencePointC,
     properties.referencePointD
   );
+  //check decimals
   const vesselLength = vesselDimensions[0] % 1 == 0 ? vesselDimensions[0] : vesselDimensions[0].toFixed(2).replace('.', ',');
   const vesselWidth = vesselDimensions[1] % 1 == 0 ? vesselDimensions[1] : vesselDimensions[1].toFixed(2).replace('.', ',');
+  const lengthAndWidth = vesselLength ? `${vesselLength} x ${vesselWidth} m` : '';
+  const draught = properties.draught ? `${String(properties.draught).replace('.', ',')} m` : '';
 
-  const draught = String(properties.draught).replace('.', ',');
   const closePopup = () => {
     if (setPopupProperties) setPopupProperties({});
     deselectClickSelection();
   };
-  //could use some refactoring
+
   return (
     <IonGrid id="aisVesselPopupContent">
       <IonGrid className="ion-no-padding">
@@ -82,12 +89,13 @@ const AisVesselPopupContent: React.FC<AisVesselPopupContentProps> = ({ vessel, s
         <IonRow id="higher"> {t(`popup.ais.${getAisVesselShipType(properties?.shipType)}`)}</IonRow>
         <AisVesselInfoRow title={t('popup.ais.lastUpdated')} body={dataUpdatedTime} />
         <AisVesselInfoRow title={t('popup.ais.lastLocation')} body={coordinates} />
-        <AisVesselInfoRow title={t('popup.ais.navState')} body={checkIfMoored(properties.navStat) ? t('popup.ais.moored') : t('popup.ais.moving')} />
+        <AisVesselInfoRow title={t('popup.ais.navState')} body={navState} />
+        <AisVesselInfoRow title={`${t('popup.ais.speed')} / ${t('popup.ais.course').toLowerCase()}`} body={speedAndCourse} />
         <AisVesselInfoRow title="MMSI" body={properties.mmsi} />
         <AisVesselInfoRow title={t('popup.ais.callSign')} body={String(properties.callSign)} />
         <AisVesselInfoRow title="IMO" body={String(properties.imo)} />
-        <AisVesselInfoRow title={t('popup.ais.dimensions')} body={vesselDimensions.length ? `${vesselLength} x ${vesselWidth} m` : ''} />
-        <AisVesselInfoRow title={t('popup.ais.draught')} body={draught + ' m'} />
+        <AisVesselInfoRow title={t('popup.ais.dimensions')} body={lengthAndWidth} />
+        <AisVesselInfoRow title={t('popup.ais.draught')} body={draught} />
         <AisVesselInfoRow title={`${t('popup.ais.destination')}`} body={String(properties.destination)} />
       </IonGrid>
     </IonGrid>
