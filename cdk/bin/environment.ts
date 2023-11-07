c79ec445b390ccc7e4d11ab5496e8a4e625743a0
nooksanen <noora.oksanen@cgi.com>
2023-11-07 11:12:05 +0200

    Ignore backend eslint error

--- cdk/bin/environment.ts
+++ cdk/bin/environment.ts
@@ -1,10 +1,10 @@
 // eslint-disable-next-line import/named
 import { GetParametersByPathCommand, GetParametersByPathCommandOutput, SSMClient } from '@aws-sdk/client-ssm';
-// eslint-disable-next-line import/named
 import {
   FilterNameStringType,
   GetSecretValueCommand,
   ListSecretsCommand,
+  // eslint-disable-next-line import/named
   ListSecretsCommandOutput,
   SecretsManagerClient,
 } from '@aws-sdk/client-secrets-manager';
