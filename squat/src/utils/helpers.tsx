4529fe646b51da4e7a362ed84b320c15f13075dc
samkaariCGI <sampo.kaariainen@cgi.com>
2023-11-08 14:19:29 +0200

    sonarlint fix for squat helpers

--- squat/src/utils/helpers.tsx
+++ squat/src/utils/helpers.tsx
@@ -10,11 +10,94 @@ export const countDecimals = (val: number) => {
 };
 
 const getQuerystringForField = (fieldName: string, value: number | boolean, explicitDefault?: boolean) => {
-  const defaultValue = explicitDefault !== undefined ? explicitDefault : fieldParams[fieldName].default;
+  const defaultValue = explicitDefault ?? fieldParams[fieldName].default;
   if (value !== defaultValue) {
     return fieldName + '=' + value;
   }
   return '';
+};
+
+const getFieldParameters = (currentState: State) => {
+  const limitedView = currentState.status.showLimitedView;
+  const embeddedSquat = currentState.embeddedSquat;
+  let parameters = [];
+
+  // Vessel - General
+  parameters.push(getQuerystringForField('lengthBPP', currentState.vessel.general.lengthBPP));
+  parameters.push(getQuerystringForField('breadth', currentState.vessel.general.breadth));
+  parameters.push(getQuerystringForField('draught', currentState.vessel.general.draught));
+  parameters.push(getQuerystringForField('blockCoefficient', currentState.vessel.general.blockCoefficient));
+  parameters.push(getQuerystringForField('displacement', currentState.vessel.general.displacement));
+
+  if (!limitedView) {
+    // Vessel - Detailed
+    parameters.push(getQuerystringForField('windSurface', currentState.vessel.detailed.windSurface));
+    parameters.push(getQuerystringForField('deckCargo', currentState.vessel.detailed.deckCargo));
+    parameters.push(getQuerystringForField('bowThruster', currentState.vessel.detailed.bowThruster));
+    parameters.push(getQuerystringForField('bowThrusterEfficiency', currentState.vessel.detailed.bowThrusterEfficiency));
+    parameters.push(getQuerystringForField('profileSelected', currentState.vessel.detailed.profileSelected.id - 1));
+
+    // Vessel - Stability
+    parameters.push(getQuerystringForField('KG', currentState.vessel.stability.KG));
+    parameters.push(getQuerystringForField('GM', currentState.vessel.stability.GM));
+    parameters.push(getQuerystringForField('KB', currentState.vessel.stability.KB));
+
+    // Environment - Weather
+    parameters.push(getQuerystringForField('windSpeed', currentState.environment.weather.windSpeed));
+    parameters.push(getQuerystringForField('windDirection', currentState.environment.weather.windDirection));
+    parameters.push(getQuerystringForField('waveHeight', currentState.environment.weather.waveHeight));
+    parameters.push(getQuerystringForField('wavePeriod', currentState.environment.weather.wavePeriod));
+  }
+
+  // Environment - Fairway
+  parameters.push(getQuerystringForField('sweptDepth', currentState.environment.fairway.sweptDepth));
+  parameters.push(getQuerystringForField('waterLevel', currentState.environment.fairway.waterLevel));
+  if (!limitedView) parameters.push(getQuerystringForField('waterDepth', currentState.environment.fairway.waterDepth));
+  parameters.push(getQuerystringForField('fairwayForm', currentState.environment.fairway.fairwayForm.id - 1));
+  parameters.push(getQuerystringForField('channelWidth', currentState.environment.fairway.channelWidth));
+  parameters.push(getQuerystringForField('slopeScale', currentState.environment.fairway.slopeScale));
+  parameters.push(getQuerystringForField('slopeHeight', currentState.environment.fairway.slopeHeight));
+
+  // Environment - Vessel
+  if (!limitedView) parameters.push(getQuerystringForField('vesselCourse', currentState.environment.vessel.vesselCourse));
+  parameters.push(getQuerystringForField('vesselSpeed', currentState.environment.vessel.vesselSpeed));
+  if (!limitedView) {
+    parameters.push(getQuerystringForField('turningRadius', currentState.environment.vessel.turningRadius));
+
+    // Environment - Attribute
+    parameters.push(getQuerystringForField('airDensity', currentState.environment.attribute.airDensity));
+    parameters.push(getQuerystringForField('waterDensity', currentState.environment.attribute.waterDensity));
+  }
+  parameters.push(getQuerystringForField('requiredUKC', currentState.environment.attribute.requiredUKC));
+  if (!limitedView) parameters.push(getQuerystringForField('safetyMarginWindForce', currentState.environment.attribute.safetyMarginWindForce));
+  parameters.push(getQuerystringForField('motionClearance', currentState.environment.attribute.motionClearance));
+
+  // Calculations - Status
+  if (!limitedView) parameters.push(getQuerystringForField('showDeepWaterValues', currentState.status.showDeepWaterValues, false));
+  parameters.push(getQuerystringForField('showBarrass', currentState.status.showBarrass, false));
+  if (!embeddedSquat) parameters.push(getQuerystringForField('showLimitedView', currentState.status.showLimitedView, false));
+
+  parameters = parameters.filter((param) => {
+    return param && param?.length > 0;
+  });
+
+  return parameters;
+};
+
+const getUrlBasedParameters = (
+  parameters: string[],
+  showLanguages: string | null,
+  showLogo: string | null,
+  lang: string | null,
+  baseURL: string | null,
+  useBaseURL?: boolean
+) => {
+  if (showLanguages && !useBaseURL) parameters.push('showLanguages=' + showLanguages);
+  if (showLogo && !useBaseURL) parameters.push('showLogo=' + showLogo);
+  if (lang && !useBaseURL) parameters.push('lang=' + lang);
+  if (baseURL && !useBaseURL) parameters.push('baseURL=' + baseURL);
+
+  return parameters;
 };
 
 export const createShareableLink = (currentState: State, useBaseURL?: boolean) => {
@@ -25,75 +108,11 @@ export const createShareableLink = (currentState: State, useBaseURL?: boolean) =
   const showLogo = urlParams.get('showLogo');
   const lang = urlParams.get('lang');
   const currentURL = useBaseURL && baseURL ? baseURL : window.location.href.split('?')[0];
-  const limitedView = currentState.status.showLimitedView;
-  const embeddedSquat = currentState.embeddedSquat;
 
   // Build query string based on current state (in the same order as form fields)
-  let parametres = [];
-  // Vessel - General
-  parametres.push(getQuerystringForField('lengthBPP', currentState.vessel.general.lengthBPP));
-  parametres.push(getQuerystringForField('breadth', currentState.vessel.general.breadth));
-  parametres.push(getQuerystringForField('draught', currentState.vessel.general.draught));
-  parametres.push(getQuerystringForField('blockCoefficient', currentState.vessel.general.blockCoefficient));
-  parametres.push(getQuerystringForField('displacement', currentState.vessel.general.displacement));
-
-  if (!limitedView) {
-    // Vessel - Detailed
-    parametres.push(getQuerystringForField('windSurface', currentState.vessel.detailed.windSurface));
-    parametres.push(getQuerystringForField('deckCargo', currentState.vessel.detailed.deckCargo));
-    parametres.push(getQuerystringForField('bowThruster', currentState.vessel.detailed.bowThruster));
-    parametres.push(getQuerystringForField('bowThrusterEfficiency', currentState.vessel.detailed.bowThrusterEfficiency));
-    parametres.push(getQuerystringForField('profileSelected', currentState.vessel.detailed.profileSelected.id - 1));
-
-    // Vessel - Stability
-    parametres.push(getQuerystringForField('KG', currentState.vessel.stability.KG));
-    parametres.push(getQuerystringForField('GM', currentState.vessel.stability.GM));
-    parametres.push(getQuerystringForField('KB', currentState.vessel.stability.KB));
-
-    // Environment - Weather
-    parametres.push(getQuerystringForField('windSpeed', currentState.environment.weather.windSpeed));
-    parametres.push(getQuerystringForField('windDirection', currentState.environment.weather.windDirection));
-    parametres.push(getQuerystringForField('waveHeight', currentState.environment.weather.waveHeight));
-    parametres.push(getQuerystringForField('wavePeriod', currentState.environment.weather.wavePeriod));
-  }
-
-  // Environment - Fairway
-  parametres.push(getQuerystringForField('sweptDepth', currentState.environment.fairway.sweptDepth));
-  parametres.push(getQuerystringForField('waterLevel', currentState.environment.fairway.waterLevel));
-  if (!limitedView) parametres.push(getQuerystringForField('waterDepth', currentState.environment.fairway.waterDepth));
-  parametres.push(getQuerystringForField('fairwayForm', currentState.environment.fairway.fairwayForm.id - 1));
-  parametres.push(getQuerystringForField('channelWidth', currentState.environment.fairway.channelWidth));
-  parametres.push(getQuerystringForField('slopeScale', currentState.environment.fairway.slopeScale));
-  parametres.push(getQuerystringForField('slopeHeight', currentState.environment.fairway.slopeHeight));
-
-  // Environment - Vessel
-  if (!limitedView) parametres.push(getQuerystringForField('vesselCourse', currentState.environment.vessel.vesselCourse));
-  parametres.push(getQuerystringForField('vesselSpeed', currentState.environment.vessel.vesselSpeed));
-  if (!limitedView) {
-    parametres.push(getQuerystringForField('turningRadius', currentState.environment.vessel.turningRadius));
-
-    // Environment - Attribute
-    parametres.push(getQuerystringForField('airDensity', currentState.environment.attribute.airDensity));
-    parametres.push(getQuerystringForField('waterDensity', currentState.environment.attribute.waterDensity));
-  }
-  parametres.push(getQuerystringForField('requiredUKC', currentState.environment.attribute.requiredUKC));
-  if (!limitedView) parametres.push(getQuerystringForField('safetyMarginWindForce', currentState.environment.attribute.safetyMarginWindForce));
-  parametres.push(getQuerystringForField('motionClearance', currentState.environment.attribute.motionClearance));
-
-  // Calculations - Status
-  if (!limitedView) parametres.push(getQuerystringForField('showDeepWaterValues', currentState.status.showDeepWaterValues, false));
-  parametres.push(getQuerystringForField('showBarrass', currentState.status.showBarrass, false));
-  if (!embeddedSquat) parametres.push(getQuerystringForField('showLimitedView', currentState.status.showLimitedView, false));
-
-  parametres = parametres.filter((param) => {
-    return param && param?.length > 0 ? true : false;
-  });
-
-  if (showLanguages && !useBaseURL) parametres.push('showLanguages=' + showLanguages);
-  if (showLogo && !useBaseURL) parametres.push('showLogo=' + showLogo);
-  if (lang && !useBaseURL) parametres.push('lang=' + lang);
-  if (baseURL && !useBaseURL) parametres.push('baseURL=' + baseURL);
-  return currentURL + (parametres.length ? '?' + parametres.join('&') : '');
+  let parameters = getFieldParameters(currentState);
+  parameters = getUrlBasedParameters(parameters, showLanguages, showLogo, lang, baseURL, useBaseURL);
+  return currentURL + (parameters.length ? '?' + parameters.join('&') : '');
 };
 
 export const copyToClipboard = (text: string) => {
