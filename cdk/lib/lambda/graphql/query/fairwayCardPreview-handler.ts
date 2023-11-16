bbe3a110de24796843c09f8226c937ebcc07a31d
nooksanen <noora.oksanen@cgi.com>
2023-11-16 15:53:42 +0200

    Check admin role on preview fetch

--- cdk/lib/lambda/graphql/query/fairwayCardPreview-handler.ts
+++ cdk/lib/lambda/graphql/query/fairwayCardPreview-handler.ts
@@ -3,18 +3,22 @@ import { FairwayCard, QueryFairwayCardPreviewArgs, Status } from '../../../../gr
 import { log } from '../../logger';
 import FairwayCardDBModel from '../../db/fairwayCardDBModel';
 import { getPilotPlaceMap, mapFairwayCardDBModelToGraphqlType } from '../../db/modelMapper';
-import { getOptionalCurrentUser } from '../../api/login';
+import { ADMIN_ROLE, getOptionalCurrentUser } from '../../api/login';
 
 export const handler: AppSyncResolverHandler<QueryFairwayCardPreviewArgs, FairwayCard | undefined> = async (
   event: AppSyncResolverEvent<QueryFairwayCardPreviewArgs>
 ): Promise<FairwayCard | undefined> => {
   log.info(`fairwayCard(${event.arguments.id})`);
-  const pilotMap = await getPilotPlaceMap();
-  const dbModel = await FairwayCardDBModel.get(event.arguments.id);
+  const user = await getOptionalCurrentUser(event);
 
-  if (dbModel?.status === Status.Draft || dbModel?.status === Status.Public) {
-    const user = await getOptionalCurrentUser(event);
-    return mapFairwayCardDBModelToGraphqlType(dbModel, pilotMap, user);
+  if (user?.roles.includes(ADMIN_ROLE)) {
+    const pilotMap = await getPilotPlaceMap();
+    const dbModel = await FairwayCardDBModel.get(event.arguments.id);
+
+    if (dbModel?.status === Status.Draft || dbModel?.status === Status.Public) {
+      return mapFairwayCardDBModelToGraphqlType(dbModel, pilotMap, user);
+    }
   }
+
   return undefined;
 };
