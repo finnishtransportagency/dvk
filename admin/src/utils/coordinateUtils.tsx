69d038c28452194d65e2764e3250d80ba291967a
nooksanen <noora.oksanen@cgi.com>
2023-11-17 15:03:10 +0200

    Update admin coordinateUtils

--- admin/src/utils/coordinateUtils.tsx
+++ admin/src/utils/coordinateUtils.tsx
@@ -4,12 +4,13 @@ import { padNumber } from 'ol/string';
 const degreesToStringHDM = (hemispheres: string, degrees: number, opt_fractionDigits = 0) => {
   const normalizedDegrees = modulo(degrees + 180, 360) - 180;
   const x = Math.abs(3600 * normalizedDegrees);
-  const dflPrecision = opt_fractionDigits || 0;
+  const dflPrecision = opt_fractionDigits ?? 0;
   const precision = Math.pow(10, dflPrecision);
+
   let deg = Math.floor(x / 3600);
   let min = Math.floor((x - deg * 3600) / 60);
   let sec = x - deg * 3600 - min * 60;
-  let returnedSeconds = '';
+  let hemisphere = '';
 
   if (sec >= 60) {
     sec = 0;
@@ -21,13 +22,13 @@ const degreesToStringHDM = (hemispheres: string, degrees: number, opt_fractionDi
   }
 
   min += sec / 60;
-  min = Math.ceil(min * precision) / precision;
+  min = Math.round(min * precision) / precision;
 
   if (normalizedDegrees !== 0) {
-    returnedSeconds = hemispheres.charAt(normalizedDegrees < 0 ? 1 : 0);
+    hemisphere = hemispheres.charAt(normalizedDegrees < 0 ? 1 : 0);
   }
 
-  return deg + '\u00b0 ' + padNumber(min, 2, dflPrecision).replace('.', ',') + '\u2032 ' + returnedSeconds;
+  return deg + '\u00b0 ' + padNumber(min, 2, dflPrecision).replace('.', ',') + '\u2032 ' + hemisphere;
 };
 
 export const coordinatesToStringHDM = (coords: number[] | undefined): string => {
