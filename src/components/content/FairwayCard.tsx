25886b50a0e1630d7c886583de72b3de1261a26a
samkaariCGI <sampo.kaariainen@cgi.com>
2023-11-29 09:09:22 +0200

    translations fix for alert

--- src/components/content/FairwayCard.tsx
+++ src/components/content/FairwayCard.tsx
@@ -793,6 +793,7 @@ interface AlertProps {
 
 const Alert: React.FC<AlertProps> = ({ fairwayCardId }) => {
   const { t } = useTranslation(undefined, { keyPrefix: 'fairwayCards' });
+  const { state } = useDvkContext();
   return (
     <IonGrid className="top-margin alert danger">
       <IonRow className="ion-align-items-center">
@@ -800,10 +801,16 @@ const Alert: React.FC<AlertProps> = ({ fairwayCardId }) => {
           <IonIcon icon={alertIcon} color="danger" />
         </IonCol>
         <IonCol>
-          <Trans t={t} i18nKey="alert">
-            The {{ fairwayCardId }} fairway card you requested was not found. Check the site address for typos. You can also check if the fairway card
-            you are looking for can be found in the <Link to="/kortit">Fairway cards</Link> listing.
-          </Trans>
+          {state.preview ? (
+            <Trans t={t} i18nKey="previewAlert">
+              The {{ fairwayCardId }} fairway card you requested was not found. Check the site address for typos.
+            </Trans>
+          ) : (
+            <Trans t={t} i18nKey="alert">
+              The {{ fairwayCardId }} fairway card you requested was not found. Check the site address for typos. You can also check if the fairway
+              card you are looking for can be found in the <Link to="/kortit">Fairway cards</Link> listing.
+            </Trans>
+          )}
         </IonCol>
       </IonRow>
     </IonGrid>
