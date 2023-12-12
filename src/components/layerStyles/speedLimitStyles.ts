5d688d06c92ff16a0d9db087d8eb47e286b499e5
Aki Hiisilä <75674217+hiisilaa@users.noreply.github.com>
2023-12-12 09:00:20 +0200

    DVK-1162: Handle overlapping speedlimit areas (#768)
    * Handle overlapping speedlimit areas
    
    * Admin overlapping speedlimits handling

--- src/components/layerStyles/speedLimitStyles.ts
+++ src/components/layerStyles/speedLimitStyles.ts
@@ -2,6 +2,8 @@ import { Fill, Icon, Style, Text } from 'ol/style';
 import Feature, { FeatureLike } from 'ol/Feature';
 import { getMap } from '../DvkMap';
 import speedLimitIcon from '../../theme/img/rajoitus_pohja.svg';
+import speedLimitMultiIcon from '../../theme/img/rajoitus_multi_pohja.svg';
+import specialarea from '../../theme/img/erityisalue_tausta.svg';
 import { Geometry, MultiPolygon, Polygon } from 'ol/geom';
 import intersect from '@turf/intersect';
 import flatten from '@turf/flatten';
@@ -13,6 +15,9 @@ import { GeoJSON } from 'ol/format';
 const speedLimitImage = new Image();
 speedLimitImage.src = speedLimitIcon;
 const format = new GeoJSON();
+
+const specialAreaImage = new Image();
+specialAreaImage.src = specialarea;
 
 const fillStyle = new Style({
   fill: new Fill({
@@ -38,27 +43,60 @@ const labelStyle = new Style({
   }),
 });
 
-export function getSpeedLimitStyle(feature: FeatureLike) {
-  const speedLimit = feature.get('speedLimit');
-  labelStyle.getText()?.setText('' + speedLimit);
+const multiLabelStyle = new Style({
+  zIndex: 100,
+  image: new Icon({
+    src: speedLimitMultiIcon,
+    anchor: [0.5, 12],
+    anchorXUnits: 'fraction',
+    anchorYUnits: 'pixels',
+    opacity: 1,
+  }),
+  text: new Text({
+    font: 'bold 12px "Exo2"',
+    placement: 'point',
+    fill: new Fill({
+      color: '#000000',
+    }),
+  }),
+});
 
-  let fillColor = 'rgba(0,0,0,0)';
-  if (speedLimit > 0 && speedLimit <= 5) {
-    fillColor = 'rgba(145,10,163,0.5)';
-  } else if (speedLimit > 5 && speedLimit <= 10) {
-    fillColor = 'rgba(199,63,0,0.5)';
-  } else if (speedLimit > 10 && speedLimit <= 15) {
-    fillColor = 'rgba(255,81,0,0.5)';
-  } else if (speedLimit > 15 && speedLimit <= 20) {
-    fillColor = 'rgba(255,195,0,0.5)';
-  } else if (speedLimit > 20 && speedLimit <= 25) {
-    fillColor = 'rgba(141,203,109,0.5)';
-  } else if (speedLimit > 25) {
-    fillColor = 'rgba(0,176,204,0.5)';
+function getFillStyle(feature: FeatureLike) {
+  const speedLimits = feature.get('speedLimits');
+  if (speedLimits.length === 1) {
+    labelStyle.getText()?.setText('' + speedLimits[0]);
+    const speedLimit = speedLimits[0];
+    let fillColor = 'rgba(0,0,0,0)';
+    if (speedLimit > 0 && speedLimit <= 5) {
+      fillColor = 'rgba(145,10,163,0.5)';
+    } else if (speedLimit > 5 && speedLimit <= 10) {
+      fillColor = 'rgba(199,63,0,0.5)';
+    } else if (speedLimit > 10 && speedLimit <= 15) {
+      fillColor = 'rgba(255,81,0,0.5)';
+    } else if (speedLimit > 15 && speedLimit <= 20) {
+      fillColor = 'rgba(255,195,0,0.5)';
+    } else if (speedLimit > 20 && speedLimit <= 25) {
+      fillColor = 'rgba(141,203,109,0.5)';
+    } else if (speedLimit > 25) {
+      fillColor = 'rgba(0,176,204,0.5)';
+    }
+    fillStyle.getFill()?.setColor(fillColor);
+  } else {
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d') as CanvasRenderingContext2D;
+    const gradient = context.createPattern(specialAreaImage, 'repeat');
+
+    return new Style({
+      fill: new Fill({
+        color: gradient,
+      }),
+    });
   }
+  return fillStyle;
+}
 
-  fillStyle.getFill()?.setColor(fillColor);
-
+export function getSpeedLimitStyle(feature: FeatureLike) {
+  const speedLimits = feature.get('speedLimits');
   const dvkMap = getMap();
   const bbox = dvkMap.olMap?.getView().calculateExtent();
 
@@ -84,9 +122,23 @@ export function getSpeedLimitStyle(feature: FeatureLike) {
         const geom = (format.readFeature(fg) as Feature<Geometry>).getGeometry() as Polygon;
         multiPolygon.appendPolygon(geom);
       }
-      labelStyle.setGeometry(multiPolygon.getInteriorPoints());
-      return [fillStyle, labelStyle];
+      if (speedLimits.length === 1) {
+        labelStyle.setGeometry(multiPolygon.getInteriorPoints());
+        labelStyle.getText()?.setText('' + speedLimits[0]);
+        return [getFillStyle(feature), labelStyle];
+      } else {
+        multiLabelStyle.setGeometry(multiPolygon.getInteriorPoints());
+        multiLabelStyle.getText()?.setText(
+          '' +
+            speedLimits
+              .sort((a: number, b: number) => {
+                return a > b;
+              })
+              .join(' / ')
+        );
+        return [getFillStyle(feature), multiLabelStyle];
+      }
     }
   }
-  return [fillStyle];
+  return [getFillStyle(feature)];
 }
