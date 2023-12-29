b3b29f41684ada6ca1518f3b72624589c5b37e67
nooksanen <noora.oksanen@cgi.com>
2023-12-29 15:32:30 +0200

    Zoom to harbor

--- src/components/content/HarborPreview.tsx
+++ src/components/content/HarborPreview.tsx
@@ -1,5 +1,5 @@
 import { IonText, IonSegment, IonSegmentButton, IonLabel } from '@ionic/react';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useHarborPreviewData } from '../../utils/dataLoader';
 import Breadcrumb from './Breadcrumb';
@@ -9,6 +9,8 @@ import { InfoParagraph } from './Paragraph';
 import { getTabLabel } from '../../utils/fairwayCardUtils';
 import PendingPlaceholder from './fairwayCard/PendingPlaceholder';
 import { FairwayCardHeader } from './fairwayCard/FairwayCardHeader';
+import { setSelectedHarborPreview, unsetSelectedHarborPreview } from '../layers';
+import { useHarborLayer } from '../FeatureLoader';
 
 interface HarborPreviewProps {
   widePane?: boolean;
@@ -18,7 +20,18 @@ const HarborPreview: React.FC<HarborPreviewProps> = ({ widePane }) => {
   const { t } = useTranslation(undefined, { keyPrefix: 'fairwayCards' });
   const { state } = useDvkContext();
   const { data, isPending, isFetching } = useHarborPreviewData(state.harborId);
+  const { ready: layerReady } = useHarborLayer();
+
   const path = [{ title: t('title', { count: 0 }), route: '/kortit/' }, { title: '-' }, { title: t('harboursTitle') }];
+
+  useEffect(() => {
+    if (data?.harborPreview && layerReady) {
+      setSelectedHarborPreview(data.harborPreview);
+    }
+    return () => {
+      unsetSelectedHarborPreview();
+    };
+  }, [data?.harborPreview, layerReady]);
 
   return (
     <>
