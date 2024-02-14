187ed909a46880ba97f7dd32caeed2fd3136489b
nooksanen <noora.oksanen@cgi.com>
2024-02-14 15:14:31 +0200

    Index.tsx SonarLint fixes

--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,18 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './i18n';
 import i18next, { changeLanguage } from 'i18next';
-// import { changeSquatLanguage } from 'squatlib';
 import { APP_CONFIG_PREVIEW } from './utils/constants';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement as Element);
+const squatLib = VITE_APP_CONFIG !== APP_CONFIG_PREVIEW ? import('squatlib') : Promise.resolve(undefined);
 
 export function getUrlParam(param: string) {
   const urlParams = new URLSearchParams(window.location.search);
   return urlParams.get(param);
 }
 
-(async () => {
-  const paramLang = getUrlParam('lang');
+async function setDvkLanguage(paramLang: string | null) {
   if (paramLang === 'fi') {
     await changeLanguage('fi', () => localStorage.setItem('dvkLang', 'fi'));
   } else if (paramLang === 'sv') {
@@ -28,22 +27,30 @@ export function getUrlParam(param: string) {
       await changeLanguage(storageLang);
     }
   }
+}
 
-  if (VITE_APP_CONFIG !== APP_CONFIG_PREVIEW) {
-    const squatlib = await import('squatlib');
-    if (paramLang === 'fi') {
-      await squatlib.changeSquatLanguage('fi');
-    } else if (paramLang === 'sv') {
-      await squatlib.changeSquatLanguage('sv');
-    } else if (paramLang === 'en') {
-      await squatlib.changeSquatLanguage('en');
-    } else {
-      const storageLang = localStorage.getItem('dvkLang');
-      if (storageLang) {
-        await squatlib.changeSquatLanguage(storageLang);
+async function setSquatLanguage(paramLang: string | null) {
+  squatLib.then((module) => {
+    if (module !== undefined) {
+      if (paramLang === 'fi') {
+        module.changeSquatLanguage('fi');
+      } else if (paramLang === 'sv') {
+        module.changeSquatLanguage('sv');
+      } else if (paramLang === 'en') {
+        module.changeSquatLanguage('en');
+      } else {
+        const storageLang = localStorage.getItem('dvkLang');
+        if (storageLang) {
+          module.changeSquatLanguage(storageLang);
+        }
       }
     }
-  }
+  });
+}
+
+(async () => {
+  const paramLang = getUrlParam('lang');
+  await Promise.all([setDvkLanguage(paramLang), setSquatLanguage(paramLang)]);
 
   i18next.on('languageChanged', (lang: string) => {
     const currentLang = getUrlParam('lang');
