b6123cf62b2730efc12c6a1fbfefede82bc58987
nooksanen <noora.oksanen@cgi.com>
2024-02-14 12:55:58 +0200

    Add skeleton for unloaded Squat

--- src/components/content/SquatCalculator.tsx
+++ src/components/content/SquatCalculator.tsx
@@ -2,10 +2,22 @@ import React, { Suspense, lazy } from 'react';
 import { useTranslation } from 'react-i18next';
 import Breadcrumb from './Breadcrumb';
 import { APP_CONFIG_PREVIEW } from '../../utils/constants';
+import { IonSkeletonText } from '@ionic/react';
+import './SquatCalculator.css';
 
 interface SquatCalculatorProps {
   widePane?: boolean;
 }
+
+const SquatSkeleton: React.FC<SquatCalculatorProps> = () => {
+  return (
+    <>
+      <IonSkeletonText animated={true} className="squatHeader ion-margin-top" />
+      <IonSkeletonText animated={true} className="squatLink" />
+      <IonSkeletonText animated={true} className="squatContent ion-margin-top" />
+    </>
+  );
+};
 
 const SquatCalculator: React.FC<SquatCalculatorProps> = ({ widePane }) => {
   const { t } = useTranslation();
@@ -17,7 +29,7 @@ const SquatCalculator: React.FC<SquatCalculatorProps> = ({ widePane }) => {
     <>
       <Breadcrumb path={path} />
       {VITE_APP_CONFIG !== APP_CONFIG_PREVIEW && (
-        <Suspense fallback={<></>}>
+        <Suspense fallback={<SquatSkeleton />}>
           <Squat widePane={widePane} />
         </Suspense>
       )}
