018c7910deba61ba522224ba7ebd4a1e2bb0b0f9
nooksanen <noora.oksanen@cgi.com>
2024-03-05 09:00:25 +0200

    Clear cache objects from s3

--- cdk/lib/lambda/graphql/mutation/saveFairwayCard-handler.ts
+++ cdk/lib/lambda/graphql/mutation/saveFairwayCard-handler.ts
@@ -33,6 +33,20 @@ import { diff } from 'deep-object-diff';
 import { ConditionalCheckFailedException } from '@aws-sdk/client-dynamodb';
 import { getExpires, getNewStaticBucketName } from '../../environment';
 import { PutObjectTaggingCommand, S3Client } from '@aws-sdk/client-s3';
+import { deleteCacheObjects } from '../cache';
+
+// Feature cache s3 objects related to fairway card
+const CACHE_KEYS = [
+  'harbor',
+  'pilot',
+  'area1,2',
+  'area3,4,5,6',
+  'specialarea151,2,3,4,5,6',
+  'specialarea21,2,3,4,5,6',
+  'boardline1,2',
+  'line1,2',
+  'line3,4,5,6',
+];
 
 export function mapFairwayCardToModel(card: FairwayCardInput, old: FairwayCardDBModel | undefined, user: CurrentUser): FairwayCardDBModel {
   return {
@@ -167,6 +181,10 @@ export const handler: AppSyncResolverHandler<MutationSaveFairwayCardArgs, Fairwa
   log.debug('card: %o', newModel);
   await tagPictures(event.arguments.card.id, event.arguments.card.pictures, dbModel?.pictures);
   try {
+    if (dbModel?.status === Status.Public && newModel.status !== Status.Public) {
+      // Clear feature cache s3 objects related to fairway card
+      await deleteCacheObjects(CACHE_KEYS);
+    }
     await FairwayCardDBModel.save(newModel, event.arguments.card.operation);
   } catch (e) {
     if (e instanceof ConditionalCheckFailedException && e.name === 'ConditionalCheckFailedException') {
