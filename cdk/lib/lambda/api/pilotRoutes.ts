cc50ddd9990867ce51c70ee1f244e35ae30a62a8
Aki Hiisilä <75674217+hiisilaa@users.noreply.github.com>
2024-03-11 12:43:50 +0200

    DVK-1309: Move pilot route calculations into backend (#878)
    * Move pilot route calculations into backend

--- cdk/lib/lambda/api/pilotRoutes.ts
+++ cdk/lib/lambda/api/pilotRoutes.ts
@@ -1,37 +1,124 @@
 import { ALBResult } from 'aws-lambda';
+import { LineString } from 'geojson';
 import { getPilotRoutesHeaders } from '../environment';
 import { getFromCache } from '../graphql/cache';
-import { handleLoaderError, saveResponseToS3 } from '../util';
-import { RtzData } from './apiModels';
+import { handleLoaderError, roundGeometry, saveResponseToS3 } from '../util';
+import { RtzData, RtzWaypoint, Coordinate, RtzResponseData } from './apiModels';
 import { fetchPilotRoutesApi } from './axios';
+import { lineString, bearingToAzimuth } from '@turf/helpers';
+import rhumbBearing from '@turf/rhumb-bearing';
+import transformTranslate from '@turf/transform-translate';
+import lineIntersect from '@turf/line-intersect';
+import nearestPointOnLine from '@turf/nearest-point-on-line';
+import lineArc from '@turf/line-arc';
 
-async function fetchPilotRouteData(): Promise<RtzData[]> {
+type TurningDirection = 'left' | 'right';
+
+/* Return turning direction based on azimuths before and after turn */
+function getTurningDirection(azimuthBefore: number, azimuthAfter: number): TurningDirection {
+  if ((azimuthAfter > azimuthBefore && azimuthAfter - azimuthBefore < 180) || (azimuthBefore > azimuthAfter && azimuthBefore - azimuthAfter > 180)) {
+    return 'right';
+  }
+  return 'left';
+}
+
+/* Get turning arc coordinates based on three coordinates and turning radius.
+   If turning arc cannot be calculated return array of single coordinate 'middle.
+*/
+function getTurningArc(start: Coordinate, middle: Coordinate, end: Coordinate, turningRadius: number): Array<Coordinate> {
+  /* Turning radius in kilometers, since turf library uses unit kilometers by default */
+  turningRadius = turningRadius / 1000;
+
+  /* Calculate azimuths before and after turn */
+  const angle1 = bearingToAzimuth(rhumbBearing(start, middle));
+  const angle2 = bearingToAzimuth(rhumbBearing(middle, end));
+
+  /* Do not calculate turning arc if turning less than one degree */
+  if (Math.abs(angle1 - angle2) < 1 || Math.abs(angle1 - angle2) > 359) {
+    return [middle];
+  }
+
+  /* Are we turning to left or right? */
+  const turningDirection = getTurningDirection(angle1, angle2);
+
+  /* Calculate offset lines to the left or rigth side of the legs based on turning direction */
+  const offsetLine1 = transformTranslate(lineString([start, middle]), turningRadius, turningDirection === 'right' ? angle1 + 90 : angle1 - 90);
+  const offsetLine2 = transformTranslate(lineString([middle, end]), turningRadius, turningDirection === 'right' ? angle2 + 90 : angle2 - 90);
+
+  const intersections = lineIntersect(offsetLine1, offsetLine2);
+
+  /* If not exactly one intersection point found, do not return arc */
+  if (intersections.features.length !== 1) {
+    return [middle];
+  }
+
+  /* Turning circle center point is the intersection point of offset lines */
+  const center = intersections.features[0].geometry.coordinates;
+
+  /* Calculate points where turning circle touches original lines */
+  const touchPoint1 = nearestPointOnLine(lineString([start, middle]), center);
+  const touchPoint2 = nearestPointOnLine(lineString([middle, end]), center);
+
+  /* If turning circle does not touch either of original lines, do not return arc */
+  if (touchPoint1 === undefined || touchPoint2 === undefined) {
+    return [middle];
+  }
+
+  /* Turf lineArc always returns arc coordinates clockwise, so if we are turning left we need to reverse coordinates */
+  if (turningDirection === 'right') {
+    const arc = lineArc(center, turningRadius, rhumbBearing(center, touchPoint1.geometry), rhumbBearing(center, touchPoint2.geometry));
+    return arc.geometry.coordinates as Coordinate[];
+  } else {
+    const arc = lineArc(center, turningRadius, rhumbBearing(center, touchPoint2.geometry), rhumbBearing(center, touchPoint1.geometry));
+    const coordinates = arc.geometry.coordinates;
+    coordinates.reverse();
+    return coordinates as Coordinate[];
+  }
+}
+
+/* Get coordinates of the whole rtz route given as RtzWaypoints */
+function getRtzRoute(waypoints: Array<RtzWaypoint>): Array<Coordinate> {
+  const routeCoordinates: Array<Coordinate> = [];
+  if (waypoints.length > 0) {
+    routeCoordinates.push(waypoints[0].coordinate);
+
+    for (let i = 0; i < waypoints.length - 2; i++) {
+      routeCoordinates.push(
+        ...getTurningArc(waypoints[i].coordinate, waypoints[i + 1].coordinate, waypoints[i + 2].coordinate, waypoints[i + 1].turnRadius)
+      );
+    }
+
+    if (waypoints.length > 1) {
+      routeCoordinates.push(waypoints[waypoints.length - 1].coordinate);
+    }
+  }
+  return routeCoordinates;
+}
+
+function getRouteLine(rtz: RtzData) {
+  const waypoints: Array<RtzWaypoint> = [];
+  rtz.reittipisteet.forEach((rp) => {
+    waypoints.push({ coordinate: rp.geometria.coordinates, turnRadius: rp.kaarresade * 1852 });
+  });
+  const coordinates = getRtzRoute(waypoints);
+  return coordinates;
+}
+
+async function fetchPilotRouteData(): Promise<RtzResponseData> {
   const data: RtzData[] = await fetchPilotRoutesApi();
   return data?.map((entry) => {
+    const geometry: LineString = {
+      type: 'LineString',
+      coordinates: getRouteLine(entry),
+    };
+
     return {
       tunnus: entry.tunnus,
       tila: entry.tila,
       nimi: entry.nimi,
       tunniste: entry.tunniste,
       rtz: entry.rtz,
-      reittipisteet: entry.reittipisteet?.map((piste) => {
-        return {
-          tunnus: piste.tunnus,
-          nimi: piste.nimi,
-          rtzTunniste: piste.rtzTunniste,
-          reittitunnus: piste.reittitunnus,
-          kaarresade: piste.kaarresade,
-          geometria: {
-            type: piste.geometria.type,
-            coordinates: piste.geometria.coordinates,
-          },
-          leveysVasen: piste.leveysVasen,
-          leveysOikea: piste.leveysOikea,
-          geometriaTyyppi: piste.geometriaTyyppi,
-          muutosaikaleima: piste.muutosaikaleima,
-          jarjestys: piste.jarjestys,
-        };
-      }),
+      geometria: roundGeometry(geometry, 5) as LineString,
     };
   });
 }
