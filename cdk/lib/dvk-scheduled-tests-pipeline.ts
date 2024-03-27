7065884eb8c10d02eaa88082a235c7294a2bfb60
kettuncgi <48211670+kettunju@users.noreply.github.com>
2024-03-27 13:09:10 +0200

    update robot image version (#904)

--- cdk/lib/dvk-scheduled-tests-pipeline.ts
+++ cdk/lib/dvk-scheduled-tests-pipeline.ts
@@ -79,7 +79,7 @@ export class DvkScheduledTestsPipelineStack extends Stack {
       source: gitHubSource,
       cache: Cache.local(LocalCacheMode.CUSTOM, LocalCacheMode.SOURCE, LocalCacheMode.DOCKER_LAYER),
       environment: {
-        buildImage: LinuxBuildImage.fromEcrRepository(Repository.fromRepositoryName(this, 'DvkRobotImage', 'dvk-robotimage'), '1.0.2'),
+        buildImage: LinuxBuildImage.fromEcrRepository(Repository.fromRepositoryName(this, 'DvkRobotImage', 'dvk-robotimage'), '1.0.3'),
         computeType: ComputeType.MEDIUM,
       },
       grantReportGroupPermissions: true,
