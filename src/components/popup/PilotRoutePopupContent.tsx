c77d5d4ecf0bcec79611798b01a2e0909f61db0a
nooksanen <noora.oksanen@cgi.com>
2024-04-08 10:48:15 +0300

    Download rtz file

--- src/components/popup/PilotRoutePopupContent.tsx
+++ src/components/popup/PilotRoutePopupContent.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { IonCol, IonGrid, IonRow } from '@ionic/react';
+import { IonCol, IonGrid, IonRow, IonText } from '@ionic/react';
 import './popup.css';
 import { PilotRouteFeatureProperties } from '../features';
 import { PopupProperties } from '../mapOverlays/MapOverlays';
 import { clearClickSelectionFeatures } from './selectInteraction';
 import CloseButton from './CloseButton';
 import { useTranslation } from 'react-i18next';
+import { saveAs } from 'file-saver';
 
 type PilotRoutePopupContentProps = {
   pilotroute: PilotRouteProperties;
@@ -14,6 +15,21 @@ type PilotRoutePopupContentProps = {
 
 export type PilotRouteProperties = {
   properties: PilotRouteFeatureProperties;
+};
+
+export const RtzFileDownload: React.FC<PilotRouteProperties> = ({ properties }) => {
+  const fileName = `${properties.name}.rtz`;
+
+  const downloadFile = () => {
+    const blob = new Blob([properties.rtz], { type: 'text/xml' });
+    saveAs(blob, fileName);
+  };
+
+  return (
+    <IonText className="fileDownload" onClick={() => downloadFile()}>
+      {fileName}
+    </IonText>
+  );
 };
 
 const PilotRoutePopupContent: React.FC<PilotRoutePopupContentProps> = ({ pilotroute, setPopupProperties }) => {
@@ -28,12 +44,24 @@ const PilotRoutePopupContent: React.FC<PilotRoutePopupContentProps> = ({ pilotro
     <IonGrid className="ion-no-padding">
       <IonRow className="ion-justify-content-between">
         <IonCol size="auto" className="header">
-          {t('popup.pilotRoute.header', { val: pilotroute?.properties.name })}
+          {t('popup.pilotRoute.header', { val: pilotroute.properties.name })}
         </IonCol>
         <IonCol size="auto">
           <CloseButton close={closePopup} />
         </IonCol>
       </IonRow>
+      {pilotroute.properties.rtz && (
+        <>
+          <IonRow>
+            <IonCol className="header">{t('popup.pilotRoute.rtz')}</IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <RtzFileDownload properties={pilotroute.properties} />
+            </IonCol>
+          </IonRow>
+        </>
+      )}
     </IonGrid>
   );
 };
