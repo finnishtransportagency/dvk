1ea66f82aeece89c72a86770dfedf5af42bc4e2a
kettunenj <juhani.kettunen@cgi.com>
2024-05-10 10:00:20 +0300

    change pipeline source back to normal

--- cdk/lib/dvk-build-image-stack.ts
+++ cdk/lib/dvk-build-image-stack.ts
@@ -36,7 +36,7 @@ export class DvkBuildImageStack extends Stack {
       repo: 'dvk',
       oauthToken: SecretValue.secretsManager('dev/dvk/github'),
       output: sourceOutput,
-      branch: 'feature/DVK-1377-analytics-ecs-task-root-access', //env === 'prod' ? 'prod' : 'main',
+      branch: env === 'prod' ? 'prod' : 'main',
       trigger: GitHubTrigger.NONE,
     });
 
