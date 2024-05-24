344d1dcc1d1989c55a094649aeedca8c17d28794
Aki Hiisilä <aki.hiisila@cgi.com>
2024-05-24 13:54:16 +0300

    Optimize harbor and quay styles

--- src/components/layerStyles/harborStyles.ts
+++ src/components/layerStyles/harborStyles.ts
@@ -6,46 +6,63 @@ import quayIcon from '../../theme/img/dock_icon.svg';
 import quayIconActive from '../../theme/img/dock_icon_active.svg';
 import { HarborFeatureProperties } from '../features';
 
+const style = new Style({
+  image: new Icon({
+    src: quayIcon,
+    anchor: [0.5, 43],
+    anchorXUnits: 'fraction',
+    anchorYUnits: 'pixels',
+  }),
+  text: new Text({
+    font: `bold 13px "Exo2"`,
+    placement: 'line',
+    offsetY: -55,
+    text: '',
+    fill: new Fill({
+      color: '#000000',
+    }),
+    stroke: new Stroke({
+      width: 3,
+      color: '#ffffff',
+    }),
+  }),
+  zIndex: 1,
+});
+
+const selectedStyle = new Style({
+  image: new Icon({
+    src: quayIconActive,
+    anchor: [0.5, 43],
+    anchorXUnits: 'fraction',
+    anchorYUnits: 'pixels',
+  }),
+  text: new Text({
+    font: `bold 13px "Exo2"`,
+    placement: 'line',
+    offsetY: -55,
+    text: '',
+    fill: new Fill({
+      color: '#0064AF',
+    }),
+    stroke: new Stroke({
+      width: 3,
+      color: '#ffffff',
+    }),
+  }),
+  zIndex: 10,
+});
+
 export function getHarborStyle(feature: FeatureLike, resolution: number, selected = false, minResolution = 0) {
   if (minResolution && resolution < minResolution) {
     return undefined;
   }
 
-  const image = new Icon({
-    src: quayIcon,
-    anchor: [0.5, 43],
-    anchorXUnits: 'fraction',
-    anchorYUnits: 'pixels',
-  });
-  const activeImage = new Icon({
-    src: quayIconActive,
-    anchor: [0.5, 43],
-    anchorXUnits: 'fraction',
-    anchorYUnits: 'pixels',
-  });
   const props = feature.getProperties() as HarborFeatureProperties;
-  let text;
   const dvkMap = getMap();
-  if (props.name) {
-    text = props.name[dvkMap.i18n.resolvedLanguage as Lang] as string;
-  } else {
-    text = '';
-  }
-  return new Style({
-    image: selected ? activeImage : image,
-    text: new Text({
-      font: `bold ${resolution < 50 ? '18' : '13'}px "Exo2"`,
-      placement: 'line',
-      offsetY: -55,
-      text,
-      fill: new Fill({
-        color: selected ? '#0064AF' : '#000000',
-      }),
-      stroke: new Stroke({
-        width: 3,
-        color: '#ffffff',
-      }),
-    }),
-    zIndex: selected ? 10 : 1,
-  });
+  const text = props.name ? (props.name[dvkMap.i18n.resolvedLanguage as Lang] as string) : '';
+
+  const s = selected ? selectedStyle : style;
+  s.getText()?.setFont(`bold ${resolution < 50 ? '18' : '13'}px "Exo2"`);
+  s.getText()?.setText(text);
+  return s;
 }
--- src/components/layerStyles/quayStyles.ts
+++ src/components/layerStyles/quayStyles.ts
@@ -9,39 +9,146 @@ import quayIconActive from '../../theme/img/dock_icon_active.svg';
 import quaySectionIcon from '../../theme/img/quay_section.svg';
 import quaySectionIconActive from '../../theme/img/quay_section_active.svg';
 
-function getSectionStyle(selected: boolean, props: QuayFeatureProperties) {
-  const color = selected ? '#0064AF' : '#000000';
-  const depth = props.depth ? `${props.depth[0]} m` : '';
-  const image = new Icon({
+const sectionStyle = new Style({
+  image: new Icon({
     src: quaySectionIcon,
     anchor: [0.5, 19],
     anchorXUnits: 'fraction',
     anchorYUnits: 'pixels',
-  });
-  const activeImage = new Icon({
+  }),
+  text: new Text({
+    font: '14px "Exo2"',
+    placement: 'line',
+    offsetY: -29,
+    text: '',
+    fill: new Fill({
+      color: '#000000',
+    }),
+    stroke: new Stroke({
+      width: 2,
+      color: '#ffffff',
+    }),
+  }),
+});
+
+const sectionSelectedStyle = new Style({
+  image: new Icon({
     src: quaySectionIconActive,
     anchor: [0.5, 19],
     anchorXUnits: 'fraction',
     anchorYUnits: 'pixels',
-  });
-
-  return new Style({
-    image: selected ? activeImage : image,
-    text: new Text({
-      font: '14px "Exo2"',
-      placement: 'line',
-      offsetY: -29,
-      text: depth,
-      fill: new Fill({
-        color: color,
-      }),
-      stroke: new Stroke({
-        width: 2,
-        color: '#ffffff',
-      }),
+  }),
+  text: new Text({
+    font: '14px "Exo2"',
+    placement: 'line',
+    offsetY: -29,
+    text: '',
+    fill: new Fill({
+      color: '#0064AF',
     }),
-  });
+    stroke: new Stroke({
+      width: 2,
+      color: '#ffffff',
+    }),
+  }),
+});
+
+function getSectionStyle(selected: boolean, props: QuayFeatureProperties) {
+  const s = selected ? sectionSelectedStyle : sectionStyle;
+  s.getText()?.setText(props.depth ? `${props.depth[0]} m` : '');
+  return s;
 }
+
+const quayStyle = new Style({
+  image: new Icon({
+    src: quayIcon,
+    anchor: [0.5, 43],
+    anchorXUnits: 'fraction',
+    anchorYUnits: 'pixels',
+  }),
+  text: new Text({
+    font: 'bold 18px "Exo2"',
+    placement: 'line',
+    offsetY: 0,
+    text: '',
+    fill: new Fill({
+      color: '#000000',
+    }),
+    stroke: new Stroke({
+      width: 3,
+      color: '#ffffff',
+    }),
+  }),
+  zIndex: 1,
+});
+
+const quaySelectedStyle = new Style({
+  image: new Icon({
+    src: quayIconActive,
+    anchor: [0.5, 43],
+    anchorXUnits: 'fraction',
+    anchorYUnits: 'pixels',
+  }),
+  text: new Text({
+    font: 'bold 18px "Exo2"',
+    placement: 'line',
+    offsetY: 0,
+    text: '',
+    fill: new Fill({
+      color: '#0064AF',
+    }),
+    stroke: new Stroke({
+      width: 3,
+      color: '#ffffff',
+    }),
+  }),
+  zIndex: 10,
+});
+
+const circleStyle = new Style({
+  image: new CircleStyle({
+    radius: 20,
+    displacement: [0, 20],
+    fill: new Fill({
+      color: 'rgba(0,0,0,0)',
+    }),
+  }),
+  zIndex: 2,
+});
+
+const depthStyle = new Style({
+  text: new Text({
+    font: '12px "Exo2"',
+    placement: 'line',
+    offsetY: -53,
+    text: '',
+    fill: new Fill({
+      color: '#000000',
+    }),
+    stroke: new Stroke({
+      width: 3,
+      color: '#ffffff',
+    }),
+  }),
+  zIndex: 1,
+});
+
+const depthSelectedStyle = new Style({
+  text: new Text({
+    font: '12px "Exo2"',
+    placement: 'line',
+    offsetY: -53,
+    text: '',
+    fill: new Fill({
+      color: '#0064AF',
+    }),
+    stroke: new Stroke({
+      width: 3,
+      color: '#ffffff',
+    }),
+  }),
+  zIndex: 10,
+});
 
 export function getQuayStyle(feature: FeatureLike, resolution: number, selected: boolean) {
   if (resolution > 3) {
@@ -57,70 +164,22 @@ export function getQuayStyle(feature: FeatureLike, resolution: number, selected:
   const dvkMap = getMap();
   const lang = dvkMap.i18n.resolvedLanguage as Lang;
   const quayName = props.quay?.[lang] ?? '';
-  const depthText =
-    props.depth && props.depth.length > 0 ? `${props.depth.map((d) => dvkMap.t('popup.quay.number', { val: d })).join(' m / ')} m` : '';
 
-  const image = new Icon({
-    src: quayIcon,
-    anchor: [0.5, 43],
-    anchorXUnits: 'fraction',
-    anchorYUnits: 'pixels',
-  });
-  const activeImage = new Icon({
-    src: quayIconActive,
-    anchor: [0.5, 43],
-    anchorXUnits: 'fraction',
-    anchorYUnits: 'pixels',
-  });
-  const labelFill = new Fill({
-    color: selected ? '#0064AF' : '#000000',
-  });
-  const labelStroke = new Stroke({
-    width: 3,
-    color: '#ffffff',
-  });
-  const labelZIndex = selected ? 10 : 1;
+  const s = selected ? quaySelectedStyle : quayStyle;
+  s.getText()?.setOffsetY(props.showDepth ? -72 : -55);
+  s.getText()?.setText(quayName);
 
-  const quayStyle = [
-    new Style({
-      image: selected ? activeImage : image,
-      text: new Text({
-        font: 'bold 18px "Exo2"',
-        placement: 'line',
-        offsetY: props.showDepth ? -72 : -55,
-        text: quayName,
-        fill: labelFill,
-        stroke: labelStroke,
-      }),
-      zIndex: labelZIndex,
-    }),
-    new Style({
-      image: new CircleStyle({
-        radius: 20,
-        displacement: [0, 20],
-        fill: new Fill({
-          color: 'rgba(0,0,0,0)',
-        }),
-      }),
-      zIndex: selected ? 11 : 2,
-    }),
-  ];
+  circleStyle.setZIndex(selected ? 11 : 2);
+
+  const styles = [s, circleStyle];
 
   if (props.showDepth) {
-    quayStyle.push(
-      new Style({
-        text: new Text({
-          font: '12px "Exo2"',
-          placement: 'line',
-          offsetY: -53,
-          text: depthText,
-          fill: labelFill,
-          stroke: labelStroke,
-        }),
-        zIndex: labelZIndex,
-      })
-    );
+    const depthText =
+      props.depth && props.depth.length > 0 ? `${props.depth.map((d) => dvkMap.t('popup.quay.number', { val: d })).join(' m / ')} m` : '';
+    const ds = selected ? depthSelectedStyle : depthStyle;
+    ds.getText()?.setText(depthText);
+    styles.push(ds);
   }
 
-  return quayStyle;
+  return styles;
 }
