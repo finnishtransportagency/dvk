3606740a040d55a027edb4d85d660bfa49746707
nooksanen <noora.oksanen@cgi.com>
2024-05-27 12:52:42 +0300

    Focus, validate and some styling

--- admin/src/components/form/MarkdownInput.tsx
+++ admin/src/components/form/MarkdownInput.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import MDEditor, { ICommand, bold, codeEdit, codeLive, codePreview, italic, link } from '@uiw/react-md-editor';
 import rehypeSanitize from 'rehype-sanitize';
 import { IonLabel } from '@ionic/react';
 import { ActionType, Lang, TEXTAREA_MAXLENGTH } from '../../utils/constants';
+import './MarkdownInput.css';
 
 interface MarkdownInputProps {
   label: string;
@@ -16,27 +17,50 @@ interface MarkdownInputProps {
 }
 
 const MarkdownInput: React.FC<MarkdownInputProps> = ({ label, val, setValue, actionType, actionLang, required, disabled, error }) => {
-  /* const inputRef = useRef<HTMLIonTextareaElement>(null);
-  const focusInput = () => {
-    inputRef.current?.setFocus().catch((err) => {
-      console.error(err.message);
-    });
-  }; */
+  const editorRef = useRef<HTMLDivElement>(null);
 
-  const [, setIsValid] = useState(!error);
+  const getTextareaElement = () => {
+    // div "container" > div "w-md-editor" > div "w-md-editor-content" > div "w-md-editor-area" > div "w-md-editor-text" > textarea
+    const textareaNode = editorRef.current?.firstChild?.childNodes[1]?.firstChild?.firstChild?.lastChild;
+    return textareaNode ? (textareaNode as HTMLTextAreaElement) : null;
+  };
+
+  const focusInput = () => {
+    const element = getTextareaElement();
+    element?.focus();
+  };
+
+  const [isValid, setIsValid] = useState(!error);
   const [isTouched, setIsTouched] = useState(false);
 
   const checkValidity = () => {
     if (error) {
       setIsValid(false);
+    } else {
+      const element = getTextareaElement();
+      if (element) {
+        setIsValid(element?.checkValidity());
+      }
     }
     setIsTouched(true);
+    console.log(isValid);
   };
 
   const handleChange = (newVal: string | null | undefined) => {
     if (isTouched) checkValidity();
     setValue(newVal as string, actionType, actionLang);
   };
+
+  useEffect(() => {
+    if (isTouched) {
+      const element = getTextareaElement();
+      if (error) setIsValid(false);
+      if (element) setIsValid(element?.checkValidity());
+      setIsTouched(false);
+    } else if (!required && !val.trim() && !error) {
+      setIsValid(true);
+    }
+  }, [required, error, isTouched, val]);
 
   const boldCommand: ICommand = {
     ...bold,
@@ -70,18 +94,21 @@ const MarkdownInput: React.FC<MarkdownInputProps> = ({ label, val, setValue, act
 
   return (
     <>
-      <IonLabel className={'formLabel' + (disabled ? ' disabled' : '')}>
+      <IonLabel className={'formLabel' + (disabled ? ' disabled' : '')} onClick={() => focusInput()}>
         {label} {required ? '*' : ''}
       </IonLabel>
-      <div className="container">
+      <div className="md-editor-container" ref={editorRef}>
         <MDEditor
+          className={isValid ? '' : 'error'}
           value={val}
           onChange={(value) => handleChange(value)}
+          onBlur={() => checkValidity()}
           previewOptions={{
             rehypePlugins: [[rehypeSanitize]],
           }}
           textareaProps={{
             maxLength: TEXTAREA_MAXLENGTH,
+            required: required,
           }}
           commands={[boldCommand, italicCommand, linkCommand]}
           extraCommands={[editViewCommand, liveViewCommand, previewCommand]}
