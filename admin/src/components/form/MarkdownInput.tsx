0fa0b6901af20e1c1bb30d34834f1c00769da102
nooksanen <noora.oksanen@cgi.com>
2024-05-29 11:09:52 +0300

    Button titles, counter, fix textarea ref

--- admin/src/components/form/MarkdownInput.tsx
+++ admin/src/components/form/MarkdownInput.tsx
@@ -5,6 +5,7 @@ import rehypeSanitize from 'rehype-sanitize';
 import { IonLabel } from '@ionic/react';
 import { ActionType, Lang, TEXTAREA_MAXLENGTH } from '../../utils/constants';
 import './MarkdownInput.css';
+import { getInputCounterText } from '../../utils/common';
 
 interface MarkdownInputProps {
   label: string;
@@ -18,29 +19,33 @@ interface MarkdownInputProps {
   helperText?: string;
 }
 
-function getTextareaElement(editorRef: React.RefObject<HTMLDivElement>) {
-  // div "container" > div "w-md-editor" > div "w-md-editor-content" > div "w-md-editor-area" > div "w-md-editor-text" > textarea
-  const textareaNode = editorRef.current?.firstChild?.childNodes[1]?.firstChild?.firstChild?.lastChild;
-  return textareaNode ? (textareaNode as HTMLTextAreaElement) : null;
-}
-
 const MarkdownInput: React.FC<MarkdownInputProps> = ({ label, val, setValue, actionType, actionLang, required, disabled, error, helperText }) => {
   const { t } = useTranslation(undefined, { keyPrefix: 'general' });
   const editorRef = useRef<HTMLDivElement>(null);
 
-  const focusInput = () => {
-    const element = getTextareaElement(editorRef);
-    element?.focus();
-  };
-
   const [isValid, setIsValid] = useState(!error);
   const [isTouched, setIsTouched] = useState(false);
+
+  const maxLength = TEXTAREA_MAXLENGTH;
+  const counterText = val ? getInputCounterText(val.length, maxLength) : '';
+
+  const getTextareaElement = () => {
+    // div "container" > div "w-md-editor" > div "w-md-editor-content" > div "w-md-editor-area" > div "w-md-editor-text" > textarea
+    // When preview mode is selected, returns 'P' instead of 'TEXTAREA'
+    const textareaNode = editorRef.current?.firstChild?.childNodes[1]?.firstChild?.firstChild?.lastChild;
+    return textareaNode?.nodeName === 'TEXTAREA' ? (textareaNode as HTMLTextAreaElement) : null;
+  };
+
+  const focusInput = () => {
+    const element = getTextareaElement();
+    element?.focus();
+  };
 
   const checkValidity = () => {
     if (error) {
       setIsValid(false);
     } else {
-      const element = getTextareaElement(editorRef);
+      const element = getTextareaElement();
       if (element) {
         setIsValid(element.checkValidity());
       }
@@ -62,7 +67,7 @@ const MarkdownInput: React.FC<MarkdownInputProps> = ({ label, val, setValue, act
   useEffect(() => {
     if (isTouched) {
       if (error) setIsValid(false);
-      const element = getTextareaElement(editorRef);
+      const element = getTextareaElement();
       if (element) {
         setIsValid(element.checkValidity());
       }
@@ -74,32 +79,32 @@ const MarkdownInput: React.FC<MarkdownInputProps> = ({ label, val, setValue, act
 
   const boldCommand: ICommand = {
     ...bold,
-    buttonProps: { 'aria-label': 'Lihavoitu', title: 'Lihavoitu' },
+    buttonProps: { 'aria-label': t('markdown.bold'), title: `${t('markdown.bold')} (ctrl + b)` },
   };
 
   const italicCommand: ICommand = {
     ...italic,
-    buttonProps: { 'aria-label': 'Kursivoitu', title: 'Kursivoitu' },
+    buttonProps: { 'aria-label': t('markdown.italic'), title: `${t('markdown.italic')} (ctrl + i)` },
   };
 
   const linkCommand: ICommand = {
     ...link,
-    buttonProps: { 'aria-label': 'Linkki', title: 'Linkki' },
+    buttonProps: { 'aria-label': t('markdown.hyperlink'), title: `${t('markdown.hyperlink')} (ctrl + l)` },
   };
 
   const editViewCommand: ICommand = {
     ...codeEdit,
-    buttonProps: { 'aria-label': 'Muokkaa', title: 'Muokkaa' },
+    buttonProps: { 'aria-label': t('markdown.edit-view'), title: `${t('markdown.edit-view')} (ctrl + 7)` },
   };
 
   const liveViewCommand: ICommand = {
     ...codeLive,
-    buttonProps: { 'aria-label': 'Live', title: 'Live' },
+    buttonProps: { 'aria-label': t('markdown.live-view'), title: `${t('markdown.live-view')} (ctrl + 8)` },
   };
 
   const previewCommand: ICommand = {
     ...codePreview,
-    buttonProps: { 'aria-label': 'Esikatsele', title: 'Esikatsele' },
+    buttonProps: { 'aria-label': t('markdown.preview'), title: `${t('markdown.preview')} (ctrl + 9)` },
   };
 
   return (
@@ -117,14 +122,19 @@ const MarkdownInput: React.FC<MarkdownInputProps> = ({ label, val, setValue, act
             rehypePlugins: [[rehypeSanitize]],
           }}
           textareaProps={{
-            maxLength: TEXTAREA_MAXLENGTH,
+            maxLength: maxLength,
             required: required,
           }}
           commands={[boldCommand, italicCommand, linkCommand]}
           extraCommands={[editViewCommand, liveViewCommand, previewCommand]}
+          minHeight={80}
+          height="100%"
+          visibleDragbar={false}
+          defaultTabEnable
         />
         <div className="textarea-helper">
-          {!error && isValid && helperText && <div className="helper-text"></div>}
+          {!error && isValid && counterText && <div className="counter">{counterText}</div>}
+          {!error && isValid && !counterText && helperText && <div className="helper-text">{helperText}</div>}
           {(error || !isValid) && <div className="error-text">{getErrorText()}</div>}
         </div>
       </div>
