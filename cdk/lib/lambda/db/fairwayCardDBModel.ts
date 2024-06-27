53fa18602aa7df4d9bb8b963db388f0d37e4eef5
samkaariCGI <sampo.kaariainen@cgi.com>
2024-06-27 15:28:46 +0300

    HOTFIX: DVK-1535

--- cdk/lib/lambda/db/fairwayCardDBModel.ts
+++ cdk/lib/lambda/db/fairwayCardDBModel.ts
@@ -1,4 +1,4 @@
-import { GetCommand, ScanCommand } from '@aws-sdk/lib-dynamodb';
+import { GetCommand, ScanCommand, ScanCommandInput } from '@aws-sdk/lib-dynamodb';
 import { Maybe, Status, Operation, Orientation, Mareograph } from '../../../graphql/generated';
 import { log } from '../logger';
 import { getDynamoDBDocumentClient } from './dynamoClient';
@@ -134,8 +134,6 @@ class FairwayCardDBModel {
 
   vesselRecommendation?: Maybe<Text>;
 
-  seaLevel?: Maybe<Text>;
-
   mareographs?: Maybe<Mareograph[]>;
 
   windRecommendation?: Maybe<Text>;
@@ -154,27 +152,70 @@ class FairwayCardDBModel {
 
   temporaryNotifications?: Maybe<TemporaryNotification[]>;
 
-  // at the moment this gives any latest version, should be separate function for latest public
+  private static getLatestSortKey() {
+    return 'v0_latest';
+  }
+
+  private static getPublicSortKey() {
+    return 'v0_public';
+  }
+
+  // this can be deleted when structure changed to use getVersion, getLatest and getPublic
   static async get(id: string, version: string = 'v0_latest'): Promise<FairwayCardDBModel | undefined> {
-    // v0 always the latest
     const response = await getDynamoDBDocumentClient().send(
       new GetCommand({ TableName: getFairwayCardTableName(), Key: { id: id, version: version } })
     );
-    const fairwayCard = response.Item as FairwayCardDBModel | undefined;
+    const fairwayCard = response?.Item as FairwayCardDBModel | undefined;
+    log.debug('Fairway card name: %s', fairwayCard?.name?.fi);
+    return fairwayCard;
+  }
+
+  static async getVersion(id: string, version: string = 'v1'): Promise<FairwayCardDBModel | undefined> {
+    const response = await getDynamoDBDocumentClient().send(
+      new GetCommand({ TableName: getFairwayCardTableName(), Key: { id: id, version: version } })
+    );
+    const fairwayCard = response?.Item as FairwayCardDBModel | undefined;
+    log.debug('Fairway card name: %s', fairwayCard?.name?.fi);
+    return fairwayCard;
+  }
+
+  static async getLatest(id: string): Promise<FairwayCardDBModel | undefined> {
+    // v0 always the latest
+    const response = await getDynamoDBDocumentClient().send(
+      new GetCommand({ TableName: getFairwayCardTableName(), Key: { id: id, version: this.getLatestSortKey() } })
+    );
+    const fairwayCard = response?.Item as FairwayCardDBModel | undefined;
+    log.debug('Fairway card name: %s', fairwayCard?.name?.fi);
+    return fairwayCard;
+  }
+
+  static async getPublic(id: string): Promise<FairwayCardDBModel | undefined> {
+    // v0 always the public version
+    const response = await getDynamoDBDocumentClient().send(
+      new GetCommand({ TableName: getFairwayCardTableName(), Key: { id: id, version: this.getPublicSortKey() } })
+    );
+    const fairwayCard = response?.Item as FairwayCardDBModel | undefined;
     log.debug('Fairway card name: %s', fairwayCard?.name?.fi);
     return fairwayCard;
   }
 
   static async getAllLatest(): Promise<FairwayCardDBModel[]> {
-    const response = await getDynamoDBDocumentClient().send(
-      new ScanCommand({
-        TableName: getFairwayCardTableName(),
-        FilterExpression: '#version = :vVersion',
-        ExpressionAttributeNames: { '#version': 'version' },
-        ExpressionAttributeValues: { ':vVersion': 'v0_latest' },
-      })
-    );
-    const fairwayCards = response.Items as FairwayCardDBModel[] | undefined;
+    const fairwayCards: FairwayCardDBModel[] | undefined = [];
+    let response;
+
+    const params: ScanCommandInput = {
+      TableName: getFairwayCardTableName(),
+      FilterExpression: '#version = :vVersion',
+      ExpressionAttributeNames: { '#version': 'version' },
+      ExpressionAttributeValues: { ':vVersion': this.getLatestSortKey() },
+    };
+
+    do {
+      response = await getDynamoDBDocumentClient().send(new ScanCommand(params));
+      response.Items?.forEach((item) => fairwayCards.push(item as FairwayCardDBModel));
+      params.ExclusiveStartKey = response.LastEvaluatedKey;
+    } while (typeof response.LastEvaluatedKey !== 'undefined');
+
     if (fairwayCards) {
       log.debug('%d fairway card(s) found', fairwayCards.length);
       return fairwayCards;
@@ -184,17 +225,24 @@ class FairwayCardDBModel {
   }
 
   static async getAllPublic(): Promise<FairwayCardDBModel[]> {
-    const response = await getDynamoDBDocumentClient().send(
-      new ScanCommand({
-        TableName: getFairwayCardTableName(),
-        FilterExpression: '#version = :vVersion AND attribute_exists(#currentPublic)',
-        ExpressionAttributeNames: { '#version': 'version', '#currentPublic': 'currentPublic' },
-        ExpressionAttributeValues: { ':vVersion': 'v0_public' },
-      })
-    );
-    const fairwayCards = response.Items as FairwayCardDBModel[] | undefined;
+    const fairwayCards: FairwayCardDBModel[] | undefined = [];
+    let response;
+
+    const params: ScanCommandInput = {
+      TableName: getFairwayCardTableName(),
+      FilterExpression: '#version = :vVersion AND attribute_exists(#currentPublic)',
+      ExpressionAttributeNames: { '#version': 'version', '#currentPublic': 'currentPublic' },
+      ExpressionAttributeValues: { ':vVersion': this.getPublicSortKey() },
+    };
+
+    do {
+      response = await getDynamoDBDocumentClient().send(new ScanCommand(params));
+      response.Items?.forEach((item) => fairwayCards.push(item as FairwayCardDBModel));
+      params.ExclusiveStartKey = response.LastEvaluatedKey;
+    } while (typeof response.LastEvaluatedKey !== 'undefined');
+
     if (fairwayCards) {
-      log.debug('%d public fairway card(s) found', fairwayCards.length);
+      log.debug('%d Public fairway card(s) found', fairwayCards.length);
       return fairwayCards;
     }
     log.debug('No public fairway cards found');
--- cdk/lib/lambda/db/harborDBModel.ts
+++ cdk/lib/lambda/db/harborDBModel.ts
@@ -1,4 +1,4 @@
-import { GetCommand, ScanCommand } from '@aws-sdk/lib-dynamodb';
+import { GetCommand, ScanCommand, ScanCommandInput } from '@aws-sdk/lib-dynamodb';
 import { GeometryPoint, Maybe, Operation, Status } from '../../../graphql/generated';
 import { log } from '../logger';
 import { getDynamoDBDocumentClient } from './dynamoClient';
@@ -61,24 +61,67 @@ class HarborDBModel {
 
   expires?: Maybe<number>;
 
+  private static getLatestSortKey() {
+    return 'v0_latest';
+  }
+
+  private static getPublicSortKey() {
+    return 'v0_public';
+  }
+
   // at the moment this gives any latest version, should be separate function for latest public
   static async get(id: string, version: string = 'v0_latest'): Promise<HarborDBModel | undefined> {
     const response = await getDynamoDBDocumentClient().send(new GetCommand({ TableName: getHarborTableName(), Key: { id: id, version: version } }));
-    const harbor = response.Item as HarborDBModel | undefined;
+    const harbor = response?.Item as HarborDBModel | undefined;
     log.debug('Harbor name: %s', harbor?.name?.fi);
     return harbor;
   }
 
-  static async getAllLatest(): Promise<HarborDBModel[]> {
+  static async getVersion(id: string, version: string = 'v1'): Promise<HarborDBModel | undefined> {
+    // v0 always the latest version
+    const response = await getDynamoDBDocumentClient().send(new GetCommand({ TableName: getHarborTableName(), Key: { id: id, version: version } }));
+    const harbor = response?.Item as HarborDBModel | undefined;
+    log.debug('Harbor card name: %s', harbor?.name?.fi);
+    return harbor;
+  }
+
+  static async getLatest(id: string): Promise<HarborDBModel | undefined> {
+    // v0 always the public version
     const response = await getDynamoDBDocumentClient().send(
-      new ScanCommand({
-        TableName: getHarborTableName(),
-        FilterExpression: '#version = :vVersion',
-        ExpressionAttributeNames: { '#version': 'version' },
-        ExpressionAttributeValues: { ':vVersion': 'v0_latest' },
-      })
+      new GetCommand({ TableName: getHarborTableName(), Key: { id: id, version: this.getLatestSortKey() } })
     );
-    const harbors = response.Items as HarborDBModel[] | undefined;
+    const harbor = response?.Item as HarborDBModel | undefined;
+    log.debug('Harbor card name: %s', harbor?.name?.fi);
+    return harbor;
+  }
+
+  static async getPublic(id: string): Promise<HarborDBModel | undefined> {
+    // v0 always the public version
+    const response = await getDynamoDBDocumentClient().send(
+      new GetCommand({ TableName: getHarborTableName(), Key: { id: id, version: this.getPublicSortKey() } })
+    );
+    const harbor = response?.Item as HarborDBModel | undefined;
+    log.debug('Harbor card name: %s', harbor?.name?.fi);
+    return harbor;
+  }
+
+  static async getAllLatest(): Promise<HarborDBModel[]> {
+    const harbors: HarborDBModel[] | undefined = [];
+    let response;
+
+    const params: ScanCommandInput = {
+      TableName: getHarborTableName(),
+      FilterExpression: '#version = :vVersion',
+      ExpressionAttributeNames: { '#version': 'version' },
+      ExpressionAttributeValues: { ':vVersion': this.getLatestSortKey() },
+    };
+
+    do {
+      response = await getDynamoDBDocumentClient().send(new ScanCommand(params));
+      response.Items?.forEach((item) => harbors.push(item as HarborDBModel));
+      params.ExclusiveStartKey = response.LastEvaluatedKey;
+    } while (typeof response.LastEvaluatedKey !== 'undefined');
+
     if (harbors) {
       log.debug('%d harbor(s) found', harbors.length);
       return harbors;
@@ -88,15 +131,22 @@ class HarborDBModel {
   }
 
   static async getAllPublic(): Promise<HarborDBModel[]> {
-    const response = await getDynamoDBDocumentClient().send(
-      new ScanCommand({
-        TableName: getHarborTableName(),
-        FilterExpression: '#version = :vVersion AND attribute_exists(#currentPublic)',
-        ExpressionAttributeNames: { '#version': 'version', '#currentPublic': 'currentPublic' },
-        ExpressionAttributeValues: { ':vVersion': 'v0_public' },
-      })
-    );
-    const harbors = response.Items as HarborDBModel[] | undefined;
+    const harbors: HarborDBModel[] | undefined = [];
+    let response;
+
+    const params: ScanCommandInput = {
+      TableName: getHarborTableName(),
+      FilterExpression: '#version = :vVersion AND attribute_exists(#currentPublic)',
+      ExpressionAttributeNames: { '#version': 'version', '#currentPublic': 'currentPublic' },
+      ExpressionAttributeValues: { ':vVersion': this.getPublicSortKey() },
+    };
+
+    do {
+      response = await getDynamoDBDocumentClient().send(new ScanCommand(params));
+      response.Items?.forEach((item) => harbors.push(item as HarborDBModel));
+      params.ExclusiveStartKey = response.LastEvaluatedKey;
+    } while (typeof response.LastEvaluatedKey !== 'undefined');
+
     if (harbors) {
       log.debug('%d public harbor(s) found', harbors.length);
       return harbors;
