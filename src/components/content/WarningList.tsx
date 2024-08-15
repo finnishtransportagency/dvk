a3c7fa1b80438dd87f89e7478b01f377fa87b4c7
samkaariCGI <sampo.kaariainen@cgi.com>
2024-08-15 13:27:10 +0300

    adjust hover shadow

--- src/components/content/WarningList.tsx
+++ src/components/content/WarningList.tsx
@@ -43,163 +43,166 @@ export const WarningList: React.FC<WarningListProps> = ({ data, loading, sortNew
           : undefined;
         const marineWarningDataLayerId = getMarineWarningDataLayerId(warning.type);
         return (
-          <IonGrid
-            className="table light group ion-no-padding inlineHoverText"
-            key={warning.id}
-            onMouseEnter={() => setSelectedMarineWarning(warning.id, true, marineWarningDataLayerId)}
-            onFocus={() => setSelectedMarineWarning(warning.id, true, marineWarningDataLayerId)}
-            onMouseLeave={() => setSelectedMarineWarning(warning.id, false, marineWarningDataLayerId)}
-            onBlur={() => setSelectedMarineWarning(warning.id, false, marineWarningDataLayerId)}
-          >
-            <IonRow className="header">
-              <IonCol>
-                <IonText>
-                  <h3 className="h5">
-                    {warning.type[lang]} - {warning.number}
-                  </h3>
-                </IonText>
-              </IonCol>
-              <IonCol className="ion-text-end ion-align-self-end">
-                <IonText>
-                  <em>{t('datetimeFormat', { val: warning.dateTime })}</em>
-                </IonText>
-              </IonCol>
-            </IonRow>
-            <IonRow>
-              <IonCol size="6" className="no-padding">
-                <IonRow>
-                  <IonCol size="auto">
-                    <IonIcon className="listIcon" src={getWarningImgSource(warning.type[lang] ?? '')} />
-                  </IonCol>
-                  <IonCol>
-                    <IonText className="no-margin-top">
-                      <h4 className="h5">{t('area')}</h4>
-                      <Paragraph bodyText={warning.area} />
-                    </IonText>
-                  </IonCol>
-                </IonRow>
-              </IonCol>
-              <IonCol size="6">
-                <IonText className="no-margin-top">
-                  <h4 className="h5">{t('location')}</h4>
-                  <p>
-                    <Link
-                      to="/merivaroitukset/"
-                      onClick={(e) => {
-                        e.preventDefault();
-                        goToFeature(warning.id, marineWarningDataLayerId, state.layers);
-                      }}
-                    >
-                      {warning.location[lang] ?? warning.location.fi ?? t('noObjects')}
-                    </Link>
-                  </p>
-                </IonText>
-              </IonCol>
-            </IonRow>
-            <IonRow>
-              <IonCol>
-                <IonText className="no-margin-top">
-                  <h4 className="h5">{t('content')}</h4>
-                  <Paragraph bodyText={warning.description} />
-                </IonText>
-              </IonCol>
-            </IonRow>
-            <IonRow>
-              <IonCol size="6">
-                <IonText className="no-margin-top">
-                  <h4 className="h5">{t('relatedObjects')}</h4>
-                  {(warning.areaId || warning.lineId || warning.equipmentId) && (
-                    <p>
-                      {warning.equipmentId && (
-                        <>
-                          {equipmentFeature?.getProperties().name[lang] || equipmentFeature?.getProperties().name.fi}
-                          {' - '}
-                          {warning.equipmentId}
-                        </>
-                      )}
-                      {warning.lineId && (
-                        <>
-                          {(dvkMap.getVectorSource('line12').getFeatureById(warning.lineId) as Feature<Geometry>)
-                            ?.getProperties()
-                            .fairways?.map((fairway: LineFairway) => {
-                              const uuid = uniqueId('span_');
-                              return (
-                                <span key={uuid}>
-                                  {fairway.name[lang] ?? fairway.name.fi} - {fairway.fairwayId}
-                                </span>
-                              );
-                            })}
-                          {(dvkMap.getVectorSource('line3456').getFeatureById(warning.lineId) as Feature<Geometry>)
-                            ?.getProperties()
-                            .fairways?.map((fairway: LineFairway) => {
-                              const uuid = uniqueId('span_');
-                              return (
-                                <span key={uuid}>
-                                  {fairway.name[lang] ?? fairway.name.fi} - {fairway.fairwayId}
-                                </span>
-                              );
-                            })}
-                        </>
-                      )}
-                      {warning.areaId && (
-                        <>
-                          {(dvkMap.getVectorSource('area12').getFeatureById(warning.areaId) as Feature<Geometry>)
-                            ?.getProperties()
-                            .fairways?.map((fairway: AreaFairway) => {
-                              const uuid = uniqueId('span_');
-                              return (
-                                <span key={uuid}>
-                                  {fairway.name[lang] ?? fairway.name.fi} - {fairway.fairwayId}
-                                </span>
-                              );
-                            })}
-                          {(dvkMap.getVectorSource('area3456').getFeatureById(warning.areaId) as Feature<Geometry>)
-                            ?.getProperties()
-                            .fairways?.map((fairway: AreaFairway) => {
-                              const uuid = uniqueId('span_');
-                              return (
-                                <span key={uuid}>
-                                  {fairway.name[lang] ?? fairway.name.fi} - {fairway.fairwayId}
-                                </span>
-                              );
-                            })}
-                        </>
-                      )}
-                    </p>
-                  )}
-                  {!warning.areaId && !warning.lineId && !warning.equipmentId && <InfoParagraph />}
-                </IonText>
-              </IonCol>
-              {(warning.startDateTime || warning.endDateTime) && (
-                <IonCol size="6">
-                  <IonText className="no-margin-top">
-                    <h4 className="h5">{t('validityTime')}</h4>
-                    <p>
-                      {warning.startDateTime &&
-                        t('datetimeFormat', {
-                          val: warning.startDateTime,
-                        })}
-                      {' - '}
-                      {warning.endDateTime &&
-                        t('datetimeFormat', {
-                          val: warning.endDateTime,
-                        })}
-                    </p>
+          <>
+            <div style={{ padding: '6px' }} />
+            <IonGrid
+              className="table light group ion-no-padding inlineHoverText"
+              key={warning.id}
+              onMouseEnter={() => setSelectedMarineWarning(warning.id, true, marineWarningDataLayerId)}
+              onFocus={() => setSelectedMarineWarning(warning.id, true, marineWarningDataLayerId)}
+              onMouseLeave={() => setSelectedMarineWarning(warning.id, false, marineWarningDataLayerId)}
+              onBlur={() => setSelectedMarineWarning(warning.id, false, marineWarningDataLayerId)}
+            >
+              <IonRow className="header">
+                <IonCol>
+                  <IonText>
+                    <div className="h5">
+                      {(warning.type[lang] as string)?.toLocaleUpperCase()} - {warning.number}
+                    </div>
                   </IonText>
                 </IonCol>
-              )}
-            </IonRow>
-            {warning.notifier && (
-              <IonRow>
-                <IonCol>
-                  <IonText className="no-margin-top">
-                    <h4 className="h5">{t('notifier')}</h4>
-                    {warning.notifier}
+                <IonCol className="ion-text-end ion-align-self-end">
+                  <IonText>
+                    <em>{t('datetimeFormat', { val: warning.dateTime })}</em>
                   </IonText>
                 </IonCol>
               </IonRow>
-            )}
-          </IonGrid>
+              <IonRow>
+                <IonCol size="6" className="no-padding">
+                  <IonRow>
+                    <IonCol size="auto">
+                      <IonIcon className="listIcon" src={getWarningImgSource(warning.type[lang] ?? '')} />
+                    </IonCol>
+                    <IonCol>
+                      <IonText className="no-margin-top">
+                        <h4 className="h5">{t('area')}</h4>
+                        <Paragraph bodyText={warning.area} />
+                      </IonText>
+                    </IonCol>
+                  </IonRow>
+                </IonCol>
+                <IonCol size="6">
+                  <IonText className="no-margin-top">
+                    <h4 className="h5">{t('location')}</h4>
+                    <p>
+                      <Link
+                        to="/merivaroitukset/"
+                        onClick={(e) => {
+                          e.preventDefault();
+                          goToFeature(warning.id, marineWarningDataLayerId, state.layers);
+                        }}
+                      >
+                        {warning.location[lang] ?? warning.location.fi ?? t('noObjects')}
+                      </Link>
+                    </p>
+                  </IonText>
+                </IonCol>
+              </IonRow>
+              <IonRow>
+                <IonCol>
+                  <IonText className="no-margin-top">
+                    <h4 className="h5">{t('content')}</h4>
+                    <Paragraph bodyText={warning.description} />
+                  </IonText>
+                </IonCol>
+              </IonRow>
+              <IonRow>
+                <IonCol size="6">
+                  <IonText className="no-margin-top">
+                    <h4 className="h5">{t('relatedObjects')}</h4>
+                    {(warning.areaId || warning.lineId || warning.equipmentId) && (
+                      <p>
+                        {warning.equipmentId && (
+                          <>
+                            {equipmentFeature?.getProperties().name[lang] || equipmentFeature?.getProperties().name.fi}
+                            {' - '}
+                            {warning.equipmentId}
+                          </>
+                        )}
+                        {warning.lineId && (
+                          <>
+                            {(dvkMap.getVectorSource('line12').getFeatureById(warning.lineId) as Feature<Geometry>)
+                              ?.getProperties()
+                              .fairways?.map((fairway: LineFairway) => {
+                                const uuid = uniqueId('span_');
+                                return (
+                                  <span key={uuid}>
+                                    {fairway.name[lang] ?? fairway.name.fi} - {fairway.fairwayId}
+                                  </span>
+                                );
+                              })}
+                            {(dvkMap.getVectorSource('line3456').getFeatureById(warning.lineId) as Feature<Geometry>)
+                              ?.getProperties()
+                              .fairways?.map((fairway: LineFairway) => {
+                                const uuid = uniqueId('span_');
+                                return (
+                                  <span key={uuid}>
+                                    {fairway.name[lang] ?? fairway.name.fi} - {fairway.fairwayId}
+                                  </span>
+                                );
+                              })}
+                          </>
+                        )}
+                        {warning.areaId && (
+                          <>
+                            {(dvkMap.getVectorSource('area12').getFeatureById(warning.areaId) as Feature<Geometry>)
+                              ?.getProperties()
+                              .fairways?.map((fairway: AreaFairway) => {
+                                const uuid = uniqueId('span_');
+                                return (
+                                  <span key={uuid}>
+                                    {fairway.name[lang] ?? fairway.name.fi} - {fairway.fairwayId}
+                                  </span>
+                                );
+                              })}
+                            {(dvkMap.getVectorSource('area3456').getFeatureById(warning.areaId) as Feature<Geometry>)
+                              ?.getProperties()
+                              .fairways?.map((fairway: AreaFairway) => {
+                                const uuid = uniqueId('span_');
+                                return (
+                                  <span key={uuid}>
+                                    {fairway.name[lang] ?? fairway.name.fi} - {fairway.fairwayId}
+                                  </span>
+                                );
+                              })}
+                          </>
+                        )}
+                      </p>
+                    )}
+                    {!warning.areaId && !warning.lineId && !warning.equipmentId && <InfoParagraph />}
+                  </IonText>
+                </IonCol>
+                {(warning.startDateTime || warning.endDateTime) && (
+                  <IonCol size="6">
+                    <IonText className="no-margin-top">
+                      <h4 className="h5">{t('validityTime')}</h4>
+                      <p>
+                        {warning.startDateTime &&
+                          t('datetimeFormat', {
+                            val: warning.startDateTime,
+                          })}
+                        {' - '}
+                        {warning.endDateTime &&
+                          t('datetimeFormat', {
+                            val: warning.endDateTime,
+                          })}
+                      </p>
+                    </IonText>
+                  </IonCol>
+                )}
+              </IonRow>
+              {warning.notifier && (
+                <IonRow>
+                  <IonCol>
+                    <IonText className="no-margin-top">
+                      <h4 className="h5">{t('notifier')}</h4>
+                      {warning.notifier}
+                    </IonText>
+                  </IonCol>
+                </IonRow>
+              )}
+            </IonGrid>
+          </>
         );
       })}
     </>
