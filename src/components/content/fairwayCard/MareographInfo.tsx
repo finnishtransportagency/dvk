10b0a9f732449ef2b561c81a0114a3ac84a58998
samkaariCGI <141242759+samkaariCGI@users.noreply.github.com>
2024-08-20 14:41:31 +0300

    DVK-1530 - add sea levels and latest observation to fairway card mareographs (#1063)
    * add sealevels and latest observation to fairway card mareographs
    
    * offline check and no measurements to print
    
    * change info icon class name
    
    * fix title

--- src/components/content/fairwayCard/MareographInfo.tsx
+++ src/components/content/fairwayCard/MareographInfo.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { Feature } from 'ol';
 import { Geometry, Point } from 'ol/geom';
 import { MareographFeatureProperties } from '../../features';
-import { IonLabel } from '@ionic/react';
+import { IonIcon, IonLabel, IonRow, IonText } from '@ionic/react';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
-import { goToFeature } from '../../../utils/common';
+import { getTimeDifference, goToFeature } from '../../../utils/common';
 import { coordinatesToStringHDM } from '../../../utils/coordinateUtils';
 import { setSelectedMareograph } from '../../layers';
+import { hourInMilliseconds } from '../../../utils/constants';
+import alertIcon from '../../../theme/img/alert_icon.svg';
+import InfoIcon from '../../../theme/img/info.svg?react';
+import { useDvkContext } from '../../../hooks/dvkContext';
 
 interface MareographInfoProps {
   mareographs: Feature<Geometry>[];
@@ -15,6 +19,9 @@ interface MareographInfoProps {
 
 const MareographInfo: React.FC<MareographInfoProps> = ({ mareographs }) => {
   const { t } = useTranslation();
+
+  const { state } = useDvkContext();
+  const { isOffline } = state;
 
   const highlightMareograph = (id?: string | number) => {
     setSelectedMareograph(id);
@@ -34,6 +41,9 @@ const MareographInfo: React.FC<MareographInfoProps> = ({ mareographs }) => {
             const id = mareograph.getId();
             const geometry = mareograph.getGeometry() as Point;
             const coordinates = geometry.getCoordinates();
+            const isDataOutdated12Hours = getTimeDifference(properties.dateTime) > hourInMilliseconds * 12;
+            const isDataOutdated1Hour = getTimeDifference(properties.dateTime) > hourInMilliseconds;
+
             return (
               <p key={id}>
                 <IonLabel
@@ -45,23 +55,86 @@ const MareographInfo: React.FC<MareographInfoProps> = ({ mareographs }) => {
                   tabIndex={0}
                 >
                   <strong>
-                    {properties.name} - {t('fairwayCards.mareograph')}
+                    {properties.name} - {t('popup.mareograph.mareograph')}
                   </strong>
                   <br />
-                  {t('fairwayCards.mareographLocation')}:&nbsp;
-                  {!!coordinates[0] && !!coordinates[1] && (
-                    <u>
-                      <Link
-                        to={window.location.pathname}
-                        onClick={(e) => {
-                          e.preventDefault();
-                          goToFeature(id, 'selectedfairwaycard');
-                        }}
-                      >
-                        {coordinatesToStringHDM(coordinates)}
-                      </Link>
-                    </u>
-                  )}
+                  <IonRow>
+                    {t('fairwayCards.mareographLocation')}:&nbsp;
+                    {!!coordinates[0] && !!coordinates[1] && (
+                      <u>
+                        <Link
+                          to={window.location.pathname}
+                          onClick={(e) => {
+                            e.preventDefault();
+                            goToFeature(id, 'selectedfairwaycard');
+                          }}
+                        >
+                          {coordinatesToStringHDM(coordinates)}
+                        </Link>
+                      </u>
+                    )}
+                  </IonRow>
+                  <span className={isDataOutdated1Hour ? 'outdatedData no-print' : 'no-print'}>
+                    <IonRow>
+                      <IonText>
+                        {t('popup.mareograph.dateTime')}: {t('fairwayCards.datetimeFormat', { val: properties.dateTime })}
+                      </IonText>
+                    </IonRow>
+                  </span>
+                  <span className={isDataOutdated12Hours ? 'outdatedData no-print' : 'no-print'}>
+                    <IonRow>
+                      <IonText>
+                        {t('popup.mareograph.seaLevel')}:&nbsp;
+                        {!isDataOutdated12Hours ? (
+                          <>
+                            {properties.waterLevel && !isOffline ? (
+                              <>
+                                {properties.waterLevel >= 0 ? '+' : ''}
+                                {Math.round(properties.waterLevel / 10)}{' '}
+                                <dd aria-label={t('fairwayCards.unit.cmDesc', { count: Math.round((properties.waterLevel || 0) / 10) })}>cm</dd>
+                              </>
+                            ) : (
+                              <>
+                                <InfoIcon className="fairwayCardInfoIcon" />
+                                {t('common.noData')}
+                              </>
+                            )}
+                          </>
+                        ) : (
+                          <>
+                            <IonIcon className="outdatedDataIcon" icon={alertIcon} color="#EC0E0E" />
+                            {t('popup.common.outdatedData')}
+                          </>
+                        )}
+                      </IonText>
+                    </IonRow>
+                    <IonRow>
+                      <IonText>
+                        {t('popup.mareograph.n2000SeaLevel')}:&nbsp;
+                        {!isDataOutdated12Hours ? (
+                          <>
+                            {properties.n2000WaterLevel && !isOffline ? (
+                              <>
+                                {properties.n2000WaterLevel >= 0 ? '+' : ''}
+                                {Math.round(properties.n2000WaterLevel / 10)}{' '}
+                                <dd aria-label={t('fairwayCards.unit.cmDesc', { count: Math.round((properties.n2000WaterLevel || 0) / 10) })}>cm</dd>
+                              </>
+                            ) : (
+                              <>
+                                <InfoIcon className="fairwayCardInfoIcon" />
+                                {t('common.noData')}
+                              </>
+                            )}
+                          </>
+                        ) : (
+                          <>
+                            <IonIcon className="outdatedDataIcon" icon={alertIcon} color="#EC0E0E" />
+                            {t('popup.common.outdatedData')}
+                          </>
+                        )}
+                      </IonText>
+                    </IonRow>
+                  </span>
                 </IonLabel>
               </p>
             );
