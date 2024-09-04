fdc112b85477fa2d178688affbf912bd9d56bc7f
nooksanen <noora.oksanen@cgi.com>
2024-09-04 10:16:23 +0300

    Handle focus outline

--- admin/src/components/CreationModal.tsx
+++ admin/src/components/CreationModal.tsx
@@ -130,7 +130,7 @@ const CreationModal: React.FC<ModalProps> = ({ itemList, itemType, isOpen, setIs
             </IonLabel>
             <IonSelect
               ref={selectVersionRef}
-              className="selectInput"
+              className="selectInput version-select"
               disabled={!source}
               placeholder={t('general.choose')}
               interface="popover"
