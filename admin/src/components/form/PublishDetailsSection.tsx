6edd4034cd64e484ba9089e49ba19e8ca3630810
howard_green <howard.green@cgi.com>
2024-11-14 13:29:48 +0200

    DVK-1654 : added public section control (as disabled) to luonnos state card

--- admin/src/components/form/PublishDetailsSection.tsx
+++ admin/src/components/form/PublishDetailsSection.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { FairwayCardInput, HarborInput, Status } from '../../graphql/generated';
+import { FairwayCardInput, HarborInput } from '../../graphql/generated';
 import { IonButton, IonItem, IonText } from '@ionic/react';
 import { useTranslation } from 'react-i18next';
 import ChevronIcon from '../../theme/img/chevron.svg?react';
@@ -16,37 +16,34 @@ const PublishDetailsSection: React.FC<PublishDetailsSectionProps> = ({ state }) 
     setIsPublishSectionOpen((state) => !state);
   };
 
-  const sectionClassName = 'sectionContent' + (isPublishSectionOpen ? ' open' : ' closed');
+  const sectionClassName = 'sectionContent' + (isPublishSectionOpen && !!state.publishDetails ? ' open' : ' closed');
 
   return (
-    <>
-      {state.status === Status.Public && !!state.publishDetails && (
-        <div className="formSection" key={'publishSectionContainer'}>
-          <IonItem className="sectionHeader">
-            <IonText>
-              <h3>{t('fairwaycard.publish-details')}</h3>
-            </IonText>
-            <IonButton
-              data-testid="toggleOpenPublishSection"
-              slot="end"
-              fill="clear"
-              className={'icon-only small toggle' + (isPublishSectionOpen ? ' close' : ' open')}
-              onClick={() => toggleSection()}
-              title={(isPublishSectionOpen ? t('general.close') : t('general.open')) ?? ''}
-              aria-label={(isPublishSectionOpen ? t('general.close') : t('general.open')) ?? ''}
-            >
-              <ChevronIcon />
-            </IonButton>
-          </IonItem>
-          <div className={sectionClassName}>
-            <IonText className="sectionContent">
-              <p>{state.publishDetails}</p>
-            </IonText>
-          </div>
-          <br />
-        </div>
-      )}
-    </>
+    <div className="formSection" key={'publishSectionContainer'}>
+      <IonItem className="sectionHeader">
+        <IonText>
+          <h3 className={state.publishDetails ? '' : 'disabled'}>{t('fairwaycard.publish-details')}</h3>
+        </IonText>
+        <IonButton
+          data-testid="toggleOpenPublishSection"
+          disabled={!state.publishDetails}
+          slot="end"
+          fill="clear"
+          className={'icon-only small toggle' + (isPublishSectionOpen && !!state.publishDetails ? ' close' : ' open')}
+          onClick={() => toggleSection()}
+          title={(isPublishSectionOpen && !!state.publishDetails ? t('general.close') : t('general.open')) ?? ''}
+          aria-label={(isPublishSectionOpen && !!state.publishDetails ? t('general.close') : t('general.open')) ?? ''}
+        >
+          <ChevronIcon />
+        </IonButton>
+      </IonItem>
+      <div className={sectionClassName}>
+        <IonText className="sectionContent">
+          <p>{state.publishDetails}</p>
+        </IonText>
+      </div>
+      <br />
+    </div>
   );
 };
 export default PublishDetailsSection;
