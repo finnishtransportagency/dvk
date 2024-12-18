0eeaa3dc37199e26cfeb3fc77528ec313624e62d
howard_green <howard.green@cgi.com>
2024-12-18 16:28:46 +0200

    DVK-1708 : overcommit fix

--- src/components/content/fairwayCard/SquatCalculationTemplate.tsx
+++
@@ -1,8 +0,0 @@
-import { IonGrid } from '@ionic/react';
-import React from 'react';
-
-const SquatCalculationTemplate: React.FC = () => {
-  return <IonGrid className="forecast ion-no-padding"></IonGrid>;
-};
-
-export default SquatCalculationTemplate;
