a725f4a86080dd576555902c4ef4b6b370930811
howard_green <howard.green@cgi.com>
2024-12-19 16:51:50 +0200

    empty_squats
    (cherry picked from commit 3c4e1ec5d33cd857a16bd3ebb7e6562a25c4229b)

---
+++ src/components/content/fairwayCard/SquatCalculationTemplate.tsx
@@ -0,0 +1,8 @@
+import { IonGrid } from '@ionic/react';
+import React from 'react';
+
+const SquatCalculationTemplate: React.FC = () => {
+  return <IonGrid className="forecast ion-no-padding"></IonGrid>;
+};
+
+export default SquatCalculationTemplate;
