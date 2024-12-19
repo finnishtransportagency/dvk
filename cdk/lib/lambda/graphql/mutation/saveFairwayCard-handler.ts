ffd7077912571f8668bc036a62b7e1fef1e217e2
howard_green <howard.green@cgi.com>
2024-12-19 15:30:31 +0200

    DVK-1708: fixed types in handler

--- cdk/lib/lambda/graphql/mutation/saveFairwayCard-handler.ts
+++ cdk/lib/lambda/graphql/mutation/saveFairwayCard-handler.ts
@@ -163,13 +163,13 @@ export function mapFairwayCardToModel(
         return {
           place: mapText(t?.place),
           depth: t?.depth,
-          estimatedWaterDepth: t?.estimatedWaterDepth,
-          fairwayWidth: t?.fairwayWidth,
+          estimatedWaterDepth: t?.estimatedWaterDepth ? parseFloat(t?.estimatedWaterDepth) : null,
+          fairwayWidth: t?.fairwayWidth ? parseFloat(t?.fairwayWidth) : null,
           fairwayform: t?.fairwayForm,
           targetFairways: t?.targetFairways ?? [],
           suitableFairwayAreas: t?.suitableFairwayAreas ?? [],
-          slopeScale: t?.slopeScale,
-          slopeHeight: t?.slopeHeight,
+          slopeScale: t?.slopeScale ? parseFloat(t?.slopeScale) : null,
+          slopeHeight: t?.slopeHeight ? parseFloat(t?.slopeHeight) : null,
           additionalInformation: mapText(t?.additionalInformation),
           fairwayForm: t?.fairwayForm,
         };
