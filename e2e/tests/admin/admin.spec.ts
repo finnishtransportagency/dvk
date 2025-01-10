fd0abe629c89bf416117233fcbc212e37af61bd3
Howard Green <43003358+greeho@users.noreply.github.com>
2025-01-10 12:58:27 +0200

    DVK-1731 : Include squat calculation in admin testing (#1220)
    * DVK-1731 : fixed tests
    
    * DVK-1731 : Refactored slightly to fix running in command line
    
    * DVK-1731 : fixed SONAR lint warning for random number

--- e2e/tests/admin/admin.spec.ts
+++ e2e/tests/admin/admin.spec.ts
@@ -3,6 +3,8 @@ import { test, expect, Page, Locator } from '@playwright/test';
 const PORT = process.env.PORT ?? '3000';
 const URL = `http://localhost:${PORT}/yllapito/`;
 const HARBOR_NAME = { fi: 'fi_testHarbor', sv: 'sv_testHarbor', en: 'en_testHarbor' };
+const SQUAT_PLACE = { fi: 'fi_Squat template Finnish', sv: 'sv_Squat template Swedish', en: 'en_Squat template English' };
+const SQUAT_ADD_INFO = { fi: 'fi_Squat add info', sv: 'sv_Squat add info', en: 'en_Squat add info' };
 const SAVE_FAILED = 'Tallennus epäonnistui';
 const SAVE_SUCCEEDED = 'Tallennus onnistui';
 const FAIRWAY_CARD_REMOVAL = 'Väyläkortin poisto';
@@ -22,7 +24,6 @@ const SEARCH_BY_NAME_OR_ID = 'Hae nimellä tai tunnisteella';
 const PUBLISHED = 'Julkaistu';
 const HARBOR = 'Satama';
 const FAIRWAY_CARD = 'Väyläkortti';
-
 const E2E_TEST_PREFIX = 'e2etest';
 const NEW_HARBOR_FROM_SCRATCH_ID = E2E_TEST_PREFIX + 'nh' + generateRandomNumberAsString();
 const FAIRWAY_CARD_FROM_TEMPLATE = E2E_TEST_PREFIX + 'modfc' + generateRandomNumberAsString();
@@ -40,7 +41,7 @@ async function openPage(page: Page) {
   }
 }
 
-async function getFirstItemFromList(page: Page, type?: string, state?: string) {
+async function getFirstItemFromList(page: Page, type?: string, state?: string, isN2000: boolean = false) {
   let searchLocator = page.getByTestId('resultrow');
   if (type) {
     const typeFilter = page.getByTestId('resulttype').filter({ hasText: type });
@@ -50,15 +51,19 @@ async function getFirstItemFromList(page: Page, type?: string, state?: string) {
     const stateFilter = page.getByTestId('resultstatus').filter({ hasText: state });
     searchLocator = searchLocator.filter({ has: stateFilter });
   }
+  if (isN2000) {
+    const n2000Filter = page.getByTestId('n2000').filter({ hasText: 'N2000' });
+    searchLocator = searchLocator.filter({ has: n2000Filter });
+  }
   return searchLocator.first();
 }
 
-async function clickOnResultsByTypeAndState(page: Page, type: string, state: string) {
-  (await getFirstItemFromList(page, type, state)).click();
+async function clickOnResultsByTypeAndState(page: Page, type: string, state: string, isN2000: boolean) {
+  (await getFirstItemFromList(page, type, state, isN2000)).click();
 }
 
 function generateRandomNumberAsString(length: number = 5) {
-  const randomNumber = Math.floor(Math.pow(10, length) * Math.random());
+  const randomNumber = Math.floor(Math.pow(10, length) * Math.random()); // NOSONAR - ignore security issue with random number as it's only a test
   return ('' + randomNumber).padStart(length, '0');
 }
 
@@ -75,10 +80,51 @@ async function fillLatLngAndSaveSucceeds(page: Page) {
   await save(page, SAVE_SUCCEEDED);
 }
 
+async function fillSquatTemplateAndSaveSucceeds(page: Page) {
+  const addNewButton = page.getByTestId('addNewCalcSection');
+  await addNewButton.click();
+  const newId = await page.getByTestId('squatCalculationPlacefi').count();
+  await fillFieldWithValue(page, 'squatCalculationPlacefi', SQUAT_PLACE.fi);
+  await fillFieldWithValue(page, 'squatCalculationPlacesv', SQUAT_PLACE.sv);
+  await fillFieldWithValue(page, 'squatCalculationPlaceen', SQUAT_PLACE.en);
+  await selectItemFromSelectWithCustomDropdown(page, 'squatTargetFairwayIdsSelect');
+  await selectItemFromSelectWithCustomDropdown(page, 'squatSuitableFairwayAreaIdsSelect');
+  await fillFieldWithValue(page, 'squatCalculationEstimatedWaterDepth-' + (newId - 1), '50');
+  await selectItemFromSelectInput(page, 'squatCalculationFairwayFormSelect');
+  await fillFieldWithValue(page, 'squatCalculationAdditionalInformationfi', SQUAT_ADD_INFO.fi);
+  await fillFieldWithValue(page, 'squatCalculationAdditionalInformationsv', SQUAT_ADD_INFO.sv);
+  await fillFieldWithValue(page, 'squatCalculationAdditionalInformationen', SQUAT_ADD_INFO.en);
+
+  await save(page, SAVE_SUCCEEDED);
+}
+
+async function selectItemFromSelectInput(page: Page, id: string, index: number = 0) {
+  await selectItemFromDropdown(page, id, 'radio', index);
+}
+
+async function selectItemFromSelectWithCustomDropdown(page: Page, id: string, index: number = 0) {
+  //Checkboxes here are images
+  await selectItemFromDropdown(page, id, 'checkbox', index);
+}
+
+async function selectItemFromDropdown(page: Page, id: string, type: 'checkbox' | 'radio', index: number) {
+  //Get the last element of this test id on the page to account for multiple section elements
+  const select = page.getByTestId(id).last();
+  await select.click();
+
+  //For custom dropdown checkbox is actually an img
+  if (type === 'checkbox') {
+    await page.locator('ion-checkbox').getByRole('img').nth(index).click();
+    page.getByRole('checkbox').first().press('Escape');
+  } else {
+    await page.getByRole(type).nth(index).click();
+  }
+}
+
 async function fillFieldWithValue(page: Page, id: string | Locator, value: string, wait: number = 1000) {
   //Need to wait otherwise text not input correctly
   await page.waitForTimeout(wait);
-  const field = typeof id === 'string' ? page.getByTestId(id).locator('input') : id.locator('input');
+  const field = typeof id === 'string' ? page.getByTestId(id).locator('input').last() : id.locator('input').last();
   await field.clear();
   await field.click();
   await field.pressSequentially(value, { delay: 20 });
@@ -110,21 +156,16 @@ async function deleteItem(page: Page, expectMessage: string) {
   await page.getByRole('button', { name: DELETE }).first().click();
   await expect(page.getByText(SAVE_SUCCEEDED)).toBeVisible();
   await page.getByLabel(OK, { exact: true }).first().click();
-  await expect(page.getByText(DELETED)).toBeVisible();
+  await expect(page.getByText(DELETED, { exact: true }).first()).toBeVisible();
 }
 
-async function createNewVersionFromPublishedSave(page: Page, type: string) {
+async function createNewVersionFromPublishedSave(page: Page, type: string, isN2000: boolean = false) {
   //Get first published element
-  await clickOnResultsByTypeAndState(page, type, PUBLISHED);
+  await clickOnResultsByTypeAndState(page, type, PUBLISHED, isN2000);
   await page.getByRole('button', { name: CREATE_NEW_VERSION }).click();
   //Click save in the modal
   await page.getByRole('button', { name: SAVE }).click();
-  //Click save on the main page
-  await page.getByTestId('saveButton').locator('button').click();
-  //Check success
-  await expect(page.getByText(SAVE_SUCCEEDED)).toBeVisible();
-  //Close the modal
-  await page.getByRole('button', { name: OK }).click();
+  await page.waitForTimeout(3000);
 }
 
 async function fillTemplate(page: Page, randomname: string, id: string) {
@@ -146,17 +187,21 @@ async function fillTemplate(page: Page, randomname: string, id: string) {
 }
 
 test.describe('Modify operations for cards and harbors', () => {
-  test.beforeAll(async () => {
+  test.beforeEach(async () => {
     test.setTimeout(60000);
   });
   test.skip('should save published harbor', async ({ page }) => {
     await openPage(page);
     await createNewVersionFromPublishedSave(page, HARBOR);
+    await save(page, SAVE_SUCCEEDED);
+    await deleteHarbour(page);
   });
 
   test.skip('should save published fairway card', async ({ page }) => {
     await openPage(page);
     await createNewVersionFromPublishedSave(page, FAIRWAY_CARD);
+    await save(page, SAVE_SUCCEEDED);
+    await deleteHarbour(page);
   });
 
   test('should create new fairway card from template', async ({ page }) => {
@@ -166,6 +211,13 @@ test.describe('Modify operations for cards and harbors', () => {
     await fillTemplate(page, selectedname, FAIRWAY_CARD_FROM_TEMPLATE);
     await page.getByRole('button', { name: CREATE_FAIRWAY_CARD }).click();
     await save(page, SAVE_SUCCEEDED);
+    await deleteFairwayCard(page);
+  });
+
+  test('should add squat calculation template', async ({ page }) => {
+    await openPage(page);
+    await createNewVersionFromPublishedSave(page, FAIRWAY_CARD, true);
+    await fillSquatTemplateAndSaveSucceeds(page);
     await deleteFairwayCard(page);
   });
 
