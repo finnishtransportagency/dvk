0574cfd8d9f453412c2873cb742c454b8bea53bf
kettunenj <juhani.kettunen@cgi.com>
2025-01-15 11:04:45 +0200

    change s3 sdk to newer

--- cdk/lib/lambda/graphql/query/weatherLimits-handler.ts
+++ cdk/lib/lambda/graphql/query/weatherLimits-handler.ts
@@ -1,4 +1,4 @@
-import { S3 } from 'aws-sdk';
+import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
 import { Handler } from 'aws-lambda';
 import { getNewStaticBucketName } from '../../environment';
 
@@ -21,20 +21,20 @@ interface WeatherLimitsData {
   limits: WeatherLimit[];
 }
 
-const s3 = new S3();
+const s3Client = new S3Client({});
 const BUCKET_NAME = process.env.WEATHER_LIMITS_BUCKET ?? getNewStaticBucketName();
 const FILE_KEY = process.env.WEATHER_LIMITS_KEY ?? 'weather-limits.json';
 
 export const handler: Handler = async () => {
   try {
-    const response = await s3
-      .getObject({
-        Bucket: BUCKET_NAME,
-        Key: FILE_KEY,
-      })
-      .promise();
+    const command = new GetObjectCommand({
+      Bucket: BUCKET_NAME,
+      Key: FILE_KEY,
+    });
 
-    const weatherLimits: WeatherLimitsData = JSON.parse(response.Body?.toString() ?? '{"limits": []}');
+    const response = await s3Client.send(command);
+    const bodyContents = await response.Body?.transformToString();
+    const weatherLimits: WeatherLimitsData = JSON.parse(bodyContents ?? '{"limits": []}');
 
     return {
       limits: weatherLimits.limits,
