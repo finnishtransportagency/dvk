eca81e86b43f68eea14d7c07c0fa86e8c5d4337c
hiisilaa <aki.hiisila@cgi.com>
2025-02-12 09:12:47 +0200

    Version 1.24 translations

--- admin/src/components/map/mapOverlays/LayerModal.tsx
+++ admin/src/components/map/mapOverlays/LayerModal.tsx
@@ -48,7 +48,7 @@ const LayerModal: React.FC<ModalProps> = ({ isOpen, setIsOpen }) => {
     { id: 'speedlimit', title: t('homePage.map.controls.layer.speedLimits') },
     {
       id: 'specialarea',
-      title: t('homePage.map.controls.layer.specialAreas'),
+      title: t('homePage.map.controls.layer.specificAreas'),
       childLayers: [
         { id: 'specialarea2', title: t('homePage.map.controls.layer.specialarea2') },
         { id: 'specialarea9', title: t('homePage.map.controls.layer.specialarea9') },
