2a8ec42c84ef0a43a78e931876617d400cf6d02d
hiisilaa <aki.hiisila@cgi.com>
2025-02-14 14:15:15 +0200

    Use canvas gradient for specific area fill styles

---
+++ src/components/layerStyles/utils/stripeFill.ts
@@ -0,0 +1,28 @@
+import { Fill } from 'ol/style';
+
+export function getStripeFill(color1: string, color2: string): Fill {
+  const canvas = document.createElement('canvas');
+  canvas.width = 50;
+  canvas.height = 50;
+  const context = canvas.getContext('2d') as CanvasRenderingContext2D;
+  const gradient = context.createLinearGradient(50, 0, 0, 50);
+  gradient.addColorStop(0, color1);
+  gradient.addColorStop(1 / 8, color1);
+  gradient.addColorStop(1 / 8, color2);
+  gradient.addColorStop(3 / 8, color2);
+  gradient.addColorStop(3 / 8, color1);
+  gradient.addColorStop(5 / 8, color1);
+  gradient.addColorStop(5 / 8, color2);
+  gradient.addColorStop(7 / 8, color2);
+  gradient.addColorStop(7 / 8, color1);
+  gradient.addColorStop(1, color1);
+  context.fillStyle = gradient;
+  context.fillRect(0, 0, 50, 50);
+
+  const canvas2 = document.createElement('canvas');
+  const context2 = canvas2.getContext('2d') as CanvasRenderingContext2D;
+
+  return new Fill({
+    color: context2.createPattern(canvas, 'repeat'),
+  });
+}
