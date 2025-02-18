f5d4cb3ca0ab51da2490517cdca85394004f4a3d
hiisilaa <aki.hiisila@cgi.com>
2025-02-18 11:02:00 +0200

    Use canvas gradient for specific area fill styles in admin

--- admin/src/components/map/layerStyles/specialAreaStyles.ts
+++ admin/src/components/map/layerStyles/specialAreaStyles.ts
@@ -1,72 +1,40 @@
 import { FeatureLike } from 'ol/Feature';
 import { Polygon } from 'ol/geom';
-import { Fill, Icon, Stroke, Style } from 'ol/style';
+import { Icon, Stroke, Style } from 'ol/style';
+import { getStripeFill } from './utils/stripeFill';
 import anchorage from '../../../theme/img/ankkurointialue.svg';
 import meet from '../../../theme/img/kohtaamiskielto_ikoni.svg';
-import specialarea from '../../../theme/img/erityisalue_tausta.svg';
-import specialareaSelected from '../../../theme/img/erityisalue_tausta_active.svg';
-import specialareaSelected2 from '../../../theme/img/erityisalue_tausta_active2.svg';
-import specialarea9 from '../../../theme/img/erikoisalue_tausta.svg';
-import specialarea9Selected from '../../../theme/img/erikoisalue_tausta_active.svg';
-import specialarea9Selected2 from '../../../theme/img/erikoisalue_tausta_active2.svg';
-
-const specialAreaImage = new Image();
-specialAreaImage.src = specialarea;
-const specialAreaSelectedImage = new Image();
-specialAreaSelectedImage.src = specialareaSelected;
-const specialAreaSelectedImage2 = new Image();
-specialAreaSelectedImage2.src = specialareaSelected2;
-
-const specialArea9Image = new Image();
-specialArea9Image.src = specialarea9;
-const specialArea9SelectedImage = new Image();
-specialArea9SelectedImage.src = specialarea9Selected;
-const specialArea9SelectedImage2 = new Image();
-specialArea9SelectedImage2.src = specialarea9Selected2;
 
 export function getSpecialAreaPolygonStyle(feature: FeatureLike, selected: boolean, selected2 = false) {
-  const canvas = document.createElement('canvas');
-  const context = canvas.getContext('2d') as CanvasRenderingContext2D;
-  let gradient;
+  let stripeFill;
   if (selected2) {
-    gradient = context.createPattern(specialAreaSelectedImage2, 'repeat');
+    stripeFill = getStripeFill('rgba(255, 195, 0, 0.65)', 'rgba(255, 195, 0, 0.4)');
   } else {
-    gradient = context.createPattern(selected ? specialAreaSelectedImage : specialAreaImage, 'repeat');
+    stripeFill = getStripeFill('rgba(255, 195, 0, 0.5)', 'rgba(255, 195, 0, 0.25)');
   }
+
   return new Style({
     stroke: new Stroke({
       color: '#C57A11',
       width: 2,
     }),
-    fill: new Fill({
-      color: gradient,
-    }),
+    fill: stripeFill,
     zIndex: 2,
   });
 }
 
 export function getSpecialArea9Style(feature: FeatureLike, resolution: number) {
-  const highlighted = !!feature.get('hoverStyle');
   const selectedCard = !!feature.get('selected');
-  const canvas = document.createElement('canvas');
-  const context = canvas.getContext('2d') as CanvasRenderingContext2D;
-  let gradient;
-  if (selectedCard && highlighted) {
-    gradient = context.createPattern(specialArea9SelectedImage2, 'repeat');
-  } else if (selectedCard || highlighted) {
-    gradient = context.createPattern(specialArea9SelectedImage, 'repeat');
+  let stripeFill;
+  if (selectedCard) {
+    stripeFill = getStripeFill('rgba(236, 14, 14, 0.65)', 'rgba(236, 14, 14, 0.4)');
   } else {
-    gradient = context.createPattern(specialArea9Image, 'repeat');
+    stripeFill = getStripeFill('rgba(236, 14, 14, 0.5)', 'rgba(236, 14, 14, 0.25)');
   }
   const styles: Array<Style> = [];
 
   if (!selectedCard || resolution <= 100) {
-    const fillStyle = new Style({
-      fill: new Fill({
-        color: gradient,
-      }),
-    });
-    styles.push(fillStyle);
+    styles.push(new Style({ fill: stripeFill }));
   }
 
   if (selectedCard && resolution <= 100) {
